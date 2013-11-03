ab25ff92cf310df636a4578f562c30df0b0ed0ef
Andrew Rice <acr31@cam.ac.uk>
2013-11-03 20:12:53 +0000

    Changed topicPage to use the name of the topic and not the path

--- src/main/java/uk/ac/cam/cl/dtg/teaching/models/TopicPage.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/models/TopicPage.java
@@ -4,26 +4,27 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
 public class TopicPage {
-	private String topicName;
+
+	private String topicTitle;
 	private String level;
 	private ImmutableList<String> conceptIds;
 	private ImmutableList<String> questionIds;
 	private ImmutableMap<String, ContentInfo> environment;
 
-	public TopicPage(String topicName, String level,
+	public TopicPage(String topicTitle, String level,
 			ImmutableList<String> conceptIds,
 			ImmutableList<String> questionIds,
 			ImmutableMap<String, ContentInfo> environment) {
 		super();
-		this.topicName = topicName;
+		this.topicTitle = topicTitle;
 		this.level = level;
 		this.conceptIds = conceptIds;
 		this.questionIds = questionIds;
 		this.environment = environment;
 	}
 
-	public String getTopicName() {
-		return topicName;
+	public String getTopicTitle() {
+		return topicTitle;
 	}
 
 	public String getLevel() {
