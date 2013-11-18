9f96b7c97aaf78c7053e4b08e5259aae14b4f498
Ian Davies <git@iandavies.org>
2013-11-18 10:56:58 +0000

    Add exception handling to nREPL server launch.

--- src/main/clojure/rutherford/nrepl.clj
+++ src/main/clojure/rutherford/nrepl.clj
@@ -1,6 +1,12 @@
 (ns rutherford.nrepl
-  (:use [clojure.tools.nrepl.server :only (start-server stop-server)]))
+  (:use [clojure.tools.nrepl.server :only (start-server stop-server)])
+  (:import [java.net BindException]))
 
-(defonce server (start-server :port 7888))
+(defonce server 
+  (let [port 7888]
+    (try 
+      (start-server :port port)
+      (catch BindException e (println "Could not start nREPL server - port" port "already in use."))
+      (catch Exception e (println "Could not start nREPL server.")))))
 
 (println "nREPL server started on port 7888")
