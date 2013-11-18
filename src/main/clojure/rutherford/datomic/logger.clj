62f58ab1301a87848ca0f3d827c9e03a22ad9cf8
Ian Davies <git@iandavies.org>
2013-11-18 13:44:04 +0000

    Fix horrible Java-like Clojure.

--- src/main/clojure/rutherford/datomic/logger.clj
+++ src/main/clojure/rutherford/datomic/logger.clj
@@ -20,24 +20,24 @@
 		     (println "Logging event from session" session-id)
 	      
 	       ;; Look up which keys from the json have matching attributes in the logging.event ns
-	       (def logging-attributes
-					 (map #(name (first %)) (q '[:find ?i :where [?e :db/ident ?i]
+         (let [logging-attributes (map #(name (first %)) (q '[:find ?i :where [?e :db/ident ?i]
 					                                             [_ :db.install/attribute ?e]
 					                                             [(namespace ?i) ?n]
-					                                             [(= ?n "logging.event")]] (db @rd/conn)))) 
-	       (def matches (filter #(some #{(name %)} logging-attributes) 
-	                            (keys event)))
-	       
-	       (def values (for [m matches] {(keyword "logging.event" (name m)) (m event)}))
+					                                             [(= ?n "logging.event")]] (db @rd/conn)))
+               
+               matches (filter #(some #{(name %)} logging-attributes) 
+	                            (keys event))
+               
+               values (for [m matches] {(keyword "logging.event" (name m)) (m event)})]
 	
-	       ;; Add the event to the database
-		    @(d/transact @rd/conn [{:db/id #db/id[:db.part/user -1]
-		                            :logging/sessionId session-id}
-	                         
-	                             (apply merge {:db/id #db/id[:db.part/user]
-	                                           :logging.event/session #db/id[:db.part/user -1]}
-	                                          values)])
-         true)
+		       ;; Add the event to the database
+			    @(d/transact @rd/conn [{:db/id #db/id[:db.part/user -1]
+			                            :logging/sessionId session-id}
+		                         
+		                             (apply merge {:db/id #db/id[:db.part/user]
+		                                           :logging.event/session #db/id[:db.part/user -1]}
+		                                          values)])
+          true))
       
        ;; We only get here if @conn is nil
        (do 
