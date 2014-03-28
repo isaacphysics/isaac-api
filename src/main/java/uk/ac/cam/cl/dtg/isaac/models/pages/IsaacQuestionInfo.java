da8fdc0022295e063d1b5de3b56fa07da00c7f3a
Stephen Cummins <sacummins@gmail.com>
2014-03-28 12:56:47 +0000

    Added new QuestionInfo DTO for isaac project - this is effectively a hexagons metadata.

---
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/models/pages/IsaacQuestionInfo.java
@@ -0,0 +1,50 @@
+package uk.ac.cam.cl.dtg.isaac.models.pages;
+
+/**
+ * DTO that provides high level information for Isaac Questions
+ * 
+ * Used for gameboards and for related questions links
+ */
+public class IsaacQuestionInfo {
+	
+	private String id;
+	private String title;
+	private String description;
+	private String level;
+	private String url;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	
+	
+}
