32a0c936f863026e98daf0d1fa883b487d37398f
Stephen Cummins <sacummins@gmail.com>
2014-05-21 18:10:38 +0100

    Removed all frontend code from the api project including silken, soy templates, css and javascript.

--- src/main/java/uk/ac/cam/cl/dtg/isaac/app/IsaacController.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/app/IsaacController.java
@@ -2,17 +2,8 @@ package uk.ac.cam.cl.dtg.isaac.app;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.UUID;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 import javax.servlet.http.HttpServletRequest;
@@ -30,29 +21,18 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import uk.ac.cam.cl.dtg.isaac.models.ContentDetail;
 import uk.ac.cam.cl.dtg.isaac.models.ContentInfo;
 import uk.ac.cam.cl.dtg.isaac.models.ContentPage;
-import uk.ac.cam.cl.dtg.isaac.models.IndexPage;
-import uk.ac.cam.cl.dtg.isaac.models.TopicDetail;
-import uk.ac.cam.cl.dtg.isaac.models.TopicPage;
 import uk.ac.cam.cl.dtg.segue.api.SegueApiFacade;
 import uk.ac.cam.cl.dtg.segue.api.SegueGuiceConfigurationModule;
-import uk.ac.cam.cl.dtg.segue.dao.IUserDataManager;
 import uk.ac.cam.cl.dtg.segue.dto.Content;
 import uk.ac.cam.cl.dtg.segue.dto.Figure;
-import uk.ac.cam.cl.dtg.segue.dto.User;
 import uk.ac.cam.cl.dtg.util.Mailer;
 import uk.ac.cam.cl.dtg.util.PropertiesLoader;
 
-import com.google.api.client.util.Strings;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.template.soy.tofu.SoyTofuException;
-import com.papercut.silken.SilkenServlet;
-import com.papercut.silken.TemplateRenderer;
 
 /**
  * Rutherford Controller
@@ -66,12 +46,6 @@ public class IsaacController {
 	
 	private static SegueApiFacade api;
 
-	// Map of contentID to detail
-	private Map<String, ContentDetail> contentDetails = ContentDetail.load();
-	
-	// Map of topicPath to detail
-	private Map<String, TopicDetail> topicDetails = TopicDetail.load();
-
 	public IsaacController(){
 		// Get an instance of the segue api so that we can service requests directly from it 
 		// without using the rest endpoints.
@@ -79,99 +53,14 @@ public class IsaacController {
 			Injector injector = Guice.createInjector(new IsaacGuiceConfigurationModule(), new SegueGuiceConfigurationModule());
 			api = injector.getInstance(SegueApiFacade.class);
 		}
-	}
-	
-	/**
-	 * Temporary solution to show all content of different types in no particular order. (For dev test)
-	 * 
-	 * @param req
-	 * @return
-	 */
-	@GET
-	@Path("learn")
-	@Produces("application/json")
-	public Response getTopics(@Context HttpServletRequest req) {		
 		
-//		test of user registration
+//		test of user registration - this is just a snippet for future reference as I didn't know where else to put it.
 //		User user = api.getCurrentUser(req);
 //		// example of requiring user to be logged in.
 //		if(null == user)
 //			return api.authenticationInitialisation(req, "google");
 //		else
 //			log.info("User Logged in: " + user.getEmail());
-			
-		
-		// get all concepts
-		List<ContentInfo> conceptsList = this.extractContentInfo((List<Content>) api.getAllContentByType("concept",0).getEntity(), getSoyGlobalMap(req).get(Constants.PROXY_PATH));
-		
-		// temporary solution to get all questions as well for testing purposes. 
-
-		//TODO we need to work out a good way of allowing editors to group concepts and questions based on level in a rutherford specific sort of way.
-		// This code is just to allow easy access to all api content from one page for testing
-		conceptsList.addAll(this.extractContentInfo((List<Content>) api.getAllContentByType("isaacQuestionPage",0).getEntity(), getSoyGlobalMap(req).get(Constants.PROXY_PATH)));
-		return Response.ok(new IndexPage(conceptsList)).build();
-		
-		// get all questions
-	}
-
-	/**
-	 * Return the list of concepts and questions available for this topic at
-	 * this level
-	 * @deprecated until we come up with new approach
-	 * @param topic
-	 * @param level
-	 * @return
-	 */
-	@GET
-	@Path("topics/{topic:.*}/level-{level}")
-	@Produces("application/json")
-	public TopicPage getTopicWithLevel(@PathParam("topic") String topic, @PathParam("level") String level) {
-
-		TopicDetail topicDetail = topicDetails.get(topic);
-
-		ImmutableList.Builder<String> conceptIdBuilder = ImmutableList.builder();
-		ImmutableList.Builder<String> questionIdBuilder = ImmutableList.builder();
-		
-		HashSet<String> linkedConceptIds = new HashSet<String>();
-
-		SortedSet<ContentDetail> values = new TreeSet<ContentDetail>(contentDetails.values());
-
-		// Find all the questions for this topic.
-		for (ContentDetail detail : values) 
-		{
-			if (detail.getType().equals(ContentDetail.TYPE_QUESTION) && topic.equals(detail.getTopic()) && level.equals(detail.getLevel())) 
-			{
-				questionIdBuilder.add(detail.getId());
-				
-				for (String cid : detail.getRelatedConceptIds())
-				{
-					if (contentDetails.containsKey(cid))
-						linkedConceptIds.add(cid);
-				}
-			}
-		}
-		
-		conceptIdBuilder.addAll(linkedConceptIds);
-		
-		ImmutableList<String> questionIds = questionIdBuilder.build();
-		
-		ImmutableList<String> conceptIds = conceptIdBuilder.build();
-
-		return new TopicPage(topicDetail.getTopic(), topicDetail.getTitle(), level, conceptIds, questionIds, null, topicDetail.getPdf().get(level));
-	}
-
-	/**
-	 * For now we can circumvent this by just showing all content in the learn page.
-	 * 
-	 * @deprecated until we come up with new approach
-	 * @param topic
-	 * @return
-	 */
-	@GET
-	@Path("topics/{topic}")
-	@Produces("application/json")
-	public TopicPage getTopic(@PathParam("topic") String topic) {
-		return getTopicWithLevel(topic, "1");
 	}
 	
 	@GET
@@ -192,13 +81,17 @@ public class IsaacController {
 	}
 	
 	@GET
-	@Path("contentPages/{contentPage}")
+	@Path("pages/{page}")
 	@Produces("application/json")
 	public ContentPage getContentPage(@Context HttpServletRequest req,
-			@PathParam("contentPage") String contentPage) {
-		Content c = (Content) api.getContentById(contentPage).getEntity();
+			@PathParam("page") String page) {
+
+		Content c = (Content) api.getContentById(page).getEntity();
+		Injector injector = Guice.createInjector(new IsaacGuiceConfigurationModule());
+		PropertiesLoader propertiesLoader = injector.getInstance(PropertiesLoader.class);
 		
-		ContentPage cp = new ContentPage(c.getId(),c,this.buildMetaContentmap(getSoyGlobalMap(req).get("proxyPath"), c));		
+		String proxyPath = propertiesLoader.getProperty(Constants.PROXY_PATH);
+		ContentPage cp = new ContentPage(c.getId(),c,this.buildMetaContentmap(proxyPath, c));		
 		return cp;
 	}
 	
@@ -217,9 +110,12 @@ public class IsaacController {
 	@Path("search/full-site/")
 	@Produces("application/json")
 	public List<ContentInfo> search(@Context HttpServletRequest req, @FormParam("searchString") String searchString) {
-		String proxyPath = getSoyGlobalMap(req).get("proxyPath");
-		Response searchResponse = api.search(searchString);
+		Injector injector = Guice.createInjector(new IsaacGuiceConfigurationModule());
+		PropertiesLoader propertiesLoader = injector.getInstance(PropertiesLoader.class);
 		
+		String proxyPath = propertiesLoader.getProperty(Constants.PROXY_PATH);
+
+		Response searchResponse = api.search(searchString);
 		List<ContentInfo> summaryOfSearchResults = null;
 		if(searchResponse.getEntity() instanceof List<?>){
 			summaryOfSearchResults = this.extractContentInfo((List<Content>) searchResponse.getEntity(), proxyPath);
@@ -227,52 +123,6 @@ public class IsaacController {
 		
 		return summaryOfSearchResults;
 	}	
-	
-	@POST
-	@Consumes({"application/x-www-form-urlencoded"})
-	@Path("contact-us/register-interest")
-	public ImmutableMap<String, String> postRegisterInterest(
-			@FormParam("name") String name,
-			@FormParam("email") String email,
-			@FormParam("role") String role,
-			@FormParam("school") String school,
-			@FormParam("year") String year,
-			@FormParam("feedback") String feedbackAgreement) {
-		
-		boolean feedback = false;
-		
-		if(null != feedbackAgreement){
-			feedback = true;
-		}
-
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("Name: " + name);
-		sb.append(" email: " + email);
-		sb.append(" role: " + role);
-		sb.append(" school: " + school);
-		sb.append(" year: " + year);
-		sb.append(" feedback: " + new Boolean(feedback).toString());
-		
-		log.info("Register Interest details: " + sb.toString());
-
-		// TODO split last name and firstname.
-		User newUser = new User(null, name, null, email, role, school, year, feedback, new Date());
-		
-		Injector injector = Guice.createInjector(new SegueGuiceConfigurationModule());
-		IUserDataManager registrationManager = injector.getInstance(IUserDataManager.class);
-
-		//boolean success = registrationManager.register(newUser) != null;
-		// TODO this code is going to have to be re-written.
-		boolean success = false;
-		String outcome = "success";
-		if(!success){
-			outcome = "Registration failed: Error registering user.";
-			log.error("Error in registering interest for user " + name + " " + email);
-		}
-		
-		return ImmutableMap.of("result", outcome);
-	}
 	
 	@POST
 	@Consumes({"application/x-www-form-urlencoded"})
@@ -329,67 +179,8 @@ public class IsaacController {
 		}
 		
 		return ImmutableMap.of("result", "success");
-	}
-	
-	public ImmutableMap<String, String> getSoyGlobalMap(HttpServletRequest req) {
-		Injector injector = Guice.createInjector(new IsaacGuiceConfigurationModule());
-		PropertiesLoader propertiesLoader = injector.getInstance(PropertiesLoader.class);
-		
-		String proxyPath = propertiesLoader.getProperty(Constants.PROXY_PATH);
-		String trackingId = propertiesLoader.getProperty(Constants.ANALYTICS_TRACKING_ID);
-		
-		if(Strings.isNullOrEmpty(proxyPath)){
-			proxyPath = req.getContextPath();
-			log.debug("No proxyPath defined in properties file - using default context");
-		}
-		
-		if(Strings.isNullOrEmpty(trackingId)){
-			trackingId = "";
-		}
-		
-//		if (req.getLocalAddr().equals("128.232.20.43")) {
-//			proxyPath = "/research/dtg/rutherford-staging";
-//			trackingId = "UA-45629473-1";
-//		} else if (req.getLocalAddr().equals("128.232.20.40")) {
-//			proxyPath = "/research/dtg/rutherford";
-//			trackingId = "UA-45629473-2";
-//		}
-		
-		ImmutableMap.Builder<String,String> globalMap = ImmutableMap.builder();
-		globalMap.put("liveVersion", (String) api.getLiveVersion().getEntity());
-		globalMap.put("contextPath", req.getContextPath());
-		globalMap.put("proxyPath", proxyPath); // TODO: sort out how we pass this information to the client.
-		globalMap.put(Constants.PROXY_PATH, proxyPath); // TODO: remove duplicate
-		globalMap.put("analyticsTrackingId", trackingId);
-		globalMap.put("newSessionId", UUID.randomUUID().toString());
-		globalMap.put("newUserId", UUID.randomUUID().toString());
+	}	
 
-		// TODO: complete this so that we get proper user information.
-		User user = api.getCurrentUser(req);
-		if(null != user)
-			globalMap.put("usersFirstname", user.getGivenName());
-		else
-			globalMap.put("usersFirstname", "");
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss dd/M/yyyy");
-		String date = sdf.format(api.dateOfLastSegueVersionUpdate());
-		globalMap.put("dateOfVersionChange", date); //This actually represents the last time we checked and updated the live version number.
-		
-		return globalMap.build();
-	}
-	
-	private String renderTemplate(String templateName, ImmutableMap<String, String> globalMap) {
-		TemplateRenderer renderer = SilkenServlet.getTemplateRenderer();
-
-		String cContent = "";
-		try {
-			cContent = renderer.render(templateName, null, globalMap, Locale.ENGLISH);
-		} catch (SoyTofuException e) {
-			cContent = "<i>No content available.</i>";
-			log.error("Error applying soy template", e);
-		}
-		return cContent;
-	}
 	
 	/**
 	 * This method will look at a content objects related content list and return a list of contentInfo objects which can be used for creating links etc.
