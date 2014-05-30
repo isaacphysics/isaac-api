98828a663140f5c89a2f666633734da80dd9252a
Stephen Cummins <sacummins@gmail.com>
2014-05-30 15:54:55 +0100

    Changed isaac/api endpoint to be just /api

--- src/main/java/uk/ac/cam/cl/dtg/isaac/app/IsaacHttpServletDispatcher.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/app/IsaacHttpServletDispatcher.java
@@ -5,10 +5,10 @@ import javax.servlet.annotation.WebServlet;
 
 import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
 
-@WebServlet(urlPatterns = { "/isaac/*"}, 
+@WebServlet(urlPatterns = { "/api/*"}, 
 initParams = { 
 		@WebInitParam(name = "javax.ws.rs.Application", value = "uk.ac.cam.cl.dtg.isaac.app.IsaacApplicationRegister"),
-		@WebInitParam(name = "resteasy.servlet.mapping.prefix", value="/isaac/api")
+		@WebInitParam(name = "resteasy.servlet.mapping.prefix", value="/api")
 })
 public class IsaacHttpServletDispatcher extends HttpServletDispatcher {
 	private static final long serialVersionUID = 1L;
