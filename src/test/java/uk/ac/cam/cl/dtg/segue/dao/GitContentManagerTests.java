8738c959e917e7dabd62a561bc1bf25052574924
Nick Rogers <rogersnm@hotmail.co.uk>
2014-07-15 16:09:39 +0100

    Add 1 new test for GitContentManager (+minor refactoring of GitContentManagerTests)

--- src/test/java/uk/ac/cam/cl/dtg/segue/dao/GitContentManagerTests.java
+++ src/test/java/uk/ac/cam/cl/dtg/segue/dao/GitContentManagerTests.java
@@ -9,6 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.eclipse.jgit.errors.AmbiguousObjectException;
@@ -19,6 +20,9 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import uk.ac.cam.cl.dtg.segue.database.GitDb;
 import uk.ac.cam.cl.dtg.segue.dos.content.Content;
@@ -35,7 +39,7 @@ public class GitContentManagerTests {
 	private ContentMapper contentMapper;
 
 	private GitContentManager defaultGCM;
-	
+
 	private static final String INITIAL_VERSION = "0b72984c5eff4f53604fe9f1c724d3f387799db9";
 
 	/**
@@ -134,8 +138,8 @@ public class GitContentManagerTests {
 	}
 
 	/**
-	 * Test that the searchForContent method returns an empty ResultsWrapper
-	 * if no results are found.
+	 * Test that the searchForContent method returns an empty ResultsWrapper if
+	 * no results are found.
 	 */
 	@Test
 	@SuppressWarnings("unchecked")
@@ -143,64 +147,66 @@ public class GitContentManagerTests {
 		final String searchString = "";
 		final Map<String, List<String>> fieldsThatMustMatch = null;
 
-		Map<String, Map<String, Content>> gitCache =
-				new ConcurrentHashMap<String, Map<String, Content>>();
+		Map<String, Map<String, Content>> gitCache = new ConcurrentHashMap<String, Map<String, Content>>();
 		gitCache.put(INITIAL_VERSION, new ConcurrentHashMap<String, Content>());
-		
+
 		searchProvider = createMock(ISearchProvider.class);
-		
+
 		GitContentManager gitContentManager = new GitContentManager(database,
 				searchProvider, contentMapper, gitCache);
 
 		ResultsWrapper<String> searchHits = createMock(ResultsWrapper.class);
 
 		expect(searchProvider.hasIndex(INITIAL_VERSION)).andReturn(true).once();
-		/*expect(searchProvider.fuzzySearch(isA(String.class), isA(String.class),
-					isA(String.class), isA(Map.class), isA(String.class), isA(String.class),
-					isA(String.class), isA(String.class), ""))
-				.andReturn(searchHits).once();*/
-		expect(searchProvider.fuzzySearch(
-					anyString(), anyString(), anyString(), anyObject(Map.class),
-					anyString(), anyString(),
-					anyString(), anyString(),
-					anyString())).andReturn(searchHits).once();
+		/*
+		 * expect(searchProvider.fuzzySearch(isA(String.class),
+		 * isA(String.class), isA(String.class), isA(Map.class),
+		 * isA(String.class), isA(String.class), isA(String.class),
+		 * isA(String.class), "")) .andReturn(searchHits).once();
+		 */
+		expect(
+				searchProvider.fuzzySearch(anyString(), anyString(),
+						anyString(), anyObject(Map.class), anyString(),
+						anyString(), anyString(), anyString(), anyString()))
+				.andReturn(searchHits).once();
 		expect(searchHits.getResults()).andReturn(new LinkedList<String>())
 				.once();
 		expect(searchHits.getTotalResults()).andReturn(0L).once();
 		replay(database, searchProvider, searchHits);
 
-		assertTrue(gitContentManager.searchForContent(INITIAL_VERSION, searchString, fieldsThatMustMatch).getResults().size() == 0);
+		assertTrue(gitContentManager
+				.searchForContent(INITIAL_VERSION, searchString,
+						fieldsThatMustMatch).getResults().size() == 0);
 
 		verify(database, searchProvider, searchHits);
 	}
-	
+
 	/**
 	 * Test that the getById method returns the correct object.
 	 */
 	@Test
 	public void getById_retrieveObject_checkCorrectObjectReturned() {
 		final String id = "test";
-		
-		Map<String, Map<String, Content>> gitCache =
-				new ConcurrentHashMap<String, Map<String, Content>>();
+
+		Map<String, Map<String, Content>> gitCache = new ConcurrentHashMap<String, Map<String, Content>>();
 		Map<String, Content> contentMap = new TreeMap<String, Content>();
 		Content testContent = new Content();
 		contentMap.put(id, testContent);
 		gitCache.put(INITIAL_VERSION, contentMap);
-		
+
 		searchProvider = createMock(ISearchProvider.class);
-		
+
 		GitContentManager gitContentManager = new GitContentManager(database,
 				searchProvider, contentMapper, gitCache);
 
 		expect(searchProvider.hasIndex(INITIAL_VERSION)).andReturn(true).once();
 		replay(searchProvider);
-		
+
 		assertTrue(gitContentManager.getById(id, INITIAL_VERSION) == testContent);
-		
+
 		verify(searchProvider);
 	}
-	
+
 	/**
 	 * Test that the getById method returns null if it is passed a null id.
 	 */
@@ -209,124 +215,166 @@ public class GitContentManagerTests {
 		String id = null;
 		assertTrue(defaultGCM.getById(id, INITIAL_VERSION) == null);
 	}
-	
+
 	/**
-	 * Test that the getById method returns null if the specified version
-	 * does not exist.
+	 * Test that the getById method returns null if the specified version does
+	 * not exist.
 	 */
 	@Test
 	public void getById_missingVersion_checkNullReturned() {
 		final String id = "test";
-		
-		Map<String, Map<String, Content>> gitCache =
-				new ConcurrentHashMap<String, Map<String, Content>>();
-		
+
+		Map<String, Map<String, Content>> gitCache = new ConcurrentHashMap<String, Map<String, Content>>();
+
 		searchProvider = createMock(ISearchProvider.class);
-		
+
 		GitContentManager gitContentManager = new GitContentManager(database,
 				searchProvider, contentMapper, gitCache);
 
-		
-		expect(database.verifyCommitExists(INITIAL_VERSION)).andReturn(false).once();
+		expect(database.verifyCommitExists(INITIAL_VERSION)).andReturn(false)
+				.once();
 		replay(database);
-		
+
 		assertTrue(gitContentManager.getById(id, INITIAL_VERSION) == null);
-		
+
 		verify(database);
 	}
-	
+
 	/**
-	 * Test that the getById method returns null if the specified object
-	 * does not exist the specified version.
+	 * Test that the getById method returns null if the specified object does
+	 * not exist the specified version.
 	 */
 	@Test
 	public void getById_missingKey_checkNullReturned() {
 		final String id = "test";
-		
-		Map<String, Map<String, Content>> gitCache =
-				new ConcurrentHashMap<String, Map<String, Content>>();
+
+		Map<String, Map<String, Content>> gitCache = new ConcurrentHashMap<String, Map<String, Content>>();
 
 		// Create a version containing an empty TreeMap of Content
 		gitCache.put(INITIAL_VERSION, new TreeMap<String, Content>());
-		
+
 		searchProvider = createMock(ISearchProvider.class);
-		
+
 		GitContentManager gitContentManager = new GitContentManager(database,
 				searchProvider, contentMapper, gitCache);
 
-		
 		expect(searchProvider.hasIndex(INITIAL_VERSION)).andReturn(true).once();
 		replay(searchProvider);
-		
+
 		assertTrue(gitContentManager.getById(id, INITIAL_VERSION) == null);
-		
+
 		verify(searchProvider);
 	}
-	
+
 	/**
-	 * Test that the ensureCache method returns false if a null version hash
-	 * is provided.
+	 * Test that the ensureCache method returns false if a null version hash is
+	 * provided.
 	 */
 	@Test
 	public void ensureCache_nullVersion_checkFalseReturned() {
 		assertTrue(!defaultGCM.ensureCache(null));
 	}
-	
+
 	/**
 	 * Test that the ensureCache method returns true if a cached and indexed
 	 * version is provided.
 	 */
 	@Test
 	public void ensureCache_cachedVerion_checkTrueReturned() {
-		Map<String, Map<String, Content>> gitCache =
-				new ConcurrentHashMap<String, Map<String, Content>>();
+		Map<String, Map<String, Content>> gitCache = new ConcurrentHashMap<String, Map<String, Content>>();
 		gitCache.put(INITIAL_VERSION, new TreeMap<String, Content>());
-		
+
 		searchProvider = createMock(ISearchProvider.class);
-		
+
 		GitContentManager gitContentManager = new GitContentManager(database,
 				searchProvider, contentMapper, gitCache);
-		
+
 		expect(searchProvider.hasIndex(INITIAL_VERSION)).andReturn(true).once();
 		replay(searchProvider);
-		
+
 		assertTrue(gitContentManager.ensureCache(INITIAL_VERSION));
-		
+
 		verify(searchProvider);
 	}
-	
+
 	/**
-	 * Test that the ensureCache method rebuilds the cache when a version
-	 * that exists in the database is not found in the cache.
+	 * Test that the ensureCache method rebuilds the cache when a version that
+	 * exists in the database is not found in the cache.
 	 */
 	@Test
-	public void ensureCache_uncachedVersion_checkGitContentIndexBuilt() throws RevisionSyntaxException, AmbiguousObjectException, IncorrectObjectTypeException, IOException {
+	public void ensureCache_uncachedVersion_checkGitContentIndexBuilt()
+			throws RevisionSyntaxException, AmbiguousObjectException,
+			IncorrectObjectTypeException, IOException {
 		database = createMock(GitDb.class);
 		searchProvider = createMock(ISearchProvider.class);
 
-		Map<String, Map<String, Content>> gitCache =
-				new ConcurrentHashMap<String, Map<String, Content>>();
-		
-		GitContentManager gitContentManager = new GitContentManager(database, searchProvider, contentMapper, gitCache);
-		
+		Map<String, Map<String, Content>> gitCache = new ConcurrentHashMap<String, Map<String, Content>>();
+
+		GitContentManager gitContentManager = new GitContentManager(database,
+				searchProvider, contentMapper, gitCache);
+
 		Repository repository = createMock(Repository.class);
 		ObjectId commitId = createMock(ObjectId.class);
 		TreeWalk treeWalk = createMock(TreeWalk.class);
-		
-		expect(database.verifyCommitExists(INITIAL_VERSION)).andReturn(true).once();
+
+		expect(database.verifyCommitExists(INITIAL_VERSION)).andReturn(true)
+				.once();
 		expect(database.getGitRepository()).andReturn(repository).once();
 		expect(repository.resolve(INITIAL_VERSION)).andReturn(commitId).once();
-		expect(database.getTreeWalk(anyString(), anyString())).andReturn(treeWalk).once();
+		expect(database.getTreeWalk(anyString(), anyString())).andReturn(
+				treeWalk).once();
 		expect(treeWalk.next()).andReturn(false).once();
 		repository.close();
 		expectLastCall().once();
-		expect(searchProvider.hasIndex(INITIAL_VERSION)).andReturn(true).times(2);
-		
+		expect(searchProvider.hasIndex(INITIAL_VERSION)).andReturn(true).times(
+				2);
+
 		replay(database, repository, treeWalk, searchProvider);
-		
+
 		assertTrue(gitContentManager.ensureCache(INITIAL_VERSION));
 		assertTrue(gitCache.containsKey(INITIAL_VERSION));
-		
+
 		verify(database, repository, treeWalk, searchProvider);
+	}
+
+	/**
+	 * Test that the buildSearchIndexFromLocalGitIndex sends each Content object to the
+	 * searchProvider.
+	 */
+	@Test
+	public void buildSearchIndexFromLocalGitIndex_sendContentToSearchProvider_checkSearchProviderReceivesObject()
+			throws JsonProcessingException {
+		database = createMock(GitDb.class);
+		searchProvider = createMock(ISearchProvider.class);
+		String uniqueObjectId = UUID.randomUUID().toString();
+		String uniqueObjectHash = UUID.randomUUID().toString();
+
+		Map<String, Map<String, Content>> gitCache =
+				new ConcurrentHashMap<String, Map<String, Content>>();
+		Map<String, Content> contents = new TreeMap<String, Content>();
+		Content content = new Content();
+		content.setId(uniqueObjectId);
+		contents.put(uniqueObjectId, content);
+		gitCache.put(INITIAL_VERSION, contents);
+
+		GitContentManager gitContentManager = new GitContentManager(database,
+				searchProvider, contentMapper, gitCache);
+
+		ObjectMapper objectMapper = createMock(ObjectMapper.class);
+
+		expect(searchProvider.hasIndex(INITIAL_VERSION)).andReturn(false)
+				.once();
+		expect(contentMapper.getContentObjectMapper()).andReturn(objectMapper)
+				.once();
+		expect(objectMapper.writeValueAsString(content)).andReturn(
+				uniqueObjectHash).once();
+		expect(searchProvider.indexObject(eq(INITIAL_VERSION), anyString(),
+						eq(uniqueObjectHash), eq(uniqueObjectId))).andReturn(true).once();
+
+		replay(searchProvider, contentMapper, objectMapper);
+
+		gitContentManager.buildSearchIndexFromLocalGitIndex(INITIAL_VERSION);
+
+		verify(searchProvider, contentMapper, objectMapper);
 	}
 }
