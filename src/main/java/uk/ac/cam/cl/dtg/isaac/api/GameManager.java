isaacphysics/isaac-api

commit 94ffc4153a3cddb87040ee61e3a74d2493fda83c

Added not_attempted to my boards page endpoint

src/main/java/uk/ac/cam/cl/dtg/isaac/api/GameManager.java
@@ -289,9 +289,12 @@ public class GameManager {
 			
 			if (null == showOnly) {
 				resultToReturn.add(gameboard);
+			} else if (gameboard.getPercentageCompleted() == 0 && showOnly.equals(GameboardState.NOT_ATTEMPTED)) {
+				resultToReturn.add(gameboard);
 			} else if (gameboard.getPercentageCompleted() == 100 && showOnly.equals(GameboardState.COMPLETED)) {
 				resultToReturn.add(gameboard);
-			} else if (gameboard.getPercentageCompleted() > 0 && showOnly.equals(GameboardState.IN_PROGRESS)) {
+			} else {
+				// in_progress
 				resultToReturn.add(gameboard);
 			}
 		}

src/main/java/uk/ac/cam/cl/dtg/isaac/api/IsaacController.java
@@ -488,6 +488,8 @@ public class IsaacController {
 				gameboardShowCriteria = GameboardState.COMPLETED;
 			} else if (showCriteria.toLowerCase().contains("in_progress")) {
 				gameboardShowCriteria = GameboardState.IN_PROGRESS;
+			} else if (showCriteria.toLowerCase().contains("not_attempted")) {
+				gameboardShowCriteria = GameboardState.NOT_ATTEMPTED;
 			} else {
 				return new SegueErrorResponse(Status.BAD_REQUEST,
 						"Unable to interpret showOnly criteria specified " + showCriteria).toResponse();				

commit ffc499bf4d47753e91b9a1a869da583fe4c45f17

Fixed bug introduced in 94ffc4153a3cddb87040ee61e3a74d2493fda83c

src/main/java/uk/ac/cam/cl/dtg/isaac/api/GameManager.java
@@ -293,7 +293,7 @@ public class GameManager {
 				resultToReturn.add(gameboard);
 			} else if (gameboard.getPercentageCompleted() == 100 && showOnly.equals(GameboardState.COMPLETED)) {
 				resultToReturn.add(gameboard);
-			} else {
+			} else if (gameboard.getPercentageCompleted() > 0 && showOnly.equals(GameboardState.IN_PROGRESS)) {
 				// in_progress
 				resultToReturn.add(gameboard);
 			} 

src/main/java/uk/ac/cam/cl/dtg/isaac/api/IsaacController.java
@@ -508,14 +508,11 @@ public class IsaacController {
 				
 				if (instruction.equals("created")) {
 					parsedSortInstructions.add(immutableEntry(CREATED_DATE_FIELDNAME, s));
-				}
-				
-				if (instruction.equals("visited")) {
+				} else if (instruction.equals("visited")) {
 					parsedSortInstructions.add(immutableEntry(VISITED_DATE_FIELDNAME, s));
-				}
-				
-				if (instruction.equals("level")) {
-					parsedSortInstructions.add(immutableEntry(LEVEL_FIELDNAME, s));
+				} else {
+					return new SegueErrorResponse(Status.BAD_REQUEST,
+							"Sorry we do not recognise the sort instruction " + instruction).toResponse();
 				}
 			}
 		}