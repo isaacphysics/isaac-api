f912b59a5b8051c8121c419ffd130cebafc08d11
Stephen Cummins <sacummins@gmail.com>
2014-08-12 22:06:57 +0100

    Deleted unused interface.

--- src/main/java/uk/ac/cam/cl/dtg/segue/quiz/IMultiFieldQuestion.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.cam.cl.dtg.segue.quiz;
-
-import java.util.List;
-
-import uk.ac.cam.cl.dtg.segue.dos.content.Question;
-
-public interface IMultiFieldQuestion {
-	public List<? extends Question> getFields();
-}
