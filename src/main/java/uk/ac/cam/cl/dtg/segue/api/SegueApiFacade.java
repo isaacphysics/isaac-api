4cfd34f20981f5d8f0581fd02e5bf27c1fa8f167
Stephen Cummins <sacummins@gmail.com>
2014-08-18 13:30:41 +0100

    Removed unused imports

--- src/main/java/uk/ac/cam/cl/dtg/segue/api/SegueApiFacade.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/SegueApiFacade.java
@@ -30,8 +30,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -44,7 +42,6 @@ import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
 import org.elasticsearch.common.collect.Lists;
