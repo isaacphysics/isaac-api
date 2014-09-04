5789a503c554676876d6b0e10d798ed04dcd6c13
Ian Davies <git@iandavies.org>
2014-09-04 17:15:18 +0100

    Build script now takes single version number for both API and app.

--- segue_isaac_build.sh
+++ segue_isaac_build.sh
@@ -1,23 +1,21 @@
 #! /bin/bash
 # This script is a build script intended to only work on windows.
 
-echo "Type the tag name to deploy for the isaac-api (BACKEND)"
-read SEGUE_API_VERSION_TO_DEPLOY
+set -e # Exit on failure
 
-echo "Type the environment to build the api for either 'live' or 'staging'"
-read SEGUE_ENVIRONMENT_TO_BUILD
-
-echo "Type the tag name to deploy for the isaac-app (FRONTEND)"
-read SEGUE_APP_VERSION_TO_DEPLOY
+read -p "Version to deploy: " VERSION_TO_DEPLOY
+read -p "Segue environment (live/staging): " SEGUE_ENVIRONMENT_TO_BUILD
 
 BUILD_DIR=$LOCALAPPDATA/Temp/isaacDeploy
+
+echo Building in $BUILD_DIR
 
 cd $LOCALAPPDATA
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-git clone -b $SEGUE_API_VERSION_TO_DEPLOY --depth 1 https://github.com/ucam-cl-dtg/isaac-api.git
+git clone -b $VERSION_TO_DEPLOY --depth 1 https://github.com/ucam-cl-dtg/isaac-api.git
 cd isaac-api
 mvn clean package -P $SEGUE_ENVIRONMENT_TO_BUILD
 mv target/*.war ..
@@ -25,13 +23,13 @@ mv *.sh ..
 cd ..
 rm -rf isaac-api
 
-git clone -b $SEGUE_APP_VERSION_TO_DEPLOY --depth 1 https://github.com/ucam-cl-dtg/isaac-app.git
+git clone -b $VERSION_TO_DEPLOY --depth 1 https://github.com/ucam-cl-dtg/isaac-app.git
 cd isaac-app
 npm install
 grunt dist
 mv isaac-app.tar.gz ..
 cd ..
+#rm -rf //?/$BUILD_DIR/isaac-app
 cmd.exe /c "rmdir /s /q isaac-app"
 echo "Build complete" 
 explorer .
-#rm -rf //?/$BUILD_DIR/isaac-app
