0549146f74b7de53103e92f8de48c09c4072c4b3
Stephen Cummins <sacummins@gmail.com>
2014-09-08 14:55:02 +0100

    Added equals and hashcode methods to gameboard Item so that they work correctly in collections.contains.

--- src/main/java/uk/ac/cam/cl/dtg/isaac/dto/GameboardItem.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/dto/GameboardItem.java
@@ -146,4 +146,33 @@ public class GameboardItem {
 		this.state = state;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof GameboardItem)) {
+			return false;
+		}
+		GameboardItem other = (GameboardItem) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
 }
