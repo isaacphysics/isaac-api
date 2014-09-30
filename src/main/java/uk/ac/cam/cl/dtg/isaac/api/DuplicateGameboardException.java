5e8ad80bd1c6a50a762393546dac97be76028bcd
Stephen Cummins <sacummins@gmail.com>
2014-09-30 15:13:10 +0100

    Added potentially useful game board exceptions.

---
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/api/DuplicateGameboardException.java
@@ -0,0 +1,25 @@
+package uk.ac.cam.cl.dtg.isaac.api;
+
+/**
+ * An exception that indicates a gameboard already exists with the id provided.
+ * 
+ * @author Stephen Cummins
+ */
+public class DuplicateGameboardException extends Exception {
+	private static final long serialVersionUID = -2812103839917179690L;
+
+	/**
+	 * Default constructor.
+	 */
+	public DuplicateGameboardException() {
+		
+	}
+	
+	/**
+	 * constructor with message. 
+	 * @param message - error message
+	 */
+	public DuplicateGameboardException(final String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/api/InvalidGameboardException.java
@@ -0,0 +1,25 @@
+package uk.ac.cam.cl.dtg.isaac.api;
+
+/**
+ * An exception that indicates an invalid gameboard has been provided.
+ * 
+ * @author Stephen Cummins
+ */
+public class InvalidGameboardException extends Exception {
+	private static final long serialVersionUID = -2812103839917179690L;
+
+	/**
+	 * Default constructor.
+	 */
+	public InvalidGameboardException() {
+		
+	}
+	
+	/**
+	 * constructor with message. 
+	 * @param message - error message
+	 */
+	public InvalidGameboardException(final String message) {
+		super(message);
+	}
+}
