a91f6af89bb167b54e164cad0cd8713c6ac107e5
Stephen Cummins <sacummins@gmail.com>
2015-05-21 15:38:05 +0100

    Added a condition to stop us getting into a loop.

--- src/main/java/uk/ac/cam/cl/dtg/segue/api/managers/SegueContextNotifier.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/managers/SegueContextNotifier.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.cl.dtg.segue.api.managers;
 
+import java.util.Collection;
 import java.util.List;
 
 import javax.servlet.ServletContextEvent;
@@ -16,7 +17,7 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 /**
- * SegueContextListener 
+ * SegueContextListener
  * 
  * A class alerts any registered segue listeners as to the servlet context.
  * 
@@ -26,25 +27,31 @@ import com.google.inject.Injector;
 @WebListener
 public class SegueContextNotifier implements ServletContextListener {
 	private static final Logger log = LoggerFactory.getLogger(SegueContextNotifier.class);
-	
+
 	private final List<ServletContextListener> listeners;
-	
+
 	/**
-	 * The constructor which will invoke use the Guice injector to get
-	 * instances of everything that should be notified of any context messages.
+	 * The constructor which will invoke use the Guice injector to get instances
+	 * of everything that should be notified of any context messages.
 	 */
 	public SegueContextNotifier() {
-		Injector injector = Guice.createInjector(
-				new SegueGuiceConfigurationModule());
-		
+		Injector injector = Guice.createInjector(new SegueGuiceConfigurationModule());
+
 		listeners = Lists.newArrayList();
-		// use guice to initialise everything that needs to be kept informed of the servlet context.
-		for (Class<? extends ServletContextListener> segueListener : SegueGuiceConfigurationModule
-				.getRegisteredContextListenerClasses()) {
+		Collection<Class<? extends ServletContextListener>> registeredContextListenerClasses 
+			= SegueGuiceConfigurationModule.getRegisteredContextListenerClasses();
+		// use guice to initialise everything that needs to be kept informed of
+		// the servlet context.
+		for (Class<? extends ServletContextListener> segueListener : registeredContextListenerClasses) {
+			if (segueListener == SegueContextNotifier.class) {
+				// we probably don't want to register ourself.
+				continue;
+			}
+			log.debug("Registering context listener: " + segueListener.toString());
 			listeners.add(injector.getInstance(segueListener));
 		}
 	}
-	
+
 	@Override
 	public void contextInitialized(final ServletContextEvent sce) {
 		log.info("Segue Application Informed of server start up. Registering listeners.");
