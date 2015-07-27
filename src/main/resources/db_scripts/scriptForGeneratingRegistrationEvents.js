c769a427f478b169e377a64f96587f8b21489ed7
Stephen Cummins <sacummins@gmail.com>
2015-07-27 12:46:40 +0100

    added mongodb script to regenerate user registration events after we deploy fix included in: 5ddd65abe0be8bd399e0298a8c4fc2b902594cac

---
+++ src/main/resources/db_scripts/scriptForGeneratingRegistrationEvents.js
@@ -0,0 +1,14 @@
+db.loggedEvents.remove({eventType:"USER_REGISTRATION"});
+var users = db.users.find({});
+users.forEach(function(user){
+    db.loggedEvents.insert({
+            eventType: "USER_REGISTRATION",
+            userId: user._id.valueOf(),
+            anonymousUser: false,
+            timestamp: user.registrationDate
+        });
+});
+
+
+
+
