9bcb921354a37eec12f24f4687f8a208d98b5bd0
Stephen Cummins <sacummins@gmail.com>
2015-08-13 15:24:16 +0100

    Remove unused import

--- src/main/java/uk/ac/cam/cl/dtg/segue/dao/content/GitContentManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dao/content/GitContentManager.java
@@ -29,7 +29,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.Nullable;
