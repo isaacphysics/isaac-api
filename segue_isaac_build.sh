a0dd8538a4a91c9a1d7423a2c5ed63cbeb8369dc
Stephen Cummins <sacummins@gmail.com>
2015-09-07 11:34:24 +0100

    Modified build script to use new deploy profile for API and also enabled "master" to be specified as the version number - and correctly download the latest commit from the API

--- segue_isaac_build.sh
+++ segue_isaac_build.sh
@@ -3,8 +3,7 @@
 
 set -e # Exit on failure
 
-read -p "isaac-app version to deploy: " VERSION_TO_DEPLOY
-read -p "Segue environment (live/staging): " SEGUE_ENVIRONMENT_TO_BUILD
+read -p "isaac-app version to deploy (e.g. v1.3.0 or for a SNAPSHOT build type 'master'):" VERSION_TO_DEPLOY
 
 BUILD_DIR=$LOCALAPPDATA/Temp/isaacDeploy
 
@@ -19,17 +18,27 @@ git clone -b $VERSION_TO_DEPLOY --depth 1 https://github.com/ucam-cl-dtg/isaac-a
 cd isaac-app
 npm install
 grunt dist
+
+# determine segue version to use.
 SEGUE_VERSION=`grunt segue-version | grep ^segueVersion | sed s/.*://`
+if [ $VERSION_TO_DEPLOY = "master" ]; then
+	SEGUE_VERSION="master"
+else
+	SEGUE_VERSION = v$SEGUE_VERSION
+fi
+
 mv isaac-app.tar.gz ..
 cd ..
 #rm -rf //?/$BUILD_DIR/isaac-app
 cmd.exe /c "rmdir /s /q isaac-app"
 
-git clone -b v$SEGUE_VERSION --depth 1 https://github.com/ucam-cl-dtg/isaac-api.git
+git clone -b $SEGUE_VERSION --depth 1 https://github.com/ucam-cl-dtg/isaac-api.git
 cd isaac-api
-mvn clean package -P $SEGUE_ENVIRONMENT_TO_BUILD
+mvn clean package -P deploy
 mv target/*.war ..
 mv *.sh ..
+rm -rf $BUILD_DIR/generated-config
+mv target/generated-config/ $BUILD_DIR
 cd ..
 rm -rf isaac-api
 echo "Build complete" 
