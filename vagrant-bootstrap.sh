e57d1ff587be282bdc1172756a5d0dbb54eda067
Ian Davies <git@iandavies.org>
2015-12-02 14:43:44 +0000

    Tweak vagrant config.

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -7,7 +7,7 @@ apt-get -y install linux-image-extra-$(uname -r)
 sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
 sh -c "echo deb http://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
 apt-get update
-apt-get -y install lxc-docker openjdk-7-jre-headless
+apt-get -y install lxc-docker
 
 usermod -a -G docker vagrant
 
@@ -16,8 +16,10 @@ usermod -a -G docker vagrant
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb http://packages.elastic.co/elasticsearch/1.4/debian stable main" | tee -a /etc/apt/sources.list.d/elasticsearch-1.4.list
 apt-get update
-apt-get -y install elasticsearch
+apt-get -y install openjdk-7-jre-headless elasticsearch
 
 ### Install MongoDB
 
 apt-get -y install mongodb
+
+# Once MongoDB is installed, need to edit /etc/mongodb.conf to set bind = 0.0.0.0
