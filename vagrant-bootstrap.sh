0015d89e1a760301848aa3e70818e5cd84da63bf
Ian Davies <git@iandavies.org>
2015-12-03 15:51:27 +0000

    More vagrant config. Postgres still not quite working.

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -23,3 +23,15 @@ apt-get -y install openjdk-7-jre-headless elasticsearch
 apt-get -y install mongodb
 
 # Once MongoDB is installed, need to edit /etc/mongodb.conf to set bind = 0.0.0.0
+
+### Install PostgreSQL
+
+## TODO: Make sure we have PostgreSQL 9.4
+apt-get -y install postgresql postgresql-contrib
+
+## After installing postgres, need to create user. This will prompt for password
+#
+#     sudo -u postgres createuser -D -A -P rutherford
+#     sudo -u postgres createdb -O rutherford rutherford
+#     sudo -u postgres psql -f /isaac-api/src/main/resources/db_scripts/postgres-rutherford-create-script.sql
+
