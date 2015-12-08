4847e000f1f7fe876616481b68b5df9201599125
Ian Davies <git@iandavies.org>
2015-12-08 12:14:05 +0000

    More vagrant config. MongoDB, PostgreSQL, ElasticSearch and Docker all working.

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,37 +1,46 @@
 #!/usr/bin/env bash
 
-### Install Docker
-
-apt-get update
-apt-get -y install linux-image-extra-$(uname -r)
+## Add things we need
 sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
 sh -c "echo deb http://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/1.4/debian stable main" | tee -a /etc/apt/sources.list.d/elasticsearch-1.4.list
+sh -c "wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -"
+sh -c "echo deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main\ > /etc/apt/sources.list.d/pgdg.list"
 apt-get update
-apt-get -y install lxc-docker
+
+
+### Install Docker
+
+apt-get -y install linux-image-extra-$(uname -r) lxc-docker
 
 usermod -a -G docker vagrant
 
 ### Install ElasticSearch
-
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb http://packages.elastic.co/elasticsearch/1.4/debian stable main" | tee -a /etc/apt/sources.list.d/elasticsearch-1.4.list
-apt-get update
 apt-get -y install openjdk-7-jre-headless elasticsearch
 
 ### Install MongoDB
 
 apt-get -y install mongodb
 
-# Once MongoDB is installed, need to edit /etc/mongodb.conf to set bind = 0.0.0.0
+# Once MongoDB is installed, need to edit /etc/mongodb.conf to set bind_ip = 0.0.0.0
+# Then sudo service mongodb restart
 
 ### Install PostgreSQL
 
-## TODO: Make sure we have PostgreSQL 9.4
-apt-get -y install postgresql postgresql-contrib
+apt-get -y install postgresql-9.4
 
 ## After installing postgres, need to create user. This will prompt for password
 #
 #     sudo -u postgres createuser -D -A -P rutherford
 #     sudo -u postgres createdb -O rutherford rutherford
-#     sudo -u postgres psql -f /isaac-api/src/main/resources/db_scripts/postgres-rutherford-create-script.sql
+#     sudo -u postgres psql -f /isaac-api/src/main/resources/db_scripts/postgres-rutherford-create-script.sql rutherford
+#
+# Then add
+#     listen_address = '*'
+# to /etc/postgresql/9.4/main/postgresql.conf
+#
+# Then add
+#     host all all 0.0.0.0/0 md5
+# to /etc/postgresql/9.4/main/pg_hba.conf
 
