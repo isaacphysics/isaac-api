ac678d58a6a62460303e59e1efafb8380de61a9a
Alistair Stead <alistair.stead@gmail.com>
2016-02-01 23:41:46 +0000

    Added new files for equation content

---
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacSymbolicValidator.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2016 Alistair Stead
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.cam.cl.dtg.isaac.quiz;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.cam.cl.dtg.isaac.dos.IsaacSymbolicQuestion;
+import uk.ac.cam.cl.dtg.segue.dos.QuestionValidationResponse;
+import uk.ac.cam.cl.dtg.segue.dos.content.Choice;
+import uk.ac.cam.cl.dtg.segue.dos.content.Equation;
+import uk.ac.cam.cl.dtg.segue.dos.content.Question;
+import uk.ac.cam.cl.dtg.segue.quiz.IValidator;
+
+/**
+ * Validator that only provides functionality to validate symbolic questions.
+ *
+ * @author Alistair Stead
+ *
+ */
+public class IsaacSymbolicValidator implements IValidator {
+    private static final Logger log = LoggerFactory.getLogger(IsaacSymbolicValidator.class);
+
+    @Override
+    public QuestionValidationResponse validateQuestionResponse(final Question question, final Choice answer) {
+        Validate.notNull(question);
+        Validate.notNull(answer);
+
+        if (!(question instanceof IsaacSymbolicQuestion)) {
+            throw new IllegalArgumentException(String.format(
+                    "This validator only works with Isaac Symbolic Questions... (%s is not symbolic)",
+                    question.getId()));
+        }
+        
+        if (!(answer instanceof Equation)) {
+            throw new IllegalArgumentException(String.format(
+                    "Expected Equation for IsaacSymbolicQuestion: %s. Received (%s) ", question.getId(),
+                    answer.getClass()));
+        }
+
+        IsaacSymbolicQuestion symbolicQuestion = (IsaacSymbolicQuestion) question;
+        Equation symbolicAnswer = (Equation) answer;
+
+        final String trustedAnswer = symbolicQuestion.getPythonAnswer();
+        
+        // To determine correctness, we just need to compare 
+        symbolicAnswer.setCorrect(checkSymbolicEquality(trustedAnswer, symbolicAnswer.getPythonValue()));
+
+        // TODO Connect to the equality checker here, and return the answer
+
+        return new QuestionValidationResponse(symbolicQuestion.get_id(), answer, symbolicAnswer.isCorrect(),
+                null, new Date());
+    }
+
+    /**
+     * @param trustedAnswer
+     *            - the answer given in the content
+     * @param answer
+     *            - the answer given by the student
+     * @return - a boolean describing correctness
+     */
+    private boolean checkSymbolicEquality(final String trustedAnswer, final String answer) {
+        log.info(String.format("Checking with equality checker..."));
+        return false;
+
+    }
+
+}
