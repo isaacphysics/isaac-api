e19e61ff749e89ebb0ff85ed0f0c449b5863be72
Ian Davies <git@iandavies.org>
2016-03-04 12:32:02 +0000

    Add build and run scripts.

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t api-$1 .
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 set -e
-docker rm -f api-live-$1
+docker rm -f api-live-$1 || true
 docker create --name api-live-$1 --net isaac -v /local/data/conf:/local/data/rutherford/conf -v /local/data/rutherford-content:/local/data/rutherford/git-contentstore/rutherford-content -v /local/data/keys:/local/data/rutherford/keys isaac-api-$1
 docker network connect content-live api-live-$1
 docker network connect db-live api-live-$1
