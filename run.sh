1d2ad3846bb57cf6a53571705845c9e67dae2036
Ian Davies <git@iandavies.org>
2016-03-11 17:53:39 +0000

    Clean up

--- run.sh
+++
@@ -1,7 +0,0 @@
-set -e
-docker rm -f api-live-$1 || true
-docker create --name api-live-$1 --net isaac -v /local/data/conf:/local/data/rutherford/conf -v /local/data/rutherford-content:/local/data/rutherford/git-contentstore/rutherford-content -v /local/data/keys:/local/data/rutherford/keys isaac-api-$1
-docker network connect content-live api-live-$1
-docker network connect db-live api-live-$1
-docker start api-live-$1
-docker attach api-live-$1
