0adfd982fa84e01bb8040f881e44cc313efd9891
Ian Davies <git@iandavies.org>
2016-03-23 11:46:35 +0000

    Use named volumes instead of data containers

--- src/main/resources/db_scripts/create-pgdata-container.sh
+++
@@ -1,1 +0,0 @@
-docker create -v pgdata-$1:/pgdata --name pgstore-$1 ubuntu /bin/true
--- src/main/resources/db_scripts/run-pg-test.sh
+++
@@ -1,3 +0,0 @@
-# Just like run-pg, only don't pull in data volume, so data will remain local. Removing this container will nuke the data too.
-docker rm -f pg-test
-docker run --net isaac --name pg-test -e POSTGRES_PASSWORD=secret -e POSTGRES_USER=rutherford isaac-pg
