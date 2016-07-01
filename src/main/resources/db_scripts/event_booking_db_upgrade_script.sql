33095568465caf11ec3d6caccf216afad02a3a55
Stephen Cummins <Stephen Cummins>
2016-07-01 16:01:30 +0100

    Added new index to event_booking upgrade script

--- src/main/resources/db_scripts/event_booking_db_upgrade_script.sql
+++ src/main/resources/db_scripts/event_booking_db_upgrade_script.sql
@@ -3,3 +3,8 @@ ALTER TABLE event_bookings
 
 ALTER TABLE event_bookings
    ADD COLUMN updated timestamp without time zone;
+
+CREATE UNIQUE INDEX event_booking_user_event_id_index
+  ON event_bookings
+  USING btree
+  (event_id COLLATE pg_catalog."default", user_id);
