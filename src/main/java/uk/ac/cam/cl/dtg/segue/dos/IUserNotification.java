aca6b8ba760fb868aed5ba6e6cf0608b390991c8
James Sharkey <jsharkey13@users.noreply.github.com>
2017-05-17 15:40:03 +0100

    For backwards compatibility, leave DISMISSED as a valid notification status

--- src/main/java/uk/ac/cam/cl/dtg/segue/dos/IUserNotification.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dos/IUserNotification.java
@@ -27,7 +27,9 @@ public interface IUserNotification {
      * Represents the status of a notification.
      */
     public enum NotificationStatus {
-        ACKNOWLEDGED, POSTPONED, DISABLED
+        // DISMISSED is left in for backwards compatibility; we now use the more obvious ACKNOWLEDGE for when a user
+        // actually interacts positively with the notification.
+        ACKNOWLEDGED, POSTPONED, DISABLED, DISMISSED
     }
 
     /**
