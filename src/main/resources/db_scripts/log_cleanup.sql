b424036d11d7f219323dcd1e05d1cd15aa4df949
James Sharkey <jsharkey13@users.noreply.github.com>
2017-05-30 12:33:46 +0100

    Modify log cleanup database script to safely ignore logs in new format

--- src/main/resources/db_scripts/log_cleanup.sql
+++ src/main/resources/db_scripts/log_cleanup.sql
@@ -6,8 +6,8 @@
 /*
   Clean up non-JSON objects in the logs:
 */
-UPDATE logged_events SET event_details=concat('{"message": ', event_details::text, '}')::jsonb, event_details_type='java.util.LinkedHashMap' WHERE event_type='CONTACT_US_FORM_USED';
-UPDATE logged_events SET event_details=concat('{"gameboardId": ', event_details, '}')::jsonb, event_details_type='java.util.LinkedHashMap' WHERE event_type='ADD_BOARD_TO_PROFILE';
+UPDATE logged_events SET event_details=concat('{"message": ', event_details::text, '}')::jsonb, event_details_type='java.util.LinkedHashMap' WHERE event_type='CONTACT_US_FORM_USED' AND event_details::TEXT NOT LIKE '{%';
+UPDATE logged_events SET event_details=concat('{"gameboardId": ', event_details, '}')::jsonb, event_details_type='java.util.LinkedHashMap' WHERE event_type='ADD_BOARD_TO_PROFILE' AND event_details::TEXT NOT LIKE '{%';
 UPDATE logged_events SET event_details='{}', event_details_type='java.util.HashMap' WHERE event_type='VIEW_MY_ASSIGNMENTS' AND event_details::text='null';
 
 /*
