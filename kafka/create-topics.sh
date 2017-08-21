f3548f2c255c4cf63f833e53f4fa637f86132714
Dan Underwood <du220@cam.ac.uk>
2017-08-21 15:22:25 +0100

    scripts for creating kafka topics on kafka instance startup

---
+++ kafka/create-topics.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+topics=("topic_logged_events" "topic_anonymous_logged_events")
+
+if [[ -z "$START_TIMEOUT" ]]; then
+    START_TIMEOUT=30
+fi
+
+start_timeout_exceeded=false
+count=0
+step=10
+
+while true; do
+
+	stat=$(ss -lnt | awk '$4 ~ /:9092$/')
+	if [ ${#stat} -gt 0 ]; then
+		break
+	fi
+	
+    echo "waiting for kafka to be ready"
+    sleep $step;
+    count=$(expr $count + $step)
+    if [ $count -gt $START_TIMEOUT ]; then
+        start_timeout_exceeded=true
+        break
+    fi
+done
+
+if $start_timeout_exceeded; then
+    echo "Not able to auto-create topic (waited for $START_TIMEOUT sec)"
+    exit 1
+fi
+
+for ((i=0; i < ${#topics[@]}; i++))
+do
+	kafka-topics.sh --create --zookeeper zookeeper:2181 --topic ${topics[$i]} --partitions 1 --replication-factor 1
+done
--- kafka/run-kafka.sh
+++ kafka/run-kafka.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Create topics here
+/create-topics.sh &
 
 
 # Run kafka
-/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties"
+/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
