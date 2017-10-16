41d4766628f13797b89a0e57ed45b67f1b1ffc58
Ian Davies <git@iandavies.org>
2017-10-16 13:27:05 +0100

    Make Kafka cope with transferring anonymous log events.

--- src/main/java/uk/ac/cam/cl/dtg/segue/dao/KafkaLoggingManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dao/KafkaLoggingManager.java
@@ -57,7 +57,7 @@ import javax.servlet.http.HttpServletRequest;
  * This class implements the logging event handler interface to listen to log events, and publishes them to a kafka topic
  */
 public class KafkaLoggingManager extends LoggingEventHandler {
-    private static final Logger log = LoggerFactory.getLogger(PgLogManager.class);
+    private static final Logger log = LoggerFactory.getLogger(KafkaLoggingManager.class);
 
     private KafkaStreamsProducer kafkaProducer;
     private LocationManager locationManager;
@@ -116,26 +116,34 @@ public class KafkaLoggingManager extends LoggingEventHandler {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, oldUserId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        KafkaConsumer<String, JsonNode> loggedEventsConsumer = new KafkaConsumer<String, JsonNode>(props);
         ArrayList<String> topics = Lists.newArrayList();
         topics.add("topic_anonymous_logged_events_test");
 
-        try {
+        try (KafkaConsumer<String, JsonNode> loggedEventsConsumer = new KafkaConsumer<String, JsonNode>(props)) {
             loggedEventsConsumer.subscribe(topics);
 
             Boolean running = true;
+            Long now = new Date().getTime();
 
             while (running) {
                 log.info(String.format("Kafka Test Transfer Log Events - OldUser (%s) NewUser (%s) - About to poll", oldUserId, newUserId));
-
                 ConsumerRecords<String, JsonNode> records = loggedEventsConsumer.poll(1000);
-
                 log.info(String.format("Kafka Test Transfer Log Events - OldUser (%s) NewUser (%s) - Poll Complete (%s records)", oldUserId, newUserId, records.count()));
 
+                if (records.count() == 0) {
+                    running = false;
+                    log.info("Kafka Test Transfer Log Events - returned no records. Exiting.");
+                }
+
                 for (ConsumerRecord<String, JsonNode> record : records) {
-                    Map<String, Object> data = new HashMap<>();
+                    Long ts = record.value().path("timestamp").asLong();
 
                     if (record.value().path("user_id").asText().equals(oldUserId)) {
+
+                        if (now < ts) {
+                            running = false;
+                            log.info("Kafka Test Transfer Log Events - reached current time. Exiting.");
+                        }
 
                         Map<String, Object> kafkaLogRecord = new ImmutableMap.Builder<String, Object>()
                                 .put("user_id", newUserId)
@@ -144,25 +152,22 @@ public class KafkaLoggingManager extends LoggingEventHandler {
                                 .put("event_details_type", record.value().path("event_details_type").asText())
                                 .put("event_details", record.value().path("event_details"))
                                 .put("ip_address", (record.value().path("ip_address").asText() != null) ? record.value().path("ip_address").asText() : "")
-                                .put("timestamp", record.value().path("timestamp").asText())
+                                .put("timestamp", ts.toString())
                                 .build();
 
                         // producerRecord contains the name of the kafka topic we are publishing to, followed by the message to be sent.
-                        ProducerRecord producerRecord = new ProducerRecord<String, String>("topic_logged_events_test", newUserId,
+                        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("topic_logged_events_test", newUserId,
                                 objectMapper.writeValueAsString(kafkaLogRecord));
 
                         kafkaProducer.send(producerRecord);
                     }
                 }
-                running = false;
             }
 
         } catch (KafkaException e) {
             e.printStackTrace();
         } catch (JsonProcessingException e) {
             log.error("Could not process Json document: " + e);
-        } finally {
-            loggedEventsConsumer.close();
         }
 
     }
