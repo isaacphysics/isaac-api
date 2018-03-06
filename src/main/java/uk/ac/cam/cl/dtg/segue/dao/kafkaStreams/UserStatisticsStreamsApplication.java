53862214637bb50c120cbd590e032ca236a102b4
Dan Underwood <du220@cam.ac.uk>
2018-03-06 14:32:22 +0000

    Test version of user streams app to process user stats from the beggining of time

--- src/main/java/uk/ac/cam/cl/dtg/segue/dao/kafkaStreams/UserStatisticsStreamsApplication.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dao/kafkaStreams/UserStatisticsStreamsApplication.java
@@ -32,6 +32,7 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.connect.json.JsonDeserializer;
 import org.apache.kafka.connect.json.JsonSerializer;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
@@ -88,7 +89,7 @@ public class UserStatisticsStreamsApplication {
 
 
     private final String streamsAppName = "streamsapp_user_stats";
-    private final String streamsAppVersion = "v1.2";
+    private final String streamsAppVersion = "v1.2-060318-test-from-beginning";
 
 
     /**
@@ -202,9 +203,14 @@ public class UserStatisticsStreamsApplication {
      */
     private void streamProcess(KStream<String, JsonNode> rawStream) {
 
+        // map the key-value pair to one where the key is always the user id
+        KStream<String, JsonNode> mappedStream = rawStream
+                .map(
+                        (k, v) -> new KeyValue<>(v.path("user_id").asText(), v)
+                );
 
         // user question answer streaks
-        rawStream.groupByKey()
+        mappedStream.groupByKey(StringSerde, JsonSerde)
                 .aggregate(
                         // initializer
                         () -> {
