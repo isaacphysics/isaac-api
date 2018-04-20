81b98faf2f09d8cc60ac0c31a3c41875176303f8
James Sharkey <jsharkey13@users.noreply.github.com>
2018-04-20 16:27:07 +0100

    Bring database scripts up-to-date and add new relations for streaks
    # This change moves the functions into a separate file, and updates the
    # schema to match that of the current live database.
    # It also adds the new tables and functions required for streaks to work,
    # which could not easily be a separate commit.

---
+++ src/main/resources/db_scripts/user_streaks_migration.sql
@@ -0,0 +1,40 @@
+﻿-- Migration script to add streak tables:
+
+CREATE TABLE user_streak_freezes (
+    user_id bigint NOT NULL,
+    start_date date NOT NULL,
+    end_date date,
+    comment text
+);
+ALTER TABLE user_streak_freezes OWNER TO rutherford;
+
+
+CREATE TABLE user_streak_targets (
+    user_id bigint NOT NULL,
+    target_count integer NOT NULL,
+    start_date date NOT NULL,
+    end_date date,
+    comment text
+);
+ALTER TABLE user_streak_targets OWNER TO rutherford;
+
+ALTER TABLE ONLY user_streak_freezes
+    ADD CONSTRAINT user_streak_freeze_pkey PRIMARY KEY (user_id, start_date);
+
+ALTER TABLE ONLY user_streak_targets
+    ADD CONSTRAINT user_streak_targets_pkey PRIMARY KEY (user_id, start_date);
+
+
+ALTER TABLE ONLY user_streak_freezes
+    ADD CONSTRAINT user_streak_freezes_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY user_streak_targets
+    ADD CONSTRAINT user_streak_targets_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+
+
+CREATE INDEX user_streak_freezes_by_user_id ON user_streak_freezes USING btree (user_id);
+
+CREATE INDEX user_streak_targets_by_user_id ON user_streak_targets USING btree (user_id);
+
+
+-- Then run postgres-rutherford-functions.sql to get the required functions!
