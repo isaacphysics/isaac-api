4e14770a82ea86f8af61c5fcc957f9f48aeeb819
James Sharkey <jsharkey13@users.noreply.github.com>
2018-10-12 16:51:17 +0100

    Add session token database change to schema

--- src/main/resources/db_scripts/postgres-rutherford-create-script.sql
+++ src/main/resources/db_scripts/postgres-rutherford-create-script.sql
@@ -538,7 +538,8 @@ CREATE TABLE users (
     last_seen timestamp without time zone,
     default_level integer,
     email_verification_token text,
-    email_to_verify text
+    email_to_verify text,
+    session_token integer NOT NULL DEFAULT 0
 );
 
 
