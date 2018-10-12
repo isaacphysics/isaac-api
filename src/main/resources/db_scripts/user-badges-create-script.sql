497de964d8f2572e81d2429a72ef9e57bc65fee0
James Sharkey <jsharkey13@users.noreply.github.com>
2018-10-12 12:37:45 +0100

    Tidy unused and out-of-date files and documentation
    # This removes the incorrect information in the README,
    # and assorted outdated files.

--- src/main/resources/db_scripts/user-badges-create-script.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_badges (
-    user_id integer,
-    badge text,
-    state jsonb
-);
-
-CREATE UNIQUE INDEX user_badges_user_id_badge_unique ON user_badges USING btree (user_id, badge);
