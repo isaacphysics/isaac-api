66ec580f59533dec7442f162361e5eb5c9e3072e
James Sharkey <jsharkey13@users.noreply.github.com>
2018-11-12 11:56:14 +0000

    Ensure migration script correctly sets default values for existing rows
    # The statements before worked fine for a new database, but not for one
    # with data in it already! They left all of the existing values as NULL.

--- src/main/resources/db_scripts/group_status_upgrade_script.sql
+++ src/main/resources/db_scripts/group_status_upgrade_script.sql
@@ -2,20 +2,17 @@
 
 -- ALTER TABLE public.group_memberships DROP COLUMN updated;
 
-ALTER TABLE public.group_memberships ADD COLUMN updated timestamp with time zone;
-ALTER TABLE public.group_memberships ALTER COLUMN updated SET DEFAULT now();
+ALTER TABLE public.group_memberships ADD COLUMN updated timestamp with time zone DEFAULT now();
 
 -- Column: status
 
 -- ALTER TABLE public.group_memberships DROP COLUMN status;
 
-ALTER TABLE public.group_memberships ADD COLUMN status text;
-ALTER TABLE public.group_memberships ALTER COLUMN status SET DEFAULT 'ACTIVE'::text;
+ALTER TABLE public.group_memberships ADD COLUMN status text DEFAULT 'ACTIVE'::text;
 
 
 -- Column: status
 
 -- ALTER TABLE public.groups DROP COLUMN group_status;
 
-ALTER TABLE public.groups ADD COLUMN group_status text;
-ALTER TABLE public.groups ALTER COLUMN group_status SET DEFAULT 'ACTIVE'::text;
+ALTER TABLE public.groups ADD COLUMN group_status text DEFAULT 'ACTIVE'::text;
