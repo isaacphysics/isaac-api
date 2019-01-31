1fd6d862088c6a58c54d9c4ea022fa11367515a5
Meurig Thomas <mlt47@cam.ac.uk>
2019-01-31 09:15:39 +0000

    Add a not boolean operator to elasticsearch so that we can ignore nofilter questions

--- src/main/java/uk/ac/cam/cl/dtg/segue/api/Constants.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/Constants.java
@@ -178,7 +178,7 @@ public final class Constants {
      * 
      */
     public enum BooleanOperator {
-        AND, OR
+        AND, OR, NOT
     };
 
     public static final String SCHOOLS_INDEX_BASE = "schools";
