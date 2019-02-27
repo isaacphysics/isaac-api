a7efa729327e892622b82b66a95aecb325cd6b8c
James Sharkey <jsharkey13@users.noreply.github.com>
2019-02-27 17:54:10 +0000

    Only show open schools when searching by school name
    # Continue to provide school information for closed schools when looked up
    # by URN.

--- src/main/java/uk/ac/cam/cl/dtg/segue/dao/schools/SchoolListReader.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dao/schools/SchoolListReader.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2014 Stephen Cummins
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,6 +19,8 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.util.Lists;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +29,7 @@ import uk.ac.cam.cl.dtg.segue.search.ISearchProvider;
 import uk.ac.cam.cl.dtg.segue.search.SegueSearchException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static uk.ac.cam.cl.dtg.segue.api.Constants.DEFAULT_RESULTS_LIMIT;
@@ -84,7 +87,7 @@ public class SchoolListReader {
         }
 
         List<String> schoolSearchResults = searchProvider.fuzzySearch(SCHOOLS_INDEX_BASE, SCHOOLS_INDEX_TYPE.SCHOOL_SEARCH.toString(),
-                searchQuery, 0, DEFAULT_RESULTS_LIMIT, null, null, SCHOOL_URN_FIELDNAME_POJO,
+                searchQuery, 0, DEFAULT_RESULTS_LIMIT, ImmutableMap.of("closed", ImmutableList.of("false")), null, SCHOOL_URN_FIELDNAME_POJO,
                 SCHOOL_ESTABLISHMENT_NAME_FIELDNAME_POJO, SCHOOL_POSTCODE_FIELDNAME_POJO)
                 .getResults();
 
