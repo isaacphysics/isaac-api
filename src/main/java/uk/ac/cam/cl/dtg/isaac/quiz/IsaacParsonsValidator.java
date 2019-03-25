7b700b26a963a513c8fd54ca424e6d773d51c1d0
James Sharkey <jsharkey13@users.noreply.github.com>
2019-03-25 15:46:43 +0000

    Allow ParsonsChoices to match if everything matches
    # This is clearly not the final marking strategy, but is enough to show
    # that everything is working.

--- src/main/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacParsonsValidator.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacParsonsValidator.java
@@ -23,10 +23,13 @@ import uk.ac.cam.cl.dtg.segue.dos.QuestionValidationResponse;
 import uk.ac.cam.cl.dtg.segue.dos.content.Choice;
 import uk.ac.cam.cl.dtg.segue.dos.content.Content;
 import uk.ac.cam.cl.dtg.segue.dos.content.ParsonsChoice;
+import uk.ac.cam.cl.dtg.segue.dos.content.ParsonsItem;
 import uk.ac.cam.cl.dtg.segue.dos.content.Question;
 import uk.ac.cam.cl.dtg.segue.quiz.IValidator;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Validator that only provides functionality to validate Parsons questions.
@@ -41,17 +44,18 @@ public class IsaacParsonsValidator implements IValidator {
 
         if (!(question instanceof IsaacParsonsQuestion)) {
             throw new IllegalArgumentException(String.format(
-                    "This validator only works with Isaac String Match Questions... (%s is not string match)",
+                    "This validator only works with Isaac Parsons Questions... (%s is not Parsons)",
                     question.getId()));
         }
 
         if (!(answer instanceof ParsonsChoice)) {
             throw new IllegalArgumentException(String.format(
-                    "Expected StringChoice for IsaacParsonsQuestion: %s. Received (%s) ", question.getId(),
+                    "Expected ParsonsChoice for IsaacParsonsQuestion: %s. Received (%s) ", question.getId(),
                     answer.getClass()));
         }
 
         IsaacParsonsQuestion parsonsQuestion = (IsaacParsonsQuestion) question;
+        ParsonsChoice submittedChoice = (ParsonsChoice) answer;
 
         // These variables store the important features of the response we'll send.
         Content feedback = null;                        // The feedback we send the user
@@ -66,15 +70,48 @@ public class IsaacParsonsValidator implements IValidator {
 
         // STEP 1: Did they provide an answer at all?
 
-        if (null == feedback && (null == answer.getValue() || answer.getValue().isEmpty())) {
+        if (null == feedback && (null == submittedChoice.getItems() || submittedChoice.getItems().isEmpty())) {
             feedback = new Content("You did not provide an answer");
         }
 
         // STEP 2: If they did, does their answer match a known answer?
 
         if (null == feedback) {
-            responseCorrect = false;
-            feedback = new Content("It works!");
+            // For all the choices on this question...
+            for (Choice c : parsonsQuestion.getChoices()) {
+
+                // ... that are of the Formula type, ...
+                if (!(c instanceof ParsonsChoice)) {
+                    log.error("Validator for questionId: " + parsonsQuestion.getId()
+                            + " expected there to be a Formula. Instead it found a Choice.");
+                    continue;
+                }
+
+                ParsonsChoice parsonsChoice = (ParsonsChoice) c;
+
+                // ... and that have a python expression ...
+                if (null == parsonsChoice.getItems() || parsonsChoice.getItems().isEmpty()) {
+                    log.error("Expected list of ParsonsItems, but none found in choice for question id: "
+                            + parsonsQuestion.getId());
+                    continue;
+                }
+
+                // ... look for a match to the submitted answer.
+                if (parsonsChoice.getItems().size() != submittedChoice.getItems().size()) {
+                    continue;
+                }
+                List<String> choiceItemIds = parsonsChoice.getItems().stream().map(ParsonsItem::getId).collect(Collectors.toList());
+                List<String> submittedItemIds = submittedChoice.getItems().stream().map(ParsonsItem::getId).collect(Collectors.toList());
+
+                List<Integer> choiceItemIndentations = parsonsChoice.getItems().stream().map(ParsonsItem::getIndentation).collect(Collectors.toList());
+                List<Integer> submittedItemIndentations = submittedChoice.getItems().stream().map(ParsonsItem::getIndentation).collect(Collectors.toList());
+
+                if (choiceItemIds.equals(submittedItemIds) && choiceItemIndentations.equals(submittedItemIndentations)) {
+                    responseCorrect = parsonsChoice.isCorrect();
+                    feedback = (Content) parsonsChoice.getExplanation();
+                    break;
+                }
+            }
         }
 
         return new QuestionValidationResponse(question.getId(), answer, responseCorrect, feedback, new Date());
