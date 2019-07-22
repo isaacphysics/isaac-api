4d0b23a5c42d4f189ff5321fb5f8eaa327c8fd57
James Sharkey <jsharkey13@users.noreply.github.com>
2019-07-22 17:15:09 +0100

    Update ItemQuestionValidator to better cope with bad input

--- src/main/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacItemQuestionValidator.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacItemQuestionValidator.java
@@ -28,6 +28,7 @@ import uk.ac.cam.cl.dtg.segue.dos.content.Question;
 import uk.ac.cam.cl.dtg.segue.quiz.IValidator;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -44,45 +45,65 @@ public class IsaacItemQuestionValidator implements IValidator {
 
         if (!(question instanceof IsaacItemQuestion)) {
             throw new IllegalArgumentException(String.format(
-                    "This validator only works with IsaacItemQuestions... (%s is not ItemQuestion)", question.getId()));
+                    "This validator only works with IsaacItemQuestions (%s is not ItemQuestion)", question.getId()));
         }
 
         if (!(answer instanceof ItemChoice)) {
             throw new IllegalArgumentException(String.format(
-                    "Expected ItemChoice for IsaacItemQuestion: %s. Received (%s) ", question.getId(),
-                    answer.getClass()));
+                    "Expected ItemChoice for IsaacItemQuestion: %s. Received (%s) ", question.getId(), answer.getClass()));
         }
-
-        IsaacItemQuestion itemQuestion = (IsaacItemQuestion) question;
-        ItemChoice submittedChoice = (ItemChoice) answer;
 
         // These variables store the important features of the response we'll send.
         Content feedback = null;                        // The feedback we send the user
         boolean responseCorrect = false;                // Whether we're right or wrong
 
+        IsaacItemQuestion itemQuestion = (IsaacItemQuestion) question;
+        ItemChoice submittedChoice = (ItemChoice) answer;
+
+        // STEP 0: Is it even possible to answer this question?
+
         if (null == itemQuestion.getChoices() || itemQuestion.getChoices().isEmpty()) {
             log.error("Question does not have any answers. " + question.getId() + " src: "
                     + question.getCanonicalSourceFile());
-
-            feedback = new Content("This question does not have any correct answers");
+            feedback = new Content("This question does not have any correct answers!");
         }
 
-        // STEP 1: Did they provide an answer at all?
+        if (null == itemQuestion.getItems() || itemQuestion.getItems().isEmpty()) {
+            log.error("ItemQuestion does not have any items. " + question.getId() + " src: "
+                    + question.getCanonicalSourceFile());
+            feedback = new Content("This question does not have any items to choose from!");
+        }
+
+        // STEP 1: Did they provide a valid answer?
 
         if (null == feedback && (null == submittedChoice.getItems() || submittedChoice.getItems().isEmpty())) {
             feedback = new Content("You did not provide an answer");
         }
 
+        Set<String> submittedItemIds = null;
+        Set<String> allowedItemIds;
+        if (null != submittedChoice.getItems() && null != itemQuestion.getItems()) {
+            submittedItemIds = submittedChoice.getItems().stream().map(Item::getId).collect(Collectors.toSet());
+            allowedItemIds = itemQuestion.getItems().stream().map(Item::getId).collect(Collectors.toSet());
+            if (!allowedItemIds.containsAll(submittedItemIds)) {
+                feedback = new Content("You did not provide a valid answer; it contained unrecognised items");
+            }
+        }
+
         // STEP 2: If they did, does their answer match a known answer?
 
-        if (null == feedback) {
+        if (null == feedback && null != submittedItemIds) {
+            // Sort the choices so that we match incorrect choices last, taking precedence over correct ones.
+            List<Choice> orderedChoices = getOrderedChoices(itemQuestion.getChoices());
+
             // For all the choices on this question...
-            for (Choice c : itemQuestion.getChoices()) {
+            for (Choice c : orderedChoices) {
 
                 // ... that are ItemChoices, ...
                 if (!(c instanceof ItemChoice)) {
-                    log.error("Validator for questionId: " + itemQuestion.getId()
-                            + " expected there to be an ItemChoice. Instead it found a Choice.");
+                    log.error(String.format(
+                            "Validator for question (%s) expected there to be an ItemChoice. Instead it found a %s.",
+                            itemQuestion.getId(), c.getClass().toString()));
                     continue;
                 }
 
@@ -90,18 +111,18 @@ public class IsaacItemQuestionValidator implements IValidator {
 
                 // ... and that have items ...
                 if (null == itemChoice.getItems() || itemChoice.getItems().isEmpty()) {
-                    log.error("Missing Item list in choice for question id: " + itemQuestion.getId());
+                    log.error(String.format("Missing Item list in choice for question id (%s)!", itemQuestion.getId()));
                     continue;
                 }
 
-                // ... look for a match to the submitted answer.
+                // ... look for a match to the submitted answer:
                 if (itemChoice.getItems().size() != submittedChoice.getItems().size()) {
                     continue;
                 }
                 Set<String> choiceItemIds = itemChoice.getItems().stream().map(Item::getId).collect(Collectors.toSet());
-                Set<String> submittedItemIds = submittedChoice.getItems().stream().map(Item::getId).collect(Collectors.toSet());
 
                 if (choiceItemIds.equals(submittedItemIds)) {
+                    // This is safe from duplication issues, since the list lengths are the same too.
                     responseCorrect = itemChoice.isCorrect();
                     feedback = (Content) itemChoice.getExplanation();
                     break;
