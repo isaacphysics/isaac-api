7185d048174694c642ea6b845d0c2efb8d8c2fc1
Stephen Cummins <sac92@cam.ac.uk>
2019-09-13 16:41:00 +0100

    Add more ways of telling isaac-api where to look for config files as we need to move to real jetty in docker.

--- src/main/java/uk/ac/cam/cl/dtg/segue/configuration/SegueGuiceConfigurationModule.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/configuration/SegueGuiceConfigurationModule.java
@@ -28,6 +28,7 @@ import com.google.inject.multibindings.MapBinder;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 import ma.glasnost.orika.MapperFacade;
+import org.apache.commons.lang3.SystemUtils;
 import org.elasticsearch.client.Client;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -101,6 +102,7 @@ import uk.ac.cam.cl.dtg.util.locations.PostCodeLocationResolver;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.sql.SQLException;
@@ -150,8 +152,22 @@ public class SegueGuiceConfigurationModule extends AbstractModule implements Ser
      */
     public SegueGuiceConfigurationModule() {
         if (globalProperties == null) {
+            // check the following places to determine where config file location may be.
+            // 1) system env variable, 2) java param (system property), 3) use a default from the constant file.
+            String configLocation = SystemUtils.IS_OS_LINUX ? DEFAULT_LINUX_CONFIG_LOCATION : null;
+            if (System.getProperty("config.location") != null) {
+                configLocation = System.getProperty("config.location");
+            }
+            if (System.getenv("SEGUE_CONFIG_LOCATION") != null){
+                configLocation = System.getenv("SEGUE_CONFIG_LOCATION");
+            }
+
             try {
-                globalProperties = new PropertiesLoader(System.getProperty("config.location"));
+                if (null == configLocation) {
+                    throw new FileNotFoundException("Segue configuration location not specified, please provide it as either a java system property (config.locatioin) or environment variable SEGUE_CONFIG_LOCATION");
+                }
+
+                globalProperties = new PropertiesLoader(configLocation);
             } catch (IOException e) {
                 log.error("Error loading properties file.", e);
             }
