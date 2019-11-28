2e96007a1e4a9b3cdd4e24f9c3bcd27591997e49
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-11-28 14:54:31 +0000

    Oops, RESERVED goes in the BookingStatus, not the EventStatus

--- src/main/java/uk/ac/cam/cl/dtg/isaac/dos/eventbookings/BookingStatus.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/dos/eventbookings/BookingStatus.java
@@ -4,5 +4,5 @@ package uk.ac.cam.cl.dtg.isaac.dos.eventbookings;
  * Enum to represent the different possible statuses for an event booking.
  */
 public enum BookingStatus {
-	CONFIRMED, CANCELLED, WAITING_LIST, ATTENDED, ABSENT
+	CONFIRMED, CANCELLED, WAITING_LIST, ATTENDED, ABSENT, RESERVED
 }
