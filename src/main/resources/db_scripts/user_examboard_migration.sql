b58a0d7dcc76ac93717eac3a644f0bb4777f82b8
Meurig Thomas <mlt47@cam.ac.uk>
2020-01-14 13:21:32 +0000

    Remove unnecessary clause in migration script

--- src/main/resources/db_scripts/user_examboard_migration.sql
+++ src/main/resources/db_scripts/user_examboard_migration.sql
@@ -2,4 +2,4 @@
 ALTER TABLE users ADD COLUMN exam_board TEXT;
 
 UPDATE users SET exam_board=preference_name FROM user_preferences
-WHERE user_preferences.user_id=users.id AND preference_type='EXAM_BOARD' AND preference_value AND user_preferences.last_updated;
+WHERE user_preferences.user_id=users.id AND preference_type='EXAM_BOARD' AND preference_value;
