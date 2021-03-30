1729df5b973b53fa68137bf88e68cff0253252c1
Ben Hanson <bh412@cam.ac.uk>
2021-03-30 15:03:20 +0100

    Alter how code snippet works

---
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dos/content/CodeSnippet.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2021 Ben Hanson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.cam.cl.dtg.segue.dos.content;
+
+import uk.ac.cam.cl.dtg.segue.dto.content.CodeSnippetDTO;
+
+/**
+ * Code snippet object
+ *
+ */
+@DTOMapping(CodeSnippetDTO.class)
+@JsonContentType("codeSnippet")
+public class CodeSnippet extends Content {
+    protected String language;
+    protected String code;
+    protected boolean disableHighlighting;
+    protected String url;
+
+    /**
+     * Default constructor, required for mappers.
+     */
+    public CodeSnippet() {
+
+    }
+
+    /**
+     * Gets the language.
+     *
+     * @return the language
+     */
+    public final String getLanguage() {
+        return language;
+    }
+
+    /**
+     * Sets the language.
+     *
+     * @param language
+     *            the language to set
+     */
+    public final void setLanguage(final String language) {
+        this.language = language;
+    }
+
+    public final String getCode() {
+        return this.code;
+    }
+
+    public final void setCode(final String code) {
+        this.code = code;
+    }
+
+    public final boolean getDisableHighlighting() { return disableHighlighting; }
+    public final void setDisableHighlighting(final boolean disableHighlighting) { this.disableHighlighting = disableHighlighting; }
+
+    public final String getUrl() {
+        return this.url;
+    }
+
+    public final void setUrl(final String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dto/content/CodeSnippetDTO.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2021 Ben Hanson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.cam.cl.dtg.segue.dto.content;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Glossary Term object The glossary term object is a specialized form of content and allows the storage of data
+ * relating to glossary terms.
+ *
+ */
+public class CodeSnippetDTO extends ContentDTO {
+    protected String language;
+    protected String code;
+    protected boolean disableHighlighting;
+    protected String url;
+
+    @JsonCreator
+    public CodeSnippetDTO(@JsonProperty("language") String language, @JsonProperty("code") String code,
+                          @JsonProperty("disableHighlighting") Boolean disableHighlighting, @JsonProperty("url") String url) {
+        this.language = language;
+        this.code = code;
+        this.disableHighlighting = disableHighlighting;
+        this.url = url;
+    }
+
+    public String getLanguage() {
+        return this.language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public boolean getDisableHighlighting() { return disableHighlighting; }
+    public void setDisableHighlighting(boolean disableHighlighting) { this.disableHighlighting = disableHighlighting; }
+
+    public String getUrl() { return this.url; }
+
+    public void setUrl(String url) { this.url = url; }
+}
+
