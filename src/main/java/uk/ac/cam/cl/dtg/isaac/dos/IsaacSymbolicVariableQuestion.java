4a9eb787f228e72e05486f4244fced91f85fed9d
Ben Hanson <bh412@cam.ac.uk>
2021-07-20 09:09:09 +0100

    Add enumerated variables to dto

--- src/main/java/uk/ac/cam/cl/dtg/isaac/dos/IsaacSymbolicVariableQuestion.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/dos/IsaacSymbolicVariableQuestion.java
@@ -1,19 +1,29 @@
 package uk.ac.cam.cl.dtg.isaac.dos;
 
+import org.json.JSONObject;
 import uk.ac.cam.cl.dtg.isaac.dto.IsaacSymbolicVariableQuestionDTO;
 import uk.ac.cam.cl.dtg.isaac.quiz.IsaacSymbolicVariableValidator;
 import uk.ac.cam.cl.dtg.segue.dos.content.DTOMapping;
 import uk.ac.cam.cl.dtg.segue.dos.content.JsonContentType;
 import uk.ac.cam.cl.dtg.segue.quiz.ValidatesWith;
 
+import java.util.HashMap;
+
 @DTOMapping(IsaacSymbolicVariableQuestionDTO.class)
 @JsonContentType("isaacSymbolicVariableQuestion")
 @ValidatesWith(IsaacSymbolicVariableValidator.class)
 public class IsaacSymbolicVariableQuestion extends IsaacSymbolicQuestion {
     private String variables;
+    private HashMap<String, Integer> enumeratedVariables;
 
     public final String getVariables() {return variables;}
 
     public void setVariables(String variables) {this.variables = variables;}
+
+    public final HashMap<String, Integer> getEnumeratedVariables() {return enumeratedVariables;}
+
+    public void setEnumeratedVariables(HashMap<String, Integer> enumeratedVariables) {
+        this.enumeratedVariables = enumeratedVariables;
+    }
 
 }
--- src/main/java/uk/ac/cam/cl/dtg/isaac/dto/IsaacSymbolicVariableQuestionDTO.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/dto/IsaacSymbolicVariableQuestionDTO.java
@@ -1,16 +1,25 @@
 package uk.ac.cam.cl.dtg.isaac.dto;
 
+import org.json.JSONObject;
 import uk.ac.cam.cl.dtg.isaac.quiz.IsaacSymbolicVariableValidator;
 import uk.ac.cam.cl.dtg.segue.dos.content.JsonContentType;
 import uk.ac.cam.cl.dtg.segue.quiz.ValidatesWith;
+
+import java.util.HashMap;
 
 @JsonContentType("isaacSymbolicVariableQuestion")
 @ValidatesWith(IsaacSymbolicVariableValidator.class)
 public class IsaacSymbolicVariableQuestionDTO extends IsaacSymbolicQuestionDTO {
     private String variables;
+    private HashMap<String, Integer> enumeratedVariables;
 
     public final String getVariables() {return variables;}
 
     public void setVariables(String variables) {this.variables = variables;}
 
+    public final HashMap<String, Integer> getEnumeratedVariables() {return enumeratedVariables;}
+
+    public void setEnumeratedVariables(HashMap<String, Integer> enumeratedVariables) {
+        this.enumeratedVariables = enumeratedVariables;
+    }
 }
