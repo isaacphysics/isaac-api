54ac1dd06eaa9b8b913e78c5a287c397089d73c5
Meurig Thomas <mlt47@cam.ac.uk>
2021-08-30 20:39:11 +0100

    Change enum values for displaying in query parameters

--- src/main/java/uk/ac/cam/cl/dtg/segue/dos/ExamBoard.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dos/ExamBoard.java
@@ -1,6 +1,6 @@
 package uk.ac.cam.cl.dtg.segue.dos;
 
 public enum ExamBoard {
-    aqa, ocr, cie, edexcel, eduqas, wjec, none
+    aqa, ocr, cie, edexcel, eduqas, wjec, all
     /* NOTE: lower case is necessary for correct JSON auto mapping */
 }
