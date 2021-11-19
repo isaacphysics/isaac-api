4246be638c48379e22d52e27a59113b6f07e2c55
James Sharkey <jsharkey13@users.noreply.github.com>
2021-11-19 11:19:42 +0000

    Remove unnecessary additional list wrapper

--- src/main/java/uk/ac/cam/cl/dtg/segue/configuration/SegueGuiceConfigurationModule.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/configuration/SegueGuiceConfigurationModule.java
@@ -925,7 +925,7 @@ public class SegueGuiceConfigurationModule extends AbstractModule implements Ser
                     "Sync users to mailjet",
                     "0 0 0/4 ? * * *");
 
-            List<SegueScheduledJob> configuredScheduledJobs = new ArrayList<>(Arrays.asList(PIISQLJob, cleanUpOldAnonymousUsers, cleanUpExpiredReservations));
+            List<SegueScheduledJob> configuredScheduledJobs = Arrays.asList(PIISQLJob, cleanUpOldAnonymousUsers, cleanUpExpiredReservations);
 
             if (mailjetKey != null && mailjetSecret != null) {
                 configuredScheduledJobs.add(syncMailjetUsers);
