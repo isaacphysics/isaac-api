4a4d23fd7200cc098a4916dc35e37b1710c9dd13
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2021-12-01 11:40:00 +0000

    Add DTO and DO for interactive code snippet content blocks

---
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dos/content/InteractiveCodeSnippet.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2021 Chris Purdy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.cam.cl.dtg.segue.dos.content;
+
+import uk.ac.cam.cl.dtg.segue.dto.content.InteractiveCodeSnippetDTO;
+
+/**
+ * Interactive code snippet is a code snippet that can be run and edited
+ *
+ */
+@DTOMapping(InteractiveCodeSnippetDTO.class)
+@JsonContentType("interactiveCodeSnippet")
+public class InteractiveCodeSnippet extends CodeSnippet {
+
+    protected String setupCode;
+    protected String testCode;
+    protected String expectedResult;
+
+    /**
+     * Default constructor, required for mappers.
+     */
+    public InteractiveCodeSnippet() {
+
+    }
+
+    public String getSetupCode() {
+        return setupCode;
+    }
+
+    public void setSetupCode(String setupCode) {
+        this.setupCode = setupCode;
+    }
+
+    public String getTestCode() {
+        return testCode;
+    }
+
+    public void setTestCode(String testCode) {
+        this.testCode = testCode;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public void setExpectedResult(String expectedResult) {
+        this.expectedResult = expectedResult;
+    }
+}
---
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dto/content/InteractiveCodeSnippetDTO.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2021 Chris Purdy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.cam.cl.dtg.segue.dto.content;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Interactive code snippet is a code snippet that can be run and edited
+ *
+ */
+public class InteractiveCodeSnippetDTO extends CodeSnippetDTO {
+
+    protected String setupCode;
+    protected String testCode;
+    protected String expectedResult;
+
+    @JsonCreator
+    public InteractiveCodeSnippetDTO(@JsonProperty("language") String language, @JsonProperty("code") String code,
+                                     @JsonProperty("disableHighlighting") Boolean disableHighlighting, @JsonProperty("url") String url,
+                                     @JsonProperty("setupCode") String setupCode, @JsonProperty("testCode") String testCode,
+                                     @JsonProperty("expectedResult") String expectedResult) {
+        super(language, code, disableHighlighting, url);
+        this.setupCode = setupCode;
+        this.testCode = testCode;
+        this.expectedResult = expectedResult;
+    }
+
+    public String getSetupCode() {
+        return setupCode;
+    }
+
+    public void setSetupCode(String setupCode) {
+        this.setupCode = setupCode;
+    }
+
+    public String getTestCode() {
+        return testCode;
+    }
+
+    public void setTestCode(String testCode) {
+        this.testCode = testCode;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public void setExpectedResult(String expectedResult) {
+        this.expectedResult = expectedResult;
+    }
+}
+
