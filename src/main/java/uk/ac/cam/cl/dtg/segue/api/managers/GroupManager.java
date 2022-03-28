cbb8ef27ebf2d726d3688ddae120091ecaa79f40
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-03-28 17:03:25 +0100

    Unused import

--- src/main/java/uk/ac/cam/cl/dtg/segue/api/managers/GroupManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/managers/GroupManager.java
@@ -58,7 +58,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Comparator;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * GroupManager. Responsible for managing group related logic.
