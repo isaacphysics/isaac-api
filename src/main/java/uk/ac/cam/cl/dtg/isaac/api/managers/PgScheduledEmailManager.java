7e4bce2bd7139ef10f01cc80a19f1cb7855f257d
Ben Hanson <bh412@cam.ac.uk>
2022-04-14 13:47:18 +0100

    Moved pg methods out into own manager

---
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/api/managers/PgScheduledEmailManager.java
@@ -0,0 +1,67 @@
+package uk.ac.cam.cl.dtg.isaac.api.managers;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.ac.cam.cl.dtg.isaac.dto.IsaacEventPageDTO;
+import uk.ac.cam.cl.dtg.segue.dao.SegueDatabaseException;
+import uk.ac.cam.cl.dtg.segue.database.PostgresSqlDb;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
+
+public class PgScheduledEmailManager {
+    private static final Logger log = LoggerFactory.getLogger(PgScheduledEmailManager.class);
+
+    private final PostgresSqlDb database;
+
+    /**
+     * Creates a new user data manager object.
+     *
+     * @param database
+     *            - the database reference used for persistence.
+     */
+    @Inject
+    public PgScheduledEmailManager(final PostgresSqlDb database) {
+        this.database = database;
+    }
+
+    public Boolean scheduledEmailAlreadySent(IsaacEventPageDTO event, String appendId) throws SegueDatabaseException {
+        String query = "SELECT EXISTS(SELECT 1 from scheduled_emails WHERE email_id = ?)";
+        try (Connection conn = database.getDatabaseConnection();
+             PreparedStatement pst = conn.prepareStatement(query);
+        ) {
+            pst.setString(1, event.getId() + appendId);
+            try (ResultSet results = pst.executeQuery()) {
+                if (results.next()) {
+                    return results.getBoolean(1);
+                } else {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public Void saveScheduledEmailSent(IsaacEventPageDTO event, String appendId) throws SegueDatabaseException {
+        ZonedDateTime now = ZonedDateTime.now();
+        String query = "INSERT INTO scheduled_emails(email_id, sent) VALUES (?, ?)"
+            + " ON CONFLICT (email_id) DO NOTHING";
+        try (Connection conn = database.getDatabaseConnection();
+             PreparedStatement pst = conn.prepareStatement(query);
+        ) {
+            pst.setString(1, event.getId() + appendId);
+            pst.setTimestamp(2, Timestamp.valueOf(now.toLocalDateTime()));
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            log.error("Failed to add the scheduled email sent time: ", e);
+        }
+        return null;
+    }
+}
--- src/main/java/uk/ac/cam/cl/dtg/isaac/api/managers/ScheduledEmailManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/api/managers/ScheduledEmailManager.java
@@ -17,12 +17,10 @@ import uk.ac.cam.cl.dtg.segue.comm.EmailType;
 import uk.ac.cam.cl.dtg.segue.dao.SegueDatabaseException;
 import uk.ac.cam.cl.dtg.segue.dao.content.ContentManagerException;
 import uk.ac.cam.cl.dtg.segue.dao.content.IContentManager;
-import uk.ac.cam.cl.dtg.segue.database.PostgresSqlDb;
 import uk.ac.cam.cl.dtg.segue.dto.ResultsWrapper;
 import uk.ac.cam.cl.dtg.segue.dto.content.ContentDTO;
 import uk.ac.cam.cl.dtg.segue.dto.users.RegisteredUserDTO;
 import uk.ac.cam.cl.dtg.segue.dto.users.UserSummaryDTO;
-import uk.ac.cam.cl.dtg.segue.scheduler.jobs.EventReminderEmailJob;
 import uk.ac.cam.cl.dtg.segue.search.AbstractFilterInstruction;
 import uk.ac.cam.cl.dtg.segue.search.DateRangeFilterInstruction;
 import uk.ac.cam.cl.dtg.util.PropertiesLoader;
@@ -44,14 +42,15 @@ import static uk.ac.cam.cl.dtg.segue.api.Constants.CONTENT_INDEX;
 import static uk.ac.cam.cl.dtg.segue.api.Constants.TYPE_FIELDNAME;
 
 public class ScheduledEmailManager {
-    private static final Logger log = LoggerFactory.getLogger(EventReminderEmailJob.class);
+    private static final Logger log = LoggerFactory.getLogger(ScheduledEmailManager.class);
 
     private final PropertiesLoader properties;
-    private final PostgresSqlDb database;
     private final IContentManager contentManager;
     private final EventBookingManager bookingManager;
     private final UserAccountManager userAccountManager;
     private final EmailManager emailManager;
+    private final PgScheduledEmailManager pgScheduledEmailManager;
+
 
     /**
      * This class is required by quartz and must be executable by any instance of the segue api relying only on the
@@ -59,61 +58,41 @@ public class ScheduledEmailManager {
      */
     @Inject
     public ScheduledEmailManager(final PropertiesLoader properties,
-                                 final PostgresSqlDb database,
                                  final IContentManager contentManager,
                                  final EventBookingManager bookingManager,
                                  final UserAccountManager userAccountManager,
-                                 final EmailManager emailManager) {
+                                 final EmailManager emailManager,
+                                 final PgScheduledEmailManager pgScheduledEmailManager) {
         this.properties = properties;
         this.contentManager = contentManager;
         this.bookingManager = bookingManager;
-        this.database = database;
         this.userAccountManager = userAccountManager;
         this.emailManager = emailManager;
+        this.pgScheduledEmailManager = pgScheduledEmailManager;
     }
 
-    public boolean scheduledEmailNotAlreadySent(IsaacEventPageDTO event, String appendId) {
-        ZonedDateTime now = ZonedDateTime.now();
-        String query = "INSERT INTO scheduled_emails(email_id, sent) VALUES (?, ?)"
-            + " ON CONFLICT (email_id) DO NOTHING";
-        try (Connection conn = database.getDatabaseConnection();
-             PreparedStatement pst = conn.prepareStatement(query);
-        ) {
-            pst.setString(1, event.getId() + appendId);
-            pst.setTimestamp(2, Timestamp.valueOf(now.toLocalDateTime()));
-
-            int notAlreadySent = pst.executeUpdate();
-            return notAlreadySent > 0;
-        } catch (SQLException e) {
-            log.error("Failed to add the scheduled email sent time: ", e);
-        }
-        return false;
-    };
-
-    public void sendEmailForEvent(IsaacEventPageDTO event, String templateId, String appendId) throws SegueDatabaseException {
-        if (this.scheduledEmailNotAlreadySent(event, appendId)) {
-            List<Long> ids = Lists.newArrayList();
-            List<DetailedEventBookingDTO> eventBookings = bookingManager.adminGetBookingsByEventId(event.getId());
-            eventBookings.stream().map(DetailedEventBookingDTO::getUserBooked).map(UserSummaryDTO::getId)
-                .forEach(ids::add);
-            for (Long id : ids) {
-                try {
-                    RegisteredUserDTO user = userAccountManager.getUserDTOById(id);
-                    emailManager.sendTemplatedEmailToUser(user,
-                        emailManager.getEmailTemplateDTO(templateId),
-                        new ImmutableMap.Builder<String, Object>()
-                            .put("event.emailEventDetails", event.getEmailEventDetails() == null ? "" : event.getEmailEventDetails())
-                            .put("event", event)
-                            .build(),
-                        EmailType.SYSTEM);
-                    log.info(String.format("Sent email to user: %s %s, at: %s", user.getGivenName(), user.getFamilyName(), user.getEmail()));
-                } catch (NoUserException e) {
-                    log.error(String.format("No user found with ID: %s", id));
-                } catch (ContentManagerException e) {
-                    log.error("Failed to add the scheduled email sent time: ", e);
-                }
-
+    public void sendEmailForEvent(IsaacEventPageDTO event, String templateId) throws SegueDatabaseException {
+        List<Long> ids = Lists.newArrayList();
+        List<DetailedEventBookingDTO> eventBookings = bookingManager.adminGetBookingsByEventId(event.getId());
+        eventBookings.stream().map(DetailedEventBookingDTO::getUserBooked).map(UserSummaryDTO::getId)
+            .forEach(ids::add);
+        for (Long id : ids) {
+            try {
+                RegisteredUserDTO user = userAccountManager.getUserDTOById(id);
+//                emailManager.sendTemplatedEmailToUser(user,
+//                    emailManager.getEmailTemplateDTO(templateId),
+//                    new ImmutableMap.Builder<String, Object>()
+//                        .put("event.emailEventDetails", event.getEmailEventDetails() == null ? "" : event.getEmailEventDetails())
+//                        .put("event", event)
+//                        .build(),
+//                    EmailType.SYSTEM);
+                log.info(String.format("Sent email to user: %s %s, at: %s", user.getGivenName(), user.getFamilyName(), user.getEmail()));
+            } catch (NoUserException e) {
+                log.error(String.format("No user found with ID: %s", id));
+//            } catch (ContentManagerException e) {
+//                log.error("Failed to add the scheduled email sent time: ", e);
             }
+
         }
         log.info(" ");
     }
@@ -128,9 +107,10 @@ public class ScheduledEmailManager {
         fieldsToMatch.put(TYPE_FIELDNAME, Collections.singletonList(EVENT_TYPE));
         sortInstructions.put(DATE_FIELDNAME, Constants.SortOrder.DESC);
         ZonedDateTime now = ZonedDateTime.now();
-        ZonedDateTime threeDaysAhead = now.plusDays(3);
+        ZonedDateTime threeDaysbehind = now.plusDays(-365);
+        ZonedDateTime threeDaysAhead = now.plusDays(365);
         DateRangeFilterInstruction
-            eventsWithinThreeDays = new DateRangeFilterInstruction(new Date(), Date.from(threeDaysAhead.toInstant()));
+            eventsWithinThreeDays = new DateRangeFilterInstruction(Date.from(threeDaysbehind.toInstant()), Date.from(threeDaysAhead.toInstant()));
         filterInstructions.put(ENDDATE_FIELDNAME, eventsWithinThreeDays);
 
         try {
@@ -140,13 +120,23 @@ public class ScheduledEmailManager {
             for (ContentDTO contentResult : findByFieldNames.getResults()) {
                 if (contentResult instanceof IsaacEventPageDTO) {
                     IsaacEventPageDTO event = (IsaacEventPageDTO) contentResult;
-                    this.sendEmailForEvent(event, "event_reminder", "@pre");
+                    log.info(String.format("Trying to send emails for event: %s", event.getId()));
+                    if (!pgScheduledEmailManager.scheduledEmailAlreadySent(event, "@pre")) {
+                        log.info(String.format("Sending emails for event: %s", event.getId()));
+                        pgScheduledEmailManager.saveScheduledEmailSent(event, "@pre");
+                        this.sendEmailForEvent(event, "event_reminder");
+                    } else {
+                        log.info(String.format("Emails already sent for event: %s", event.getId()));
+                    }
                 }
             }
         } catch (ContentManagerException | SegueDatabaseException e) {
             log.error("Failed to send scheduled event reminder emails: ", e);
         }
     }
+
+
+
 
     public void sendFeedbackEmails() {
         // Magic number
@@ -175,7 +165,12 @@ public class ScheduledEmailManager {
                     boolean endDateToday = event.getEndDate() != null && event.getEndDate().toInstant().isBefore(new Date().toInstant());
                     boolean noEndDateAndStartDateToday = event.getEndDate() == null && event.getDate().toInstant().isBefore(new Date().toInstant());
                     if (endDateToday || noEndDateAndStartDateToday) {
-                        this.sendEmailForEvent(event, "event_feedback", "@post");
+                        if (!pgScheduledEmailManager.scheduledEmailAlreadySent(event, "@post")) {
+                            pgScheduledEmailManager.saveScheduledEmailSent(event, "@post");
+                            this.sendEmailForEvent(event, "event_feedback");
+                        }
+                    } else {
+                        log.info("Event falls outside range for feedback email");
                     }
                 }
             }
