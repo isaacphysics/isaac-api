c33965d62482098038793287a13f77c84814409d
Andrea Franceschini <andrea.franceschini@gmail.com>
2022-06-21 17:59:45 +0200

    This should correctly capture the cookie, but there's a problem casting it into a Cookie array and my brain is fried

--- src/test/java/uk/ac/cam/cl/dtg/isaac/api/EventsFacadeTest.java
+++ src/test/java/uk/ac/cam/cl/dtg/isaac/api/EventsFacadeTest.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.cl.dtg.isaac.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
@@ -9,6 +10,7 @@ import com.google.inject.Module;
 import com.google.inject.util.Modules;
 import ma.glasnost.orika.MapperFacade;
 import org.apache.commons.lang3.SystemUtils;
+import org.easymock.Capture;
 import org.eclipse.jgit.api.Git;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +25,8 @@ import uk.ac.cam.cl.dtg.isaac.dos.AbstractUserPreferenceManager;
 import uk.ac.cam.cl.dtg.isaac.dos.PgUserPreferenceManager;
 import uk.ac.cam.cl.dtg.isaac.dto.IsaacEventPageDTO;
 import uk.ac.cam.cl.dtg.isaac.dto.ResultsWrapper;
+import uk.ac.cam.cl.dtg.isaac.dto.users.RegisteredUserDTO;
+import uk.ac.cam.cl.dtg.segue.api.Constants;
 import uk.ac.cam.cl.dtg.segue.api.managers.PgTransactionManager;
 import uk.ac.cam.cl.dtg.segue.api.managers.QuestionManager;
 import uk.ac.cam.cl.dtg.segue.api.managers.UserAccountManager;
@@ -32,16 +36,27 @@ import uk.ac.cam.cl.dtg.segue.api.managers.UserBadgeManager;
 import uk.ac.cam.cl.dtg.segue.auth.AuthenticationProvider;
 import uk.ac.cam.cl.dtg.segue.auth.IAuthenticator;
 import uk.ac.cam.cl.dtg.segue.auth.ISecondFactorAuthenticator;
+import uk.ac.cam.cl.dtg.segue.auth.ISegueHashingAlgorithm;
+import uk.ac.cam.cl.dtg.segue.auth.SegueLocalAuthenticator;
+import uk.ac.cam.cl.dtg.segue.auth.SeguePBKDF2v3;
 import uk.ac.cam.cl.dtg.segue.auth.SegueTOTPAuthenticator;
+import uk.ac.cam.cl.dtg.segue.auth.exceptions.AdditionalAuthenticationRequiredException;
+import uk.ac.cam.cl.dtg.segue.auth.exceptions.AuthenticationProviderMappingException;
+import uk.ac.cam.cl.dtg.segue.auth.exceptions.IncorrectCredentialsProvidedException;
+import uk.ac.cam.cl.dtg.segue.auth.exceptions.MFARequiredButNotConfiguredException;
+import uk.ac.cam.cl.dtg.segue.auth.exceptions.NoCredentialsAvailableException;
+import uk.ac.cam.cl.dtg.segue.auth.exceptions.NoUserException;
 import uk.ac.cam.cl.dtg.segue.comm.EmailManager;
 import uk.ac.cam.cl.dtg.segue.configuration.SegueGuiceConfigurationModule;
 import uk.ac.cam.cl.dtg.segue.dao.ILogManager;
+import uk.ac.cam.cl.dtg.segue.dao.SegueDatabaseException;
 import uk.ac.cam.cl.dtg.segue.dao.associations.PgAssociationDataManager;
 import uk.ac.cam.cl.dtg.segue.dao.content.ContentMapper;
 import uk.ac.cam.cl.dtg.segue.dao.content.GitContentManager;
 import uk.ac.cam.cl.dtg.segue.dao.content.IContentManager;
 import uk.ac.cam.cl.dtg.segue.dao.schools.SchoolListReader;
 import uk.ac.cam.cl.dtg.segue.dao.users.PgAnonymousUsers;
+import uk.ac.cam.cl.dtg.segue.dao.users.PgPasswordDataManager;
 import uk.ac.cam.cl.dtg.segue.dao.users.PgUsers;
 import uk.ac.cam.cl.dtg.segue.database.GitDb;
 import uk.ac.cam.cl.dtg.segue.database.PostgresSqlDb;
@@ -50,12 +65,23 @@ import uk.ac.cam.cl.dtg.util.PropertiesLoader;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.easymock.EasyMock.and;
+import static org.easymock.EasyMock.capture;
+import static org.easymock.EasyMock.createNiceMock;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.isA;
 import static org.junit.Assert.assertEquals;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.replay;
@@ -66,7 +92,9 @@ import static uk.ac.cam.cl.dtg.segue.api.Constants.DEFAULT_LINUX_CONFIG_LOCATION
 @PowerMockIgnore("javax.net.ssl.*")
 public class EventsFacadeTest extends IsaacTest {
 
-    public EventsFacade eventsFacade;
+    private EventsFacade eventsFacade;
+    private UserAuthenticationManager userAuthenticationManager;
+    private UserAccountManager userAccountManager;
 
     @Before
     public void setUp() throws RuntimeException, IOException, ClassNotFoundException {
@@ -94,24 +122,28 @@ public class EventsFacadeTest extends IsaacTest {
             "somerandompassword")
             ; // user/pass are irrelevant because POSTGRES_HOST_AUTH_METHOD is set to "trust"
 
-        PgUsers pgUsers = new PgUsers(postgresSqlDb, null); // FIXME: This null thing.
+        JsonMapper jsonMapper = new JsonMapper();
+        PgUsers pgUsers = new PgUsers(postgresSqlDb, jsonMapper);
         PgAnonymousUsers pgAnonymousUsers = new PgAnonymousUsers(postgresSqlDb);
+        PgPasswordDataManager passwordDataManager = new PgPasswordDataManager(postgresSqlDb);
         QuestionManager questionDb = createMock(QuestionManager.class);
 
         ContentMapper contentMapper = new ContentMapper(new Reflections("uk.ac.cam.cl.dtg"));
         MapperFacade mapper = contentMapper.getAutoMapper();
 
         // The following may need some actual authentication providers...
-        Map<AuthenticationProvider, IAuthenticator> providersToRegister = createMock(Map.class);
+        Map<AuthenticationProvider, IAuthenticator> providersToRegister = new HashMap<>();
+        Map<String, ISegueHashingAlgorithm> algorithms = Collections.singletonMap("SeguePBKDF2v3", new SeguePBKDF2v3());
+        providersToRegister.put(AuthenticationProvider.SEGUE, new SegueLocalAuthenticator(pgUsers, passwordDataManager, properties, algorithms, algorithms.get("SeguePBKDF2v3")));
 
         EmailManager emailManager = createMock(EmailManager.class);
         ILogManager logManager = createMock(ILogManager.class);
-        UserAuthenticationManager userAuthenticationManager = new UserAuthenticationManager(pgUsers, properties,
+        userAuthenticationManager = new UserAuthenticationManager(pgUsers, properties,
                 providersToRegister, emailManager);
         ISecondFactorAuthenticator secondFactorManager = createMock(SegueTOTPAuthenticator.class);
         AbstractUserPreferenceManager userPreferenceManager = new PgUserPreferenceManager(postgresSqlDb);
 
-        UserAccountManager userAccountManager =
+        userAccountManager =
                 new UserAccountManager(pgUsers, questionDb, properties, providersToRegister, mapper,
                         emailManager, pgAnonymousUsers, logManager, userAuthenticationManager, secondFactorManager,
                         userPreferenceManager);
@@ -172,5 +204,32 @@ public class EventsFacadeTest extends IsaacTest {
         List<IsaacEventPageDTO> results = entity.getResults();
         assertNotNull(entity);
         assertEquals(results.size(), 5);
+    }
+
+    @Test
+    public void getBookingsByEventIdTest() throws NoCredentialsAvailableException, NoUserException, SegueDatabaseException, AuthenticationProviderMappingException, IncorrectCredentialsProvidedException, AdditionalAuthenticationRequiredException, InvalidKeySpecException, NoSuchAlgorithmException, MFARequiredButNotConfiguredException {
+        String someSegueAnonymousUserId = "9284723987anonymous83924923";
+
+        HttpSession httpSession = createNiceMock(HttpSession.class);
+        expect(httpSession.getAttribute(Constants.ANONYMOUS_USER)).andReturn(null).atLeastOnce();
+        expect(httpSession.getId()).andReturn(someSegueAnonymousUserId).atLeastOnce();
+        replay(httpSession);
+
+        Capture<Cookie> capturedCookie = Capture.newInstance(); // new Capture<Cookie>(); seems deprecated
+
+        HttpServletRequest request = createNiceMock(HttpServletRequest.class);
+        expect(request.getSession()).andReturn(httpSession).atLeastOnce();
+        expect(request.getCookies()).andReturn((Cookie[]) Collections.singletonList(capturedCookie).toArray()).atLeastOnce();
+        replay(request);
+
+        HttpServletResponse response = createNiceMock(HttpServletResponse.class);
+        response.addCookie(and(capture(capturedCookie), isA(Cookie.class)));
+        expectLastCall().atLeastOnce(); // This is how you expect void methods, apparently...
+        replay(response);
+
+        RegisteredUserDTO testUsers = userAccountManager.authenticateWithCredentials(request, response, AuthenticationProvider.SEGUE.toString(), "test-teacher@test.com", "test1234", false);
+        Response createBookingResponse = eventsFacade.createBookingForMe(request, "b34eeb0c-7304-4c25-b83b-f28c78b5d078", null);
+        assertEquals(createBookingResponse.getStatus(), Response.Status.OK.getStatusCode());
+        // Response response = eventsFacade.getEventBookingsById(request, )
     }
 }
