3af64500b103f18de459b0abce474cb1a9396b48
James Sharkey <jsharkey13@users.noreply.github.com>
2022-06-24 14:36:16 +0100

    Add SCrypt as default password hashing algorithm
    Use the BouncyCastle library to provide the SCrypt algorithm, and choose
    strong (perhaps too strong?) parameters for it. I implemented it as a
    versioned subclass just in case we want to adjust the parameters after
    deployment and thus need a nice naming convention.
    
    On isaac-1, the algorithm manages 2.4 H/s with N=65536 (i.e. 420ms),
    but would manage 4.8 H/s with N=32768 (i.e. 209ms) which is the
    recommended value from 2017. We may want to adjust to the faster one
    once I have checked the cracking speeds of these, but the slow choice
    in this commit is the same speed as the existing PBKDF2 algorithm.

---
+++ src/main/java/uk/ac/cam/cl/dtg/segue/auth/SegueSCryptv1.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 James Sharkey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.cam.cl.dtg.segue.auth;
+
+public class SegueSCryptv1 extends SegueSCrypt implements ISegueHashingAlgorithm {
+    private static final Integer ITERATIONS = 65536;
+    private static final Integer BLOCK_SIZE = 8;
+    private static final Integer PARALLELISM_FACTOR = 1;
+    private static final Integer KEY_LENGTH = 64;  // bytes
+    private static final String SALTING_ALGORITHM = "SHA1PRNG";
+    private static final int SALT_SIZE = 16;  // bytes
+
+    public SegueSCryptv1() {
+        super(ITERATIONS, BLOCK_SIZE, PARALLELISM_FACTOR, KEY_LENGTH, SALTING_ALGORITHM, SALT_SIZE);
+    }
+
+    @Override
+    public String hashingAlgorithmName() {
+        return "SegueSCryptv1";
+    }
+
+}
