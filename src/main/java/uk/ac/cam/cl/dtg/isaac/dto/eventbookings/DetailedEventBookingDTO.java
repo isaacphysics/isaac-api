8bc9027a13766aacb33b0a5ab044527571272992
Andrea Franceschini <andrea.franceschini@gmail.com>
2022-06-29 11:15:44 +0200

    Modify DetailedEventBookingDTO to make email disclosure explicit.
    A silent upcast in EventBookingPersistenceManager meant that we thought we were returning an EventBookingDTO where in fact we were returning a DetailedEventBookingDTO. The first contains a UserSummaryDTO, the second contained in practice a UserSummaryWithEmailAddressDTO.
    
    This commit makes the difference explicit by masking the UserSummaryDTO field in EventBookingDTO with the more detailed UserSummaryWithEmailAddressDTO field in DetailedEventBookingDTO.

--- src/main/java/uk/ac/cam/cl/dtg/isaac/dto/eventbookings/DetailedEventBookingDTO.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/dto/eventbookings/DetailedEventBookingDTO.java
@@ -1,15 +1,38 @@
 package uk.ac.cam.cl.dtg.isaac.dto.eventbookings;
 
+import uk.ac.cam.cl.dtg.isaac.dto.users.UserSummaryDTO;
+import uk.ac.cam.cl.dtg.isaac.dto.users.UserSummaryWithEmailAddressDTO;
+
 import java.util.Map;
 
 public class DetailedEventBookingDTO extends EventBookingDTO {
     private Map<String, String> additionalInformation;
 
+    // Shadows the parent's userBooked field
+    private UserSummaryWithEmailAddressDTO userBooked;
+
     public DetailedEventBookingDTO() {
 
     }
 
+    /**
+     * Gets the userBooked, with extra detail including the email address.
+     *
+     * @return the userBooked
+     */
+    public UserSummaryWithEmailAddressDTO getUserBooked() {
+        return userBooked;
+    }
 
+    /**
+     * Sets the userBooked with extra detail including the email address.
+     *
+     * @param userBooked
+     *            the userBooked to set
+     */
+    public void setUserBooked(final UserSummaryWithEmailAddressDTO userBooked) {
+        this.userBooked = userBooked;
+    }
 
     /**
      * Get additional event booking information.
