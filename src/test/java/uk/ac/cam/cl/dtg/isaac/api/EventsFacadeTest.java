7be9a892060502995248144f22a9945a96d4dd35
Andrea Franceschini <andrea.franceschini@gmail.com>
2022-07-01 12:38:36 +0100

    Refactor IsaacE2ETest initialization, moving pretty much everything in the base class.

--- src/test/java/uk/ac/cam/cl/dtg/isaac/api/EventsFacadeTest.java
+++ src/test/java/uk/ac/cam/cl/dtg/isaac/api/EventsFacadeTest.java
@@ -24,6 +24,8 @@ import uk.ac.cam.cl.dtg.isaac.dto.ResultsWrapper;
 import uk.ac.cam.cl.dtg.isaac.dto.eventbookings.EventBookingDTO;
 import uk.ac.cam.cl.dtg.isaac.dto.users.RegisteredUserDTO;
 import uk.ac.cam.cl.dtg.isaac.dto.users.UserSummaryDTO;
+import uk.ac.cam.cl.dtg.isaac.dto.users.UserSummaryWithEmailAddressDTO;
+import uk.ac.cam.cl.dtg.isaac.quiz.PgQuestionAttempts;
 import uk.ac.cam.cl.dtg.segue.api.Constants;
 import uk.ac.cam.cl.dtg.segue.api.managers.GroupManager;
 import uk.ac.cam.cl.dtg.segue.api.managers.PgTransactionManager;
@@ -57,6 +59,7 @@ import uk.ac.cam.cl.dtg.segue.dao.content.IContentManager;
 import uk.ac.cam.cl.dtg.segue.dao.schools.SchoolListReader;
 import uk.ac.cam.cl.dtg.segue.dao.users.PgAnonymousUsers;
 import uk.ac.cam.cl.dtg.segue.dao.users.PgPasswordDataManager;
+import uk.ac.cam.cl.dtg.segue.dao.users.PgUserGroupPersistenceManager;
 import uk.ac.cam.cl.dtg.segue.dao.users.PgUsers;
 import uk.ac.cam.cl.dtg.segue.database.GitDb;
 import uk.ac.cam.cl.dtg.segue.database.PostgresSqlDb;
@@ -97,127 +100,10 @@ import static uk.ac.cam.cl.dtg.segue.api.Constants.HOST_NAME;
 public class EventsFacadeTest extends IsaacE2ETest {
 
     private EventsFacade eventsFacade;
-    private UserAuthenticationManager userAuthenticationManager;
-    private UserAccountManager userAccountManager;
-    private EmailManager emailManager;
-    private GroupManager groupManager;
-    private PropertiesLoader properties;
 
     @Before
-    public void setUp() throws RuntimeException, IOException, ClassNotFoundException, SegueDatabaseException {
-        String configLocation = SystemUtils.IS_OS_LINUX ? DEFAULT_LINUX_CONFIG_LOCATION : null;
-        if (System.getProperty("test.config.location") != null) {
-            configLocation = System.getProperty("test.config.location");
-        }
-        if (System.getenv("SEGUE_TEST_CONFIG_LOCATION") != null){
-            configLocation = System.getenv("SEGUE_TEST_CONFIG_LOCATION");
-        }
-
-        properties = new PropertiesLoader(configLocation) {
-            final Map<String, String> propertyOverrides = ImmutableMap.of(
-                    "SEARCH_CLUSTER_NAME", "isaac"
-            );
-            @Override
-            public String getProperty(String key) {
-                return propertyOverrides.getOrDefault(key, super.getProperty(key));
-            }
-        };
-
-        PostgresSqlDb postgresSqlDb = new PostgresSqlDb(
-            postgres.getJdbcUrl(),
-            "rutherford",
-            "somerandompassword")
-            ; // user/pass are irrelevant because POSTGRES_HOST_AUTH_METHOD is set to "trust"
-
-        JsonMapper jsonMapper = new JsonMapper();
-        PgUsers pgUsers = new PgUsers(postgresSqlDb, jsonMapper);
-        PgAnonymousUsers pgAnonymousUsers = new PgAnonymousUsers(postgresSqlDb);
-        PgPasswordDataManager passwordDataManager = new PgPasswordDataManager(postgresSqlDb);
-        QuestionManager questionDb = createMock(QuestionManager.class);
-
-        ContentMapper contentMapper = new ContentMapper(new Reflections("uk.ac.cam.cl.dtg"));
-        MapperFacade mapper = contentMapper.getAutoMapper();
-
-        // The following may need some actual authentication providers...
-        Map<AuthenticationProvider, IAuthenticator> providersToRegister = new HashMap<>();
-        Map<String, ISegueHashingAlgorithm> algorithms = Collections.singletonMap("SeguePBKDF2v3", new SeguePBKDF2v3());
-        providersToRegister.put(AuthenticationProvider.SEGUE, new SegueLocalAuthenticator(pgUsers, passwordDataManager, properties, algorithms, algorithms.get("SeguePBKDF2v3")));
-
-        EmailCommunicator communicator = new EmailCommunicator("localhost", "default@localhost", "Howdy!");
-        AbstractUserPreferenceManager userPreferenceManager = new PgUserPreferenceManager(postgresSqlDb);
-
-        Git git = createMock(Git.class);
-        GitDb gitDb = new GitDb(git);
-        ElasticSearchProvider elasticSearchProvider =
-                new ElasticSearchProvider(ElasticSearchProvider.getTransportClient(
-                        "isaac",
-                        "localhost",
-                        elasticsearch.getMappedPort(9300))
-                );
-        IContentManager contentManager = new GitContentManager(gitDb, elasticSearchProvider, contentMapper, properties);
-        ILogManager logManager = createMock(ILogManager.class);
-
-        Map<String, String> globalTokens = Maps.newHashMap();
-        globalTokens.put("sig", properties.getProperty(EMAIL_SIGNATURE));
-        globalTokens.put("emailPreferencesURL", String.format("https://%s/account#emailpreferences",
-                properties.getProperty(HOST_NAME)));
-        globalTokens.put("myAssignmentsURL", String.format("https://%s/assignments",
-                properties.getProperty(HOST_NAME)));
-        globalTokens.put("myQuizzesURL", String.format("https://%s/quizzes",
-                properties.getProperty(HOST_NAME)));
-        globalTokens.put("myBookedEventsURL", String.format("https://%s/events?show_booked_only=true",
-                properties.getProperty(HOST_NAME)));
-        globalTokens.put("contactUsURL", String.format("https://%s/contact",
-                properties.getProperty(HOST_NAME)));
-        globalTokens.put("accountURL", String.format("https://%s/account",
-                properties.getProperty(HOST_NAME)));
-        globalTokens.put("siteBaseURL", String.format("https://%s", properties.getProperty(HOST_NAME)));
-
-        emailManager = new EmailManager(communicator, userPreferenceManager, properties, contentManager, logManager, globalTokens);
-
-        userAuthenticationManager = new UserAuthenticationManager(pgUsers, properties,
-                providersToRegister, emailManager);
-        ISecondFactorAuthenticator secondFactorManager = createMock(SegueTOTPAuthenticator.class);
-        // We don't care for MFA here so we can safely disable it
-        expect(secondFactorManager.has2FAConfigured(anyObject())).andReturn(false).atLeastOnce();
-        replay(secondFactorManager);
-
-        userAccountManager =
-                new UserAccountManager(pgUsers, questionDb, properties, providersToRegister, mapper,
-                        emailManager, pgAnonymousUsers, logManager, userAuthenticationManager, secondFactorManager,
-                        userPreferenceManager);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        EventBookingPersistenceManager bookingPersistanceManager =
-                new EventBookingPersistenceManager(postgresSqlDb, userAccountManager, contentManager, objectMapper);
-        PgAssociationDataManager pgAssociationDataManager = new PgAssociationDataManager(postgresSqlDb);
-        UserAssociationManager userAssociationManager = new UserAssociationManager(pgAssociationDataManager, userAccountManager, groupManager);
-        PgTransactionManager pgTransactionManager = new PgTransactionManager(postgresSqlDb);
-        EventBookingManager eventBookingManager =
-                new EventBookingManager(bookingPersistanceManager, emailManager, userAssociationManager,
-                        properties, groupManager, userAccountManager, pgTransactionManager);
-        UserBadgeManager userBadgeManager = createMock(UserBadgeManager.class);
-        SchoolListReader schoolListReader = createMock(SchoolListReader.class);
-
-        // NOTE: The next part is commented out until we figure out a way of actually using Guice to do the heavy lifting for us..
-        /*
-        // Create Mocked Injector
-        SegueGuiceConfigurationModule.setGlobalPropertiesIfNotSet(properties);
-        Module productionModule = new SegueGuiceConfigurationModule();
-        Module testModule = Modules.override(productionModule).with(new AbstractModule() {
-            @Override protected void configure() {
-                // ... register mocks
-                bind(UserAccountManager.class).toInstance(userAccountManager);
-                bind(GameManager.class).toInstance(createMock(GameManager.class));
-                bind(GroupChangedService.class).toInstance(createMock(GroupChangedService.class));
-                bind(EventBookingManager.class).toInstance(eventBookingManager);
-            }
-        });
-        Injector injector = Guice.createInjector(testModule);
-         */
-        // Register DTOs to json mapper
-        MapperFacade mapperFacade = contentMapper.getAutoMapper();
-        // Get instance of class to test
+    public void setUp() {
+        // Get an instance of the facade to test
         eventsFacade = new EventsFacade(properties, logManager, eventBookingManager, userAccountManager, contentManager, "latest", userBadgeManager, userAssociationManager, groupManager, userAccountManager, schoolListReader, mapperFacade);
     }
 
@@ -300,7 +186,7 @@ public class EventsFacadeTest extends IsaacE2ETest {
         Response getEventBookingsByIdResponse = eventsFacade.getEventBookingsById(getEventBookingsByIdRequest, eventBookingDTO.getBookingId().toString());
         if (null != getEventBookingsByIdResponse.getEntity() && getEventBookingsByIdResponse.getEntity() instanceof EventBookingDTO) {
             assertNotNull(((EventBookingDTO) getEventBookingsByIdResponse.getEntity()).getUserBooked());
-            assertEquals(((EventBookingDTO) getEventBookingsByIdResponse.getEntity()).getUserBooked().getClass(), UserSummaryDTO.class);
+            assertEquals(((EventBookingDTO) getEventBookingsByIdResponse.getEntity()).getUserBooked().getClass(), UserSummaryWithEmailAddressDTO.class);
         }
     }
 }
