bd2ead0927a1fe135fed241c52aebe8402f51294
James Sharkey <jsharkey13@users.noreply.github.com>
2022-08-03 17:00:39 +0100

    Restore old Swagger baseUrl logic for JSON path

--- src/main/webapp/api-docs/swagger-initializer.js
+++ src/main/webapp/api-docs/swagger-initializer.js
@@ -1,9 +1,19 @@
 window.onload = function () {
     //<editor-fold desc="Changeable Configuration Block">
 
+    // Work out where Isaac is running and where to find swagger.json:
+    var baseUrl = window.location.href.replace(/api-docs.*/, "api");
+
+    if (document.location.hostname === "localhost") {
+        baseUrl ="http://" + window.location.host + "/isaac-api/api";
+    } else if (document.location.hostname.endsWith(".eu.ngrok.io")) {
+        // Have reserved domains on ngrok.io, hardcode them for ease of use:
+        baseUrl = "https://isaacscience.eu.ngrok.io/isaac-api/api";
+    }
+
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
-        url: "/isaac-api/api/openapi.json",
+        url: baseUrl + "/openapi.json",
         dom_id: '#swagger-ui',
         deepLinking: true,
         presets: [
