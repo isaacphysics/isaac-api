0544ebdeba2a4c5b406ff991a3425753bc438b08
Matthew Trew <matthew.trew@raspberrypi.org>
2022-08-19 15:17:17 +0100

    Revert commented-out parameter and copyright notice

--- src/test/java/uk/ac/cam/cl/dtg/isaac/api/IsaacIntegrationTest.java
+++ src/test/java/uk/ac/cam/cl/dtg/isaac/api/IsaacIntegrationTest.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2022 Matthew Trew
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *
- * You may obtain a copy of the License at
- * 		http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package uk.ac.cam.cl.dtg.isaac.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -160,7 +144,6 @@ public class IsaacIntegrationTest {
                 .withExposedPorts(9200, 9300)
                 .withEnv("cluster.name", "isaac")
                 .withEnv("node.name", "localhost")
-                // .withEnv("ES_JAVA_OPTS", String.format("-Xmx%sm", ES_MAX_JVM_HEAP_SIZE_MB))
                 .withStartupTimeout(Duration.ofSeconds(120));
         ;
 
