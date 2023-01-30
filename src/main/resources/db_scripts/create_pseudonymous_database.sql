b7aae8747358e5d4ca8ab21553bb70be3f5f59e2
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-01-30 18:25:57 +0000

    Add every table used in the API to the pseudonymous DB script
    I found these by searching for every use of SELECT ... FROM, INSERT INTO and UPDATE.

--- src/main/resources/db_scripts/create_pseudonymous_database.sql
+++ src/main/resources/db_scripts/create_pseudonymous_database.sql
@@ -29,6 +29,7 @@ BEGIN
 DROP SCHEMA IF EXISTS anonymous CASCADE;
 CREATE SCHEMA anonymous;
 
+
 -- User related tables:
 
 CREATE TABLE anonymous.users AS SELECT * FROM public.users;
@@ -36,6 +37,33 @@ CREATE TABLE anonymous.users AS SELECT * FROM public.users;
 CREATE TABLE anonymous.linked_accounts AS SELECT * FROM public.linked_accounts;
 
 CREATE TABLE anonymous.user_preferences AS SELECT * FROM public.user_preferences;
+
+CREATE TABLE anonymous.user_associations_tokens AS SELECT * FROM public.user_associations_tokens;
+
+CREATE TABLE anonymous.external_accounts AS SELECT * FROM public.external_accounts;
+
+CREATE TABLE anonymous.user_badges AS SELECT * FROM public.user_badges;
+
+CREATE TABLE anonymous.user_credentials AS SELECT * FROM public.user_credentials;
+
+CREATE TABLE anonymous.user_totp AS SELECT * FROM public.user_totp;
+
+CREATE TABLE anonymous.user_email_preferences AS SELECT * FROM public.user_email_preferences;
+
+CREATE TABLE anonymous.user_notifications AS SELECT * FROM public.user_notifications;
+
+CREATE TABLE anonymous.user_alerts AS SELECT * FROM public.user_alerts;
+
+CREATE TABLE anonymous.temporary_user_store (
+    id character varying NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL,
+    last_updated timestamp with time zone DEFAULT now() NOT NULL,
+    temporary_app_data jsonb
+);
+
+CREATE TABLE anonymous.ip_location_history AS SELECT * FROM public.ip_location_history;
+
+CREATE TABLE anonymous.uk_post_codes AS SELECT * FROM public.uk_post_codes;
 
 
 -- Group related tables:
@@ -67,9 +95,18 @@ CREATE TABLE anonymous.event_bookings AS SELECT * FROM public.event_bookings;
 
 CREATE TABLE anonymous.question_attempts AS SELECT * FROM public.question_attempts;
 
+CREATE TABLE anonymous.user_streaks AS SELECT * FROM public.user_streaks;
+
+CREATE TABLE anonymous.user_streaks_current_progress AS SELECT * FROM public.user_streaks_current_progress;
+
+CREATE TABLE anonymous.user_streaks_weekly AS SELECT * FROM public.user_streaks_weekly;
+
+CREATE TABLE anonymous.user_streaks_weekly_current_progress AS SELECT * FROM public.user_streaks_weekly_current_progress;
+
 CREATE TABLE anonymous.user_streak_freezes AS SELECT * FROM public.user_streak_freezes;
 
 CREATE TABLE anonymous.user_streak_targets AS SELECT * FROM public.user_streak_targets;
+
 
 -- Quiz tables:
 
@@ -78,6 +115,21 @@ CREATE TABLE anonymous.quiz_assignments AS SELECT * FROM public.quiz_assignments
 CREATE TABLE anonymous.quiz_attempts AS SELECT * FROM public.quiz_attempts;
 
 CREATE TABLE anonymous.quiz_question_attempts AS SELECT * FROM public.quiz_question_attempts;
+
+
+-- Logged events
+
+CREATE TABLE anonymous.logged_events (
+    id integer NOT NULL,
+    user_id character varying(100) NOT NULL,
+    anonymous_user boolean NOT NULL,
+    event_type character varying(255),
+    event_details_type text,
+    event_details jsonb,
+    ip_address inet,
+    "timestamp" timestamp without time zone
+);
+
 
 -- Logged events: (commented out)
 --
