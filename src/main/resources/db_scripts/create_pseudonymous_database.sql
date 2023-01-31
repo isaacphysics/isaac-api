469b77ce2d650a78966d698bc6ec6a6f68093e11
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-01-31 14:43:32 +0000

    Anonymise data as much as possible, some work still required
    I'm unsure of logged_events in particular

--- src/main/resources/db_scripts/create_pseudonymous_database.sql
+++ src/main/resources/db_scripts/create_pseudonymous_database.sql
@@ -17,6 +17,25 @@ $$ BEGIN
 END; $$
 LANGUAGE plpgsql;
 
+-- Slightly adapted from this postgresql mailing list post
+-- https://www.postgresql.org/message-id/AANLkTi%3D34m32htPtrxb%2BTUks9i2oxu0YbJ7XyPbhK6BJ%40mail.gmail.com
+CREATE OR REPLACE FUNCTION hex_to_integer(hexval varchar) RETURNS integer AS $$
+    DECLARE
+        result  integer;
+    BEGIN
+    EXECUTE 'SELECT x''' || LEFT(hexval, 8) || '''::integer' INTO result;
+    RETURN result;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION anonymise_int(id BIGINT, salt TEXT) RETURNS integer AS
+$$ BEGIN
+    RETURN hex_to_integer(anonymise(id, salt));
+END; $$
+LANGUAGE plpgsql;
+
 -- Idea is to create a clone of as much the database as is needed to run. Switch the scheme over from public to anonymous
 -- and run the regression tests (or test manually?) to check if it works. Maybe check for every database table that is
 -- mentioned in the codebase.
@@ -32,175 +51,300 @@ CREATE SCHEMA anonymous;
 
 -- User related tables:
 
-CREATE TABLE anonymous.users AS SELECT * FROM public.users;
+CREATE TABLE anonymous.users AS
+    SELECT
+        anonymise_int(id, hash_salt) as id,
+        anonymise(_id, hash_salt),
+        'FamilyName-' || LEFT(anonymise(public.users.id, hash_salt), 5) as family_name,
+        'GivenName-' || LEFT(anonymise(public.users.id, hash_salt), 5) as given_name,
+        anonymise(public.users.id, hash_salt) || '@isaac-cs-anonymous-email.com' as email,
+        role,
+        CASE
+            WHEN date_part('month', date_of_birth) < 9 THEN make_date((date_part('year', date_of_birth)-1)::int, 9, 1)
+            WHEN date_part('month', date_of_birth) >= 9 THEN make_date(date_part('year', date_of_birth)::int, 9, 1)
+            ELSE NULL
+        END AS date_of_birth,
+        'UNKNOWN' as gender,
+        school_id,
+        registration_date,
+        last_seen
+    FROM public.users;
 
-CREATE TABLE anonymous.linked_accounts AS SELECT * FROM public.linked_accounts;
+-- linked_accounts ignored
 
-CREATE TABLE anonymous.user_preferences AS SELECT * FROM public.user_preferences;
+CREATE TABLE anonymous.user_preferences AS
+    SELECT
+        anonymise_int(user_id, hash_salt) AS user_id,
+        preference_type,
+        preference_name,
+        preference_value,
+        last_updated
+    FROM public.user_preferences;
 
-CREATE TABLE anonymous.user_associations_tokens AS SELECT * FROM public.user_associations_tokens;
+CREATE TABLE anonymous.user_associations_tokens AS
+    SELECT
+        token,
+        anonymise_int(owner_user_id, hash_salt) AS owner_user_id,
+        anonymise_int(group_id, hash_salt) AS group_id,
+    FROM public.user_associations_tokens;
 
-CREATE TABLE anonymous.external_accounts AS SELECT * FROM public.external_accounts;
+-- external_accounts ignored
 
 CREATE TABLE anonymous.user_badges AS SELECT * FROM public.user_badges;
 
-CREATE TABLE anonymous.user_credentials AS SELECT * FROM public.user_credentials;
+CREATE TABLE anonymous.user_credentials AS
+    SELECT
+        anonymise_int(user_id, hash_salt) AS user_id,
+        'bflrBg4DlCjZRl/k2hn2cotSWZERu44OFBeaywz/6Gzy4bX0k6TVjh/l+uI3cF1SKyib7zsk4fOulb1Moud+4A==' as password,
+        'hHdj7z1JEus5yTf6FmEznw==' as secure_salt,
+        'SeguePBKDF2v2' as security_scheme,
+        NULL as reset_token,
+        NULL as reset_expiry,
+        created,
+        last_updated
+    FROM public.user_credentials;
 
-CREATE TABLE anonymous.user_totp AS SELECT * FROM public.user_totp;
+-- user_totp ignored
 
-CREATE TABLE anonymous.user_email_preferences AS SELECT * FROM public.user_email_preferences;
+-- user_email_preferences ignored
 
-CREATE TABLE anonymous.user_notifications AS SELECT * FROM public.user_notifications;
+-- user_notifications ignored
 
-CREATE TABLE anonymous.user_alerts AS SELECT * FROM public.user_alerts;
+-- user_alerts ignored
 
-CREATE TABLE anonymous.temporary_user_store (
-    id character varying NOT NULL,
-    created timestamp with time zone DEFAULT now() NOT NULL,
-    last_updated timestamp with time zone DEFAULT now() NOT NULL,
-    temporary_app_data jsonb
-);
+-- temporary_user_store ignored
 
-CREATE TABLE anonymous.ip_location_history AS SELECT * FROM public.ip_location_history;
+-- ip_location_history ignored
 
 CREATE TABLE anonymous.uk_post_codes AS SELECT * FROM public.uk_post_codes;
 
 
 -- Group related tables:
 
-CREATE TABLE anonymous.user_associations AS SELECT * FROM public.user_associations;
+CREATE TABLE anonymous.user_associations AS
+    SELECT
+        anonymise_int(user_id_granting_permission, hash_salt) AS user_id_granting_permission ,
+        anonymise_int(user_id_receiving_permission, hash_salt) AS user_id_receiving_permission,
+        created
+    FROM public.user_associations;
 
-CREATE TABLE anonymous.groups AS SELECT * FROM public.groups;
+CREATE TABLE anonymous.groups AS
+    SELECT
+        anonymise_int(id, hash_salt) AS id,
+        'Group-' || anonymise_int(id, hash_salt)::varchar(255) as group_name,
+        anonymise_int(owner_id, hash_salt) AS owner_id,
+        created,
+        archived,
+        group_status,
+        last_updated
+    FROM public.groups;
 
-CREATE TABLE anonymous.group_memberships AS SELECT * FROM public.group_memberships;
+CREATE TABLE anonymous.group_memberships AS
+    SELECT
+        anonymise_int(group_id, hash_salt) AS group_id,
+        anonymise_int(user_id, hash_salt) AS user_id,
+        created,
+        updated,
+        status
+    FROM public.group_memberships;
 
-CREATE TABLE anonymous.group_additional_managers AS SELECT * FROM public.group_additional_managers;
+CREATE TABLE anonymous.group_additional_managers AS
+    SELECT
+        anonymise_int(user_id, hash_salt) AS user_id,
+        anonymise_int(group_id, hash_salt) AS group_id,
+        created
+    FROM public.group_additional_managers;
 
 
 -- Assignment related tables:
 
-CREATE TABLE anonymous.gameboards AS SELECT * FROM public.gameboards;
+CREATE TABLE anonymous.gameboards AS
+    SELECT
+       id,
+       'Gameboard-' || public.gameboards.id as title,
+       contents,
+       wildcard,
+       wildcard_position,
+       game_filter,
+       anonymise_int(owner_user_id, hash_salt) AS owner_user_id,
+       creation_method,
+       creation_date,
+       tags
+    FROM public.gameboards;
 
-CREATE TABLE anonymous.user_gameboards AS SELECT * FROM public.user_gameboards;
+CREATE TABLE anonymous.user_gameboards AS
+    SELECT
+        anonymise_int(user_id, hash_salt) as user_id,
+        gameboard_id,
+        created
+        last_visited
+    FROM public.user_gameboards;
 
-CREATE TABLE anonymous.assignments AS SELECT * FROM public.assignments;
+CREATE TABLE anonymous.assignments AS
+    SELECT
+        id,
+        gameboard_id,
+        anonymise_int(group_id, hash_salt) AS group_id,
+        anonymise_int(owner_user_id, hash_salt) as owner_user_id,
+        '' as notes,
+        creation_date,
+        due_date,
+        scheduled_start_date
+    FROM public.assignments;
 
 
 -- Event bookings:
 
-CREATE TABLE anonymous.event_bookings AS SELECT * FROM public.event_bookings;
+CREATE TABLE anonymous.event_bookings AS
+    SELECT
+        id,
+        'Event-' || anonymise(event_id, hash_salt) as event_id,
+        created,
+        anonymise_int(user_id, hash_salt) as user_id,
+        anonymise_int(reserved_by, hash_salt) as reserved_by,
+        status,
+        updated,
+        additional_booking_information, -- might need removing (replace with {}::jsonb perhaps?)
+        pii_removed
+    FROM public.event_bookings;
 
 
 -- Question attempt related tables:
 
-CREATE TABLE anonymous.question_attempts AS SELECT * FROM public.question_attempts;
+CREATE TABLE anonymous.question_attempts AS
+    SELECT
+        id,
+        anonymise_int(user_id, hash_salt) as user_id,
+        question_id,
+        '{}'::jsonb as question_attempt,
+        correct,
+        timestamp
+    FROM public.question_attempts;
 
-CREATE TABLE anonymous.user_streaks AS SELECT * FROM public.user_streaks;
+CREATE TABLE anonymous.user_streak_freezes AS
+    SELECT
+        anonymise_int(user_id, hash_salt) as user_id,
+        start_date,
+        end_date,
+        'Streak freeze' as comment
+    FROM public.user_streak_freezes;
 
-CREATE TABLE anonymous.user_streaks_current_progress AS SELECT * FROM public.user_streaks_current_progress;
-
-CREATE TABLE anonymous.user_streaks_weekly AS SELECT * FROM public.user_streaks_weekly;
-
-CREATE TABLE anonymous.user_streaks_weekly_current_progress AS SELECT * FROM public.user_streaks_weekly_current_progress;
-
-CREATE TABLE anonymous.user_streak_freezes AS SELECT * FROM public.user_streak_freezes;
-
-CREATE TABLE anonymous.user_streak_targets AS SELECT * FROM public.user_streak_targets;
+CREATE TABLE anonymous.user_streak_targets AS
+    SELECT
+        anonymise_int(user_id, hash_salt) as user_id,
+        target_count,
+        start_date,
+        end_date,
+        'Streak target' as comment
+    FROM public.user_streak_targets;
 
 
 -- Quiz tables:
 
-CREATE TABLE anonymous.quiz_assignments AS SELECT * FROM public.quiz_assignments;
+CREATE TABLE anonymous.quiz_assignments AS
+    SELECT
+        id,
+        quiz_id,
+        anonymise_int(group_id, hash_salt) as group_id,
+        anonymise_int(owner_user_id, hash_salt) as owner_user_id,
+        creation_date,
+        due_date,
+        quiz_feedback_mode,
+        deleted
+    FROM public.quiz_assignments;
 
-CREATE TABLE anonymous.quiz_attempts AS SELECT * FROM public.quiz_attempts;
+CREATE TABLE anonymous.quiz_attempts AS
+    SELECT
+        id,
+        anonymise_int(user_id, hash_salt) as user_id,
+        quiz_id,
+        quiz_assignment_id,
+        start_date,
+        completed_date
+    FROM public.quiz_attempts;
 
-CREATE TABLE anonymous.quiz_question_attempts AS SELECT * FROM public.quiz_question_attempts;
+CREATE TABLE anonymous.quiz_question_attempts AS
+    SELECT
+        id,
+        quiz_attempt_id,
+        question_id,
+        '{}'::jsonb as question_attempt,
+        correct,
+        timestamp
+    FROM public.quiz_question_attempts;
 
 
 -- Logged events
 
-CREATE TABLE anonymous.logged_events (
-    id integer NOT NULL,
-    user_id character varying(100) NOT NULL,
-    anonymous_user boolean NOT NULL,
-    event_type character varying(255),
-    event_details_type text,
-    event_details jsonb,
-    ip_address inet,
-    "timestamp" timestamp without time zone
-);
-
-
--- Logged events: (commented out)
---
--- CREATE TABLE anonymous.logged_events AS
---     SELECT
---         id,
---         anonymise(user_id, hash_salt) AS user_id,
---         anonymous_user,
---         event_type,
---         event_details,
---         timestamp
---     FROM public.logged_events
---     WHERE event_type IN (
---         -- Question usage:
---         'VIEW_QUESTION', 'ANSWER_QUESTION', 'QUESTION_ATTEMPT_RATE_LIMITED', 'QUICK_QUESTION_SHOW_ANSWER',
---         'QUESTION_PART_OPEN', 'VIEW_HINT',
---         'VIEW_RELATED_CONCEPT', 'VIEW_RELATED_QUESTION',
---         'VIDEO_ENDED', 'VIDEO_PAUSE', 'VIDEO_PLAY',
---         'VIEW_SUPERSEDED_BY_QUESTION',
---         -- Concept usage:
---         'VIEW_CONCEPT', 'CONCEPT_SECTION_OPEN', 'QUICK_QUESTION_TAB_VIEW', 'VIEW_GITHUB_CODE',
---         -- Page usage:
---         'VIEW_PAGE', 'VIEW_PAGE_FRAGMENT',
---         -- Assignment usage:
---         'VIEW_MY_ASSIGNMENTS', 'VIEW_MY_BOARDS_PAGE', 'VIEW_GAMEBOARD_BY_ID',
---         -- Group and assignment related usage:
---         'CREATE_USER_ASSOCIATION', 'RELEASE_USER_ASSOCIATION', 'REVOKE_USER_ASSOCIATION',
---         'CREATE_USER_GROUP', 'DELETE_USER_GROUP', 'ADD_ADDITIONAL_GROUP_MANAGER', 'DELETE_ADDITIONAL_GROUP_MANAGER',
---         'SET_NEW_ASSIGNMENT', 'DELETE_ASSIGNMENT', 'VIEW_BOARD_BUILDER', 'CLONE_GAMEBOARD',
---         'VIEW_ASSIGNMENT_PROGRESS', 'DOWNLOAD_ASSIGNMENT_PROGRESS_CSV', 'DOWNLOAD_GROUP_PROGRESS_CSV', 'VIEW_USER_PROGRESS',
---         -- Event related usage:
---         'ADMIN_EVENT_ATTENDANCE_RECORDED', 'ADMIN_EVENT_BOOKING_CANCELLED', 'ADMIN_EVENT_BOOKING_CONFIRMED',
---         'ADMIN_EVENT_BOOKING_CREATED', 'ADMIN_EVENT_BOOKING_DELETED', 'ADMIN_EVENT_WAITING_LIST_PROMOTION',
---         'EVENT_BOOKING', 'EVENT_BOOKING_CANCELLED', 'EVENT_WAITING_LIST_BOOKING',
---         -- Other useful events:
---         'EQN_EDITOR_LOG', 'USER_REGISTRATION', 'LOG_OUT', 'CHANGE_CONTENT_VERSION', 'DELETE_USER_ACCOUNT',
---         'USER_SCHOOL_CHANGE'
---     );
--- -- Anonymise specific details:
--- -- userIds
--- WITH
---     expanded_details AS (SELECT id, jsonb_array_elements_text(event_details->'userIds') AS elements FROM anonymous.logged_events WHERE event_details->'userIds' IS NOT NULL),
---     new_details AS (SELECT id, jsonb_strip_nulls(jsonb_agg(  anonymise(elements, hash_salt)  )) AS new_event_detail FROM expanded_details GROUP BY id)
--- UPDATE anonymous.logged_events
---     SET
---         event_details=jsonb_set(event_details, '{userIds}', new_event_detail)
---     FROM new_details
---     WHERE anonymous.logged_events.id=new_details.id;
--- -- userId
--- UPDATE anonymous.logged_events
---     SET event_details=jsonb_set(event_details, '{userId}', to_jsonb(anonymise(event_details->>'userId', hash_salt)))
---     WHERE event_details->>'userId' IS NOT NULL;
--- -- groupId
--- UPDATE anonymous.logged_events
---     SET event_details=jsonb_set(event_details, '{groupId}', to_jsonb(anonymise(event_details->>'groupId', hash_salt)))
---     WHERE event_details->>'groupId' IS NOT NULL;
--- -- assignmentId
--- UPDATE anonymous.logged_events
---     SET event_details=jsonb_set(event_details, '{assignmentId}', to_jsonb(anonymise(event_details->>'assignmentId', hash_salt)))
---     WHERE event_details->>'assignmentId' IS NOT NULL;
--- -- token
--- UPDATE anonymous.logged_events
---     SET event_details=event_details - 'token'
---     WHERE event_details->>'token' IS NOT NULL;
--- -- school_other
--- UPDATE anonymous.logged_events
--- SET event_details=jsonb_strip_nulls(event_details - 'oldSchoolOther')
--- WHERE event_details->>'oldSchoolOther' IS NOT NULL;
--- UPDATE anonymous.logged_events
--- SET event_details=jsonb_strip_nulls(event_details - 'newSchoolOther')
--- WHERE event_details->>'newSchoolOther' IS NOT NULL;
+CREATE TABLE anonymous.logged_events AS
+    SELECT
+        id,
+        anonymise(user_id, hash_salt) AS user_id,
+        anonymous_user,
+        event_type,
+        event_details,
+        '192.168.1.1'::inet as ip_address,
+        timestamp
+    FROM public.logged_events
+    WHERE event_type IN (
+        -- Question usage:
+        'VIEW_QUESTION', 'ANSWER_QUESTION', 'QUESTION_ATTEMPT_RATE_LIMITED', 'QUICK_QUESTION_SHOW_ANSWER',
+        'QUESTION_PART_OPEN', 'VIEW_HINT',
+        'VIEW_RELATED_CONCEPT', 'VIEW_RELATED_QUESTION',
+        'VIDEO_ENDED', 'VIDEO_PAUSE', 'VIDEO_PLAY',
+        'VIEW_SUPERSEDED_BY_QUESTION',
+        -- Concept usage:
+        'VIEW_CONCEPT', 'CONCEPT_SECTION_OPEN', 'QUICK_QUESTION_TAB_VIEW', 'VIEW_GITHUB_CODE',
+        -- Page usage:
+        'VIEW_PAGE', 'VIEW_PAGE_FRAGMENT',
+        -- Assignment usage:
+        'VIEW_MY_ASSIGNMENTS', 'VIEW_MY_BOARDS_PAGE', 'VIEW_GAMEBOARD_BY_ID',
+        -- Group and assignment related usage:
+        'CREATE_USER_ASSOCIATION', 'RELEASE_USER_ASSOCIATION', 'REVOKE_USER_ASSOCIATION',
+        'CREATE_USER_GROUP', 'DELETE_USER_GROUP', 'ADD_ADDITIONAL_GROUP_MANAGER', 'DELETE_ADDITIONAL_GROUP_MANAGER',
+        'SET_NEW_ASSIGNMENT', 'DELETE_ASSIGNMENT', 'VIEW_BOARD_BUILDER', 'CLONE_GAMEBOARD',
+        'VIEW_ASSIGNMENT_PROGRESS', 'DOWNLOAD_ASSIGNMENT_PROGRESS_CSV', 'DOWNLOAD_GROUP_PROGRESS_CSV', 'VIEW_USER_PROGRESS',
+        -- Event related usage:
+        'ADMIN_EVENT_ATTENDANCE_RECORDED', 'ADMIN_EVENT_BOOKING_CANCELLED', 'ADMIN_EVENT_BOOKING_CONFIRMED',
+        'ADMIN_EVENT_BOOKING_CREATED', 'ADMIN_EVENT_BOOKING_DELETED', 'ADMIN_EVENT_WAITING_LIST_PROMOTION',
+        'EVENT_BOOKING', 'EVENT_BOOKING_CANCELLED', 'EVENT_WAITING_LIST_BOOKING',
+        -- Other useful events:
+        'EQN_EDITOR_LOG', 'USER_REGISTRATION', 'LOG_OUT', 'CHANGE_CONTENT_VERSION', 'DELETE_USER_ACCOUNT',
+        'USER_SCHOOL_CHANGE'
+    );
+    -- Anonymise specific details:
+    -- userIds
+    WITH
+        expanded_details AS (SELECT id, jsonb_array_elements_text(event_details->'userIds') AS elements FROM anonymous.logged_events WHERE event_details->'userIds' IS NOT NULL),
+        new_details AS (SELECT id, jsonb_strip_nulls(jsonb_agg(  anonymise(elements, hash_salt)  )) AS new_event_detail FROM expanded_details GROUP BY id)
+    UPDATE anonymous.logged_events
+        SET
+            event_details=jsonb_set(event_details, '{userIds}', new_event_detail)
+        FROM new_details
+        WHERE anonymous.logged_events.id=new_details.id;
+    -- userId
+    UPDATE anonymous.logged_events
+        SET event_details=jsonb_set(event_details, '{userId}', to_jsonb(anonymise(event_details->>'userId', hash_salt)))
+        WHERE event_details->>'userId' IS NOT NULL;
+    -- groupId
+    UPDATE anonymous.logged_events
+        SET event_details=jsonb_set(event_details, '{groupId}', to_jsonb(anonymise(event_details->>'groupId', hash_salt)))
+        WHERE event_details->>'groupId' IS NOT NULL;
+    -- assignmentId
+    UPDATE anonymous.logged_events
+        SET event_details=jsonb_set(event_details, '{assignmentId}', to_jsonb(anonymise(event_details->>'assignmentId', hash_salt)))
+        WHERE event_details->>'assignmentId' IS NOT NULL;
+    -- token
+    UPDATE anonymous.logged_events
+        SET event_details=event_details - 'token'
+        WHERE event_details->>'token' IS NOT NULL;
+    -- school_other
+    UPDATE anonymous.logged_events
+    SET event_details=jsonb_strip_nulls(event_details - 'oldSchoolOther')
+    WHERE event_details->>'oldSchoolOther' IS NOT NULL;
+    UPDATE anonymous.logged_events
+    SET event_details=jsonb_strip_nulls(event_details - 'newSchoolOther')
+    WHERE event_details->>'newSchoolOther' IS NOT NULL;
 
 RETURN true;
 END;
