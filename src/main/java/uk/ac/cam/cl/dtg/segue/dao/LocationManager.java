1d9ce0ba7f506b2b797b9ff63a21af47d06d6163
Sean Morton <seanmorton@hey.com>
2023-01-31 17:31:08 -0800

    Remove IpInfoDB integration

--- src/main/java/uk/ac/cam/cl/dtg/segue/dao/LocationManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/dao/LocationManager.java
@@ -35,7 +35,6 @@ import com.google.inject.Inject;
 
 import uk.ac.cam.cl.dtg.isaac.dos.LocationHistoryEvent;
 import uk.ac.cam.cl.dtg.isaac.dos.LocationHistory;
-import uk.ac.cam.cl.dtg.util.locations.IPLocationResolver;
 import uk.ac.cam.cl.dtg.util.locations.Location;
 import uk.ac.cam.cl.dtg.util.locations.LocationServerException;
 import uk.ac.cam.cl.dtg.util.locations.PostCodeLocationResolver;
@@ -45,17 +44,16 @@ import uk.ac.cam.cl.dtg.util.locations.PostCodeRadius;
  * LocationHistoryManager. This class is intended to be used to maintain a database of geocoded ip addresses such that
  * we can look up historically where a particular ip address was. This is based on the assumption that ip address
  * allocation change over time.
- * 
+ *
  * @author sac92, ags46
  *
  */
-public class LocationManager implements IPLocationResolver {
+public class LocationManager {
     private static final Logger log = LoggerFactory.getLogger(LocationManager.class);
     private static final int LOCATION_UPDATE_FREQUENCY_IN_DAYS = 30;
     private static final int NON_PERSISTENT_CACHE_TIME_IN_HOURS = 1;
 
     private final LocationHistory dao;
-    private final IPLocationResolver ipLocationResolver;
     private final PostCodeLocationResolver postCodeLocationResolver;
     private final Cache<String, Location> locationCache;
     private final Cache<String, Location> failedLocationCache;
@@ -63,16 +61,13 @@ public class LocationManager implements IPLocationResolver {
     /**
      * @param dao
      *            - the location history data access object.
-     * @param ipLocationResolver
-     *            - the external ip location resolver.
      * @param postCodeLocationResolver
      *            - the external postCode location resolver.
      */
     @Inject
-    public LocationManager(final LocationHistory dao, final IPLocationResolver ipLocationResolver,
+    public LocationManager(final LocationHistory dao,
             final PostCodeLocationResolver postCodeLocationResolver) {
         this.dao = dao;
-        this.ipLocationResolver = ipLocationResolver;
         this.postCodeLocationResolver = postCodeLocationResolver;
 
         // This cache is here to prevent lots of needless look ups to the database.
@@ -83,85 +78,8 @@ public class LocationManager implements IPLocationResolver {
     }
 
     /**
-     * This method will keep our database of ip addresses to locations updated.
-     * 
-     * @param ipAddress
-     *            that should be looked up
-     * @throws SegueDatabaseException
-     *             - if we cannot resolve the location from our database
-     * @throws IOException
-     *             - if there is an IO error
-     */
-    public void refreshLocation(final String ipAddress) throws SegueDatabaseException, IOException {
-        // special case
-        if (ipAddress == null || ipAddress.startsWith("localhost") || ipAddress.contains("0:0:0:0:0:0:0:1")
-                || ipAddress.contains("127.0.0.1")) {
-            // do not record
-            log.debug("Not geocoding ip address as it looks like localhost: " + ipAddress);
-            return;
-        }
-
-        // if it is present in a local cache we have no need to hit the database
-        if (locationCache.getIfPresent(ipAddress) != null || failedLocationCache.getIfPresent(ipAddress) != null) {
-            return;
-        }
-
-        // do we have an existing location for this ip address.
-        LocationHistoryEvent latestByIPAddress = dao.getLatestByIPAddress(ipAddress);
-        Location locationToCache;
-        try {
-            if (latestByIPAddress != null) {
-                locationToCache = latestByIPAddress.getLocationInformation();
-                Calendar locationExpiry = Calendar.getInstance();
-                locationExpiry.setTime(latestByIPAddress.getLastUpdated());
-                locationExpiry.add(Calendar.DATE, LOCATION_UPDATE_FREQUENCY_IN_DAYS);
-
-                if (new Date().after(locationExpiry.getTime())) {
-                    // lookup to see if ip location data is different. If so update it.
-                    log.info("Sending IP Location request to external provider.");
-                    Location locationInformation = ipLocationResolver
-                            .resolveAllLocationInformation(ipAddress);
-
-                    if (locationInformation.equals(latestByIPAddress.getLocationInformation())) {
-                        dao.updateLocationEventDate(latestByIPAddress.getId(), true);
-                        log.debug("Ip address location is the same. Refreshing.");
-
-                    } else {
-                        dao.updateLocationEventDate(latestByIPAddress.getId(), false);
-                        locationToCache = dao.storeLocationEvent(ipAddress, locationInformation)
-                                .getLocationInformation();
-                        log.debug("Location Info Different. Updating to new value.");
-                    }
-                }
-            } else {
-                Location locationInformation = ipLocationResolver.resolveAllLocationInformation(ipAddress);
-                locationToCache = dao.storeLocationEvent(ipAddress, locationInformation).getLocationInformation();
-            }
-
-            this.locationCache.put(ipAddress, locationToCache);
-            log.debug("Location Cache currently has " + locationCache.size() + " ip addresses");
-
-        } catch (LocationServerException e) {
-            log.error(String.format("Unable to resolve location for ip address: %s. Skipping...", ipAddress), e.getMessage());
-            // add it to the ephemeral failed cache for a while so we don't keep asking for an address with no info immediately.
-            this.failedLocationCache.put(ipAddress, new Location(null, 0.0, 0.0));
-        }
-    }
-
-    @Override
-    public Location resolveAllLocationInformation(final String ipAddress) throws IOException, LocationServerException {
-        return ipLocationResolver.resolveAllLocationInformation(ipAddress);
-
-    }
-
-    @Override
-    public Location resolveCountryOnly(final String ipAddress) throws IOException, LocationServerException {
-        return ipLocationResolver.resolveCountryOnly(ipAddress);
-    }
-
-    /**
      * Get the latest location information held by our history.
-     * 
+     *
      * @param ipAddress
      *            that we are interested in.
      * @return latest location info for that ip address. or null if we have no data.
@@ -189,7 +107,7 @@ public class LocationManager implements IPLocationResolver {
 
     /**
      * Get the latest location information held by our history.
-     * 
+     *
      * @param ipAddresses
      *            that we are interested in.
      * @return latest location info for that ip address. or null if we have no data.
@@ -199,11 +117,11 @@ public class LocationManager implements IPLocationResolver {
     public Map<String, Location> getLocationsFromHistory(final Collection<String> ipAddresses)
             throws SegueDatabaseException {
 
-        Map<String, LocationHistoryEvent> latestByIPAddresses = dao.getLatestByIPAddresses(ipAddresses);    
+        Map<String, LocationHistoryEvent> latestByIPAddresses = dao.getLatestByIPAddresses(ipAddresses);
 
         return this.convertToIPLocationMap(latestByIPAddresses);
     }
-    
+
     /**
      * @param fromDate - lower bound for inclusion in the results.
      * @param toDate - upper bound for inclusion in the results.
@@ -214,7 +132,7 @@ public class LocationManager implements IPLocationResolver {
             throws SegueDatabaseException {
         return this.convertToIPLocationMap(dao.getLatestByIPAddresses(fromDate, toDate));
     }
-    
+
     /**
      * @param toConvert the map to convert
      * @return map of ip address to location
@@ -225,7 +143,7 @@ public class LocationManager implements IPLocationResolver {
             resultToReturn.put(e.getKey(), e.getValue().getLocationInformation());
             locationCache.put(e.getKey(), e.getValue().getLocationInformation());
         }
-        
+
         return resultToReturn;
     }
 
@@ -248,5 +166,5 @@ public class LocationManager implements IPLocationResolver {
         return postCodeLocationResolver.filterPostcodesWithinProximityOfPostcode(postCodeAndUserIds,
                 targetPostCode, radius);
     }
-    
+
 }
