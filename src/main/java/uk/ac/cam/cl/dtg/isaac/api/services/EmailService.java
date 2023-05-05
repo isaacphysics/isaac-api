c8eb81f108c4476f9141e37340637428123a11b6
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-05-05 12:39:36 +0100

    Import Strings utilities from correct package in EmailService

--- src/main/java/uk/ac/cam/cl/dtg/isaac/api/services/EmailService.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/api/services/EmailService.java
@@ -18,7 +18,7 @@ package uk.ac.cam.cl.dtg.isaac.api.services;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import feign.FeignException;
-import joptsimple.internal.Strings;
+import com.google.common.base.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.ac.cam.cl.dtg.isaac.api.Constants;
