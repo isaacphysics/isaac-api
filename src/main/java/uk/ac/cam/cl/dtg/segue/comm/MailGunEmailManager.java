4fae3cb55cafbc88a7f9a7a0141036945d355fd5
James Sharkey <jsharkey13@users.noreply.github.com>
2023-06-05 15:06:31 +0100

    Add Prometheus metrics for MailGun API emails
    The "sender" label for these is not an email address since it doesn't
    matter, for these emails, what the Envelope From is as it always uses
    the MailGun API.

--- src/main/java/uk/ac/cam/cl/dtg/segue/comm/MailGunEmailManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/comm/MailGunEmailManager.java
@@ -53,6 +53,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static uk.ac.cam.cl.dtg.isaac.api.Constants.*;
+import static uk.ac.cam.cl.dtg.segue.api.monitors.SegueMetrics.QUEUED_EMAIL;
 
 public class MailGunEmailManager {
 
@@ -186,6 +187,8 @@ public class MailGunEmailManager {
                 .mailgunVariables(templateVariables)
                 .recipientVariables(recipientVariables)
                 .build();
+
+        QUEUED_EMAIL.labels(emailType.name(), "mailgun-api").inc();
 
         return mailgunMessagesApi.sendMessage(globalProperties.getProperty(MAILGUN_DOMAIN), message);
     }
