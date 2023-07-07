aac8353cec54db8b58962cc2359d6c3fc056d168
Matthew Trew <matthew.trew@raspberrypi.org>
2023-07-07 15:15:27 +0100

    Remove unused import

--- src/test/java/uk/ac/cam/cl/dtg/isaac/api/InfoFacadeIT.java
+++ src/test/java/uk/ac/cam/cl/dtg/isaac/api/InfoFacadeIT.java
@@ -1,7 +1,6 @@
 package uk.ac.cam.cl.dtg.isaac.api;
 
 import com.google.common.collect.ImmutableMap;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.ac.cam.cl.dtg.segue.api.InfoFacade;
