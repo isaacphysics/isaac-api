05408aab630d190d65ee176cfa5bea6a3071df82
Skye Purchase <atp45@cam.ac.uk>
2023-11-17 16:01:31 +0000

    Add support for stage field in `glossaryTerm`s
    This can be nullable as Ada will not be using this term

--- src/main/java/uk/ac/cam/cl/dtg/isaac/dos/content/GlossaryTerm.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/dos/content/GlossaryTerm.java
@@ -18,6 +18,8 @@ package uk.ac.cam.cl.dtg.isaac.dos.content;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import uk.ac.cam.cl.dtg.isaac.dto.content.GlossaryTermDTO;
 
+import java.util.Set;
+
 /**
  * Glossary Term object The glossary term object is a specialized form of content and allows the storage of data
  * relating to glossary terms.
@@ -29,6 +31,7 @@ public class GlossaryTerm extends Content {
     protected Content explanation;
     protected boolean autoId;
     protected String examBoard;
+    protected Set<String> stages;
 
     /**
      * Default constructor, required for mappers.
@@ -64,7 +67,20 @@ public class GlossaryTerm extends Content {
         this.examBoard = examBoard;
     }
 
+    public Set<String> getStages() {
+        return stages;
+    }
+
+    public void setStages(Set<String> stages) {
+        this.stages = stages;
+    }
+
     @JsonIgnore
-    public final boolean getAutoId() { return autoId; }
-    public final void setAutoId(final boolean autoId) { this.autoId = autoId; }
+    public final boolean getAutoId() {
+        return autoId;
+    }
+
+    public final void setAutoId(final boolean autoId) {
+        this.autoId = autoId;
+    }
 }
