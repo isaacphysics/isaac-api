2ca0cacdb30c3eaffa87ce3414467a955e5fa156
James Sharkey <jsharkey13@users.noreply.github.com>
2024-01-23 17:18:05 +0000

    Make method behaviour match description
    This endpoint is a good candidate for removal, however, since it appears
    to be unused.

--- src/main/java/uk/ac/cam/cl/dtg/segue/api/GroupsFacade.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/GroupsFacade.java
@@ -301,7 +301,7 @@ public class GroupsFacade extends AbstractSegueFacade {
 
             RegisteredUserDTO userOfInterest = userManager.getUserDTOById(userId);
 
-            List<UserGroupDTO> groups = groupManager.getGroupsByOwner(userOfInterest);
+            List<UserGroupDTO> groups = groupManager.getAllGroupsOwnedAndManagedByUser(userOfInterest, false);
             return Response.ok(groups).cacheControl(getCacheControl(NEVER_CACHE_WITHOUT_ETAG_CHECK, false)).build();
         } catch (NoUserLoggedInException e) {
             return SegueErrorResponse.getNotLoggedInResponse();
