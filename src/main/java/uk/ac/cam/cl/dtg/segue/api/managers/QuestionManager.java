30539595a6edaa6f4d8e5f0df6db7c8f95eb407a
Shavana Peynado <shavypeynado@gmail.com>
2024-03-26 11:44:59 +0000

    shav/368 question-finder-bug (#172)
    * avoid filtering random questions if 'all' stage/examboard selected in contexts
    
    ---------
    
    Co-authored-by: Hannah Whelan <hannah.whelan@and.digital>

--- src/main/java/uk/ac/cam/cl/dtg/segue/api/managers/QuestionManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/managers/QuestionManager.java
@@ -633,9 +633,16 @@ public class QuestionManager {
       List<String> stagesList = new ArrayList<>();
       List<String> examBoardsList = new ArrayList<>();
 
+      boolean containsAllStages = userContexts.stream().anyMatch(uc -> "all".equals(uc.getStage().name()));
+      boolean containsAllExamBoards = userContexts.stream().anyMatch(uc -> "all".equals(uc.getExamBoard().name()));
+
       for (UserContext uc : userContexts) {
-        stagesList.add(uc.getStage().name());
-        examBoardsList.add(uc.getExamBoard().name());
+        if (!containsAllStages) {
+          stagesList.add(uc.getStage().name());
+        }
+        if (!containsAllExamBoards) {
+          examBoardsList.add(uc.getExamBoard().name());
+        }
       }
 
       gameFilter = new GameFilter(
