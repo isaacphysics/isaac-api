4a429f26aee7bef2f8adff5d528eeece2a1bff2b
James Sharkey <jsharkey13@users.noreply.github.com>
2024-03-26 15:02:36 +0000

    Update archive script to use temporary table

--- src/main/resources/db_scripts/scheduled/archive-users-transaction.sql
+++ src/main/resources/db_scripts/scheduled/archive-users-transaction.sql
@@ -3,18 +3,26 @@
 -- Repeatable read is required to ensure the same users are moved into archived as are deleted.
 BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 
+CREATE TEMPORARY TABLE archive_in_progress(user_id INTEGER) ON COMMIT DROP;
+
 -- We need to use coalesce(last_seen, last_updated, registration_date) since some accounts have NULL for last_seen,
 -- if they were never used immediately after registration.
-INSERT INTO archived_users SELECT id, family_name, given_name, email, date_of_birth, school_other, CURRENT_TIMESTAMP FROM users
+INSERT INTO archive_in_progress SELECT id FROM users
 WHERE coalesce(last_seen, last_updated, registration_date) <= CURRENT_DATE - INTERVAL '4 YEARS' AND NOT deleted;
 
--- This mirrors PgUsers::deleteUserAccount, particularly the logic in PgUsers::removePIIFromUserDO to remove PII:
+-- Archive the PII.
+INSERT INTO archived_users SELECT id, family_name, given_name, email, date_of_birth, school_other, CURRENT_TIMESTAMP FROM users
+WHERE id IN (SELECT user_id FROM archive_in_progress);
+
+-- The following mirrors PgUsers::deleteUserAccount, particularly PgUsers::removePIIFromUserDO.
+
+-- Remove PII from users table.
 UPDATE users SET family_name=NULL, given_name=NULL,
                  email=gen_random_uuid(),
                  email_to_verify=NULL, email_verification_token=NULL,
                  date_of_birth=date_trunc('MONTH', date_of_birth),
                  school_other=NULL,
                  deleted=TRUE
-WHERE coalesce(last_seen, last_updated, registration_date) <= CURRENT_DATE - INTERVAL '4 YEARS' AND NOT deleted;
+WHERE id IN (SELECT user_id FROM archive_in_progress);
 
 COMMIT;
