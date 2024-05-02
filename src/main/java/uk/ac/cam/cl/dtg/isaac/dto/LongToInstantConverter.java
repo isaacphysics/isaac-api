2db4b9459481b2be650c2afc354c381a6ba14298
charlescolbourn <charles.colbourn@and.digital>
2024-05-02 14:28:04 +0100

    formatted with checkstyle

--- src/main/java/uk/ac/cam/cl/dtg/isaac/dto/LongToInstantConverter.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/dto/LongToInstantConverter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.util.StdConverter;
 import java.time.Instant;
 
 public class LongToInstantConverter extends StdConverter<Long, Instant> {
+  @Override
   public Instant convert(final Long value) {
     return Instant.ofEpochMilli(value);
   }
