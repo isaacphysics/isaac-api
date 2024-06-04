ca9941e464b9b5b6066af215b786d66fff869b31
charlescolbourn <charles.colbourn@and.digital>
2024-06-04 10:26:31 +0100

    Removed deprecation annotation as it's noisy.

--- src/main/java/uk/ac/cam/cl/dtg/segue/api/useralerts/IAlertListener.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/useralerts/IAlertListener.java
@@ -1,6 +1,5 @@
 package uk.ac.cam.cl.dtg.segue.api.useralerts;
 
-@Deprecated
 public interface IAlertListener {
   // Useful listener methods are static because each user can have multiple alert listeners (one for each tab).
   // We can't get the same polymorphism gains for static methods so, for now, we probably don't need this interface.
