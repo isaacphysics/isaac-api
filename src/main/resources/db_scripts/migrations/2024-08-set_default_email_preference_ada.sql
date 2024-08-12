2f0763000ff93afa5a5e3b98e4f6e2492e4c7381
Matthew Trew <matthew.trew@raspberrypi.org>
2024-08-12 15:29:00 +0100

    Correct query to update preferences regardless of existing

--- src/main/resources/db_scripts/migrations/2024-08-set_default_email_preference_ada.sql
+++ src/main/resources/db_scripts/migrations/2024-08-set_default_email_preference_ada.sql
@@ -1,7 +1,4 @@
-INSERT INTO public.user_preferences (user_id, preference_type, preference_name, preference_value, last_updated)
-SELECT DISTINCT user_id, 'EMAIL_PREFERENCE' AS preference_type, 'ASSIGNMENTS' AS preference_name, true AS preference_value, now() as last_updated
-FROM public.user_preferences
-WHERE user_id NOT IN (
-    SELECT user_id FROM public.user_preferences
-    WHERE preference_type = 'EMAIL_PREFERENCE'
-);
+INSERT INTO user_preferences
+SELECT id, 'EMAIL_PREFERENCE', 'ASSIGNMENTS', true, CURRENT_TIMESTAMP
+FROM users
+WHERE users.id NOT IN (SELECT user_id FROM user_preferences WHERE preference_type = 'EMAIL_PREFERENCE');
