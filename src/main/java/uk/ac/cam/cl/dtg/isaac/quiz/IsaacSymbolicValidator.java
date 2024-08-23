20067ce30a86bbcddf64da1f9db3e25b7e47fd8f
Alex Lewin <sl2281@cam.ac.uk>
2024-08-23 12:21:17 +0100

    Make feedback messages consistent
    Set some common feedback messages to inherit from a constant string so that they have consistent punctuation etc.

--- src/main/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacSymbolicValidator.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacSymbolicValidator.java
@@ -34,6 +34,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 
+import static uk.ac.cam.cl.dtg.isaac.api.Constants.*;
+
 /**
  * Validator that provides functionality to validate symbolic questions.
  *
@@ -95,13 +97,13 @@ public class IsaacSymbolicValidator implements IValidator {
             log.error("Question does not have any answers. " + question.getId() + " src: "
                     + question.getCanonicalSourceFile());
 
-            feedback = new Content("This question does not have any correct answers");
+            feedback = new Content(FEEDBACK_NO_CORRECT_ANSWERS);
         }
 
         // STEP 1: Did they provide an answer?
 
         if (null == feedback && (null == submittedFormula.getPythonExpression() || submittedFormula.getPythonExpression().isEmpty())) {
-            feedback = new Content("You did not provide an answer");
+            feedback = new Content(FEEDBACK_NO_ANSWER_PROVIDED);
         }
 
         // STEP 2: Otherwise, Does their answer match a choice exactly?
