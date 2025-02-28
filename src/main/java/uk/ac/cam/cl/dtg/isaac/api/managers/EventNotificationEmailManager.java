a1f3647a71e83af475efd9fafb5849fb79c582c8
Parth Sharma <32276128+Parth097@users.noreply.github.com>
2025-02-28 09:29:48 +0000

    Adding functionality for 24 hours (#326)
    * Adding functionality for 24 hours
    
    * adding 24 hour delay to EventFeedbackEmailJob
    
    * removing previous non working implementations
    
    * Amended functionality, event feedback email to be sent out 24 hours after the event
    
    * pre event emails for both confirmed and attended status, post for only attended
    
    * Updated comment

--- src/main/java/uk/ac/cam/cl/dtg/isaac/api/managers/EventNotificationEmailManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/api/managers/EventNotificationEmailManager.java
@@ -13,6 +13,7 @@ import com.google.inject.Inject;
 import java.time.Instant;
 import java.time.LocalTime;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -106,13 +107,12 @@ public class EventNotificationEmailManager {
     }
   }
 
-  private void commitAndSendStatusFilteredEmail(IsaacEventPageDTO event, String emailKeyPostfix, String templateId)
+  private void commitAndSendReminderEmail(IsaacEventPageDTO event, String emailKeyPostfix, String templateId)
       throws SegueDatabaseException {
     String emailKey = String.format("%s@%s", event.getId(), emailKeyPostfix);
     /*
-    Confirmed and Attended statuses are both included for both pre- and post-event emails.
+    Confirmed and Attended statuses are both included for pre-event emails.
     Pre-event emails include the attended status in case the events team have pre-emptively marked someone as attended.
-    Post-event emails include the confirmed status in case the events team don't update the status to attended in time.
      */
     List<BookingStatus> bookingStatuses = Arrays.asList(BookingStatus.CONFIRMED, BookingStatus.ATTENDED);
     if (pgScheduledEmailManager.commitToSchedulingEmail(emailKey)) {
@@ -148,14 +148,26 @@ public class EventNotificationEmailManager {
             continue;
           }
           if (event.getDate().isAfter(endOfToday)) {
-            commitAndSendStatusFilteredEmail(event, "pre", "event_reminder");
+            commitAndSendReminderEmail(event, "pre", "event_reminder");
           } else {
-            commitAndSendStatusFilteredEmail(event, "presameday", "event_reminder_same_day");
+            commitAndSendReminderEmail(event, "presameday", "event_reminder_same_day");
           }
         }
       }
     } catch (ContentManagerException | SegueDatabaseException e) {
       log.error("Failed to send scheduled event reminder emails: ", e);
+    }
+  }
+
+  private void commitAndSendFeedbackEmail(IsaacEventPageDTO event, String emailKeyPostfix, String templateId)
+      throws SegueDatabaseException {
+    String emailKey = String.format("%s@%s", event.getId(), emailKeyPostfix);
+    /*
+    Attended statuses included for post-event emails.
+     */
+    List<BookingStatus> bookingStatuses = List.of(BookingStatus.ATTENDED);
+    if (pgScheduledEmailManager.commitToSchedulingEmail(emailKey)) {
+      this.sendBookingStatusFilteredEmailForEvent(event, templateId, bookingStatuses);
     }
   }
 
@@ -185,22 +197,23 @@ public class EventNotificationEmailManager {
           if (EventStatus.CANCELLED.equals(event.getEventStatus())) {
             continue;
           }
-          // Event end date (if present) is today or before, else event date is today or before
-          boolean endDateToday =
-              event.getEndDate() != null && event.getEndDate().isBefore(Instant.now());
-          boolean noEndDateAndStartDateToday =
-              event.getEndDate() == null && event.getDate().isBefore(Instant.now());
-          if (endDateToday || noEndDateAndStartDateToday) {
+          // Event end date (if present) is yesterday or before, else event date is yesterday, or before
+          // We want to send the event_feedback email 24 hours after the event
+          boolean endDateYesterday =
+              event.getEndDate() != null && event.getEndDate().isBefore(Instant.now().minus(1, ChronoUnit.DAYS));
+          boolean noEndDateAndStartDateYesterday =
+              event.getEndDate() == null && event.getDate().isBefore(Instant.now().minus(1, ChronoUnit.DAYS));
+          if (endDateYesterday || noEndDateAndStartDateYesterday) {
             List<ExternalReference> postResources = event.getPostResources();
             boolean postResourcesPresent =
                 postResources != null && !postResources.isEmpty() && !postResources.contains(null);
             if (postResourcesPresent) {
-              commitAndSendStatusFilteredEmail(event, "post", "event_feedback");
+              commitAndSendFeedbackEmail(event, "post", "event_feedback");
             }
             // New logic for sending survey email
             boolean shouldSendSurvey = true; // Define your criteria for sending surveys
             if (shouldSendSurvey) {
-              commitAndSendStatusFilteredEmail(event, "survey", "event_survey");
+              commitAndSendFeedbackEmail(event, "survey", "event_survey");
             }
           }
         }
