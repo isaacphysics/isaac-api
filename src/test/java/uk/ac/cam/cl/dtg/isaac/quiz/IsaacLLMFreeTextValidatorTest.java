812119f1e4b22195a286e3fcb822ba9733829fd8
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2025-03-28 17:02:41 +0000

    Add example marking formula functions

--- src/test/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacLLMFreeTextValidatorTest.java
+++ src/test/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacLLMFreeTextValidatorTest.java
@@ -17,38 +17,23 @@ package uk.ac.cam.cl.dtg.isaac.quiz;
 
 import com.azure.ai.openai.OpenAIClient;
 import com.azure.ai.openai.OpenAIClientBuilder;
-import com.google.api.client.util.Lists;
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.reflect.Whitebox;
-import org.slf4j.Logger;
 import uk.ac.cam.cl.dtg.isaac.dos.IsaacLLMFreeTextQuestion;
-import uk.ac.cam.cl.dtg.isaac.dos.IsaacNumericQuestion;
-import uk.ac.cam.cl.dtg.isaac.dos.IsaacQuickQuestion;
-import uk.ac.cam.cl.dtg.isaac.dos.QuantityValidationResponse;
-import uk.ac.cam.cl.dtg.isaac.dos.QuestionValidationResponse;
-import uk.ac.cam.cl.dtg.isaac.dos.content.Choice;
-import uk.ac.cam.cl.dtg.isaac.dos.content.Content;
+import uk.ac.cam.cl.dtg.isaac.dos.content.LLMFreeTextMarkSchemeEntry;
 import uk.ac.cam.cl.dtg.isaac.dos.content.LLMMarkingConstant;
 import uk.ac.cam.cl.dtg.isaac.dos.content.LLMMarkingExpression;
 import uk.ac.cam.cl.dtg.isaac.dos.content.LLMMarkingFunction;
 import uk.ac.cam.cl.dtg.isaac.dos.content.LLMMarkingVariable;
-import uk.ac.cam.cl.dtg.isaac.dos.content.Quantity;
-import uk.ac.cam.cl.dtg.segue.configuration.SegueGuiceConfigurationModule;
 import uk.ac.cam.cl.dtg.util.AbstractConfigLoader;
-import uk.ac.cam.cl.dtg.util.PropertiesLoader;
 import uk.ac.cam.cl.dtg.util.YamlLoader;
 
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -86,7 +71,10 @@ public class IsaacLLMFreeTextValidatorTest {
         // Set up a question object worth one mark:
         llmFreeTextQuestionOneMark = new IsaacLLMFreeTextQuestion();
         llmFreeTextQuestionOneMark.setMaxMarks(1);
-        llmFreeTextQuestionOneMark.setMarkingFormula(defaultMarkingFormula());
+        LLMFreeTextMarkSchemeEntry hi = new LLMFreeTextMarkSchemeEntry();
+        List<LLMFreeTextMarkSchemeEntry> hiya = Arrays.asList(hi);
+        llmFreeTextQuestionOneMark.setMarkScheme(hiya);
+        llmFreeTextQuestionOneMark.setMarkingFormula(defaultMarkingFormula(llmFreeTextQuestionOneMark.getMarkScheme()));
 
         // Set up a question object worth two marks:
         llmFreeTextQuestionTwoMarks = new IsaacLLMFreeTextQuestion();
@@ -143,8 +131,7 @@ public class IsaacLLMFreeTextValidatorTest {
 
     */
 
-
-    //  ---------- Helper methods to test internal functionality of the validator class ----------
+    //  --- LLMMarkingElement Syntax Sugar ---
 
     private LLMMarkingFunction markingFormulaFunction(String name, List<LLMMarkingExpression> args) {
         LLMMarkingFunction function = new LLMMarkingFunction();
@@ -171,18 +158,73 @@ public class IsaacLLMFreeTextValidatorTest {
         return variable;
     }
 
-    private LLMMarkingFunction defaultMarkingFormula() {
-        // TODO get marking variables from markscheme
+    //  --- Useful Example Marking Formulas ---
+
+    // default = MIN(maxMarks, SUM(... All Marks ...))
+    private LLMMarkingFunction defaultMarkingFormula(List<LLMFreeTextMarkSchemeEntry> markScheme) {
+        List<LLMMarkingExpression> markingVariables = new LinkedList<>();
+        for (LLMFreeTextMarkSchemeEntry mark : markScheme) {
+            markingVariables.add(markingFormulaVariable(mark.getJsonField()));
+        }
+
         return markingFormulaFunction("MIN",
                 Arrays.asList(
                         markingFormulaVariable("maxMarks"),
                         markingFormulaFunction("SUM",
-                                Arrays.asList(
-                                        markingFormulaVariable("x"),
-                                        markingFormulaVariable("y"),
-                                        markingFormulaVariable("z")
-                                ))
+                                markingVariables
+                        )
                 )
         );
+    }
+
+    // advantageDisadvantage = SUM(MAX(... All Advantage Marks ...), MAX(... All Disadvantage Marks ...))
+    private LLMMarkingFunction advantageDisadvantageMarkingFormula(List<LLMFreeTextMarkSchemeEntry> markScheme) {
+        List<LLMMarkingExpression> markingVariablesDisadvantage = new LinkedList<>();
+        List<LLMMarkingExpression> markingVariablesAdvantage = new LinkedList<>();
+        for (LLMFreeTextMarkSchemeEntry mark : markScheme) {
+            String variableName = mark.getJsonField();
+            if (variableName.contains("disadvantage")) {
+                markingVariablesDisadvantage.add(markingFormulaVariable(variableName));
+            } else if (variableName.contains("advantage")) {
+                markingVariablesAdvantage.add(markingFormulaVariable(variableName));
+            }
+        }
+
+        return markingFormulaFunction("SUM",
+                Arrays.asList(
+                        markingFormulaFunction("MAX",
+                                markingVariablesAdvantage
+                        ),
+                        markingFormulaFunction("SUM",
+                                markingVariablesDisadvantage
+                        )
+                )
+        );
+    }
+
+    // pointExplanation = SUM(MAX(pointOne, pointTwo, ... pointN), MAX(MIN(pointOne, explanationOne), MIN(pointTwo, explanationTwo), ... MIN(pointN, explanationN))
+    private LLMMarkingFunction pointExplanationMarkingFormula(List<LLMFreeTextMarkSchemeEntry> markScheme) {
+        List<LLMMarkingExpression> markingVariablesPoint = new LinkedList<>();
+        List<LLMMarkingExpression> markingVariablesPointExplanation = new LinkedList<>();
+        for (LLMFreeTextMarkSchemeEntry mark : markScheme) {
+            String variableName = mark.getJsonField();
+            if (variableName.contains("point")) {
+                markingVariablesPoint.add(markingFormulaVariable(variableName));
+             //   markingVariablesPointExplanation = markScheme.stream().filter(mark -> mark.getJsonField().contains("explanation" + variableName.replace("point", "")).) // GIVE UP - JUST HARDCODE IT
+             //   markingVariablesPointExplanation.add(markingFormulaVariable(variableName));
+            }
+        }
+
+
+
+        return markingFormulaFunction("SUM",
+                Arrays.asList(
+                        markingFormulaFunction("MAX",
+                                markingVariablesPoint
+                        ),
+
+                )
+
+        )
     }
 }
