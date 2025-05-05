885d046ca257eb5ebaa93e42cd57987e45703ea7
Barna Magyarkuti <bmagyarkuti@gmail.com>
2025-05-05 11:55:06 +0000

    use parametrised tests

--- src/test/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacLLMFreeTextValidatorTest.java
+++ src/test/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacLLMFreeTextValidatorTest.java
@@ -23,11 +23,16 @@ import com.azure.ai.openai.models.ChatResponseMessage;
 
 import org.easymock.EasyMock;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
 import uk.ac.cam.cl.dtg.isaac.Mark;
 import uk.ac.cam.cl.dtg.isaac.dos.IsaacLLMFreeTextQuestion;
@@ -38,6 +43,8 @@ import uk.ac.cam.cl.dtg.isaac.dos.content.Question;
 import uk.ac.cam.cl.dtg.util.YamlLoader;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -54,152 +61,122 @@ import static uk.ac.cam.cl.dtg.segue.api.Constants.*;
 import static uk.ac.cam.cl.dtg.isaac.QuestionFactory.*;
 import static uk.ac.cam.cl.dtg.isaac.quiz.Helpers.*;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ OpenAIClient.class, ChatCompletions.class, ChatChoice.class, ChatResponseMessage.class })
+@RunWith(Enclosed.class)
 public class IsaacLLMFreeTextValidatorTest {
-    @Test
-    @DisplayName("A one-mark answer for a default marking formula one-mark question gets recognised as correct")
-    public void isaacLLMFreeTextValidator_OneMarkQuestionOneMarkAnswer_MarkSchemeShouldIncludeMark() throws Exception {
-        var breakdown = mark().setReasonFoo(1);
-        var response = validate(genericOneMarkQuestion(), breakdown);
-        expectMark(response, CORRECT, 1, breakdown);
+    @RunWith(PowerMockRunner.class)
+    @PowerMockRunnerDelegate(Parameterized.class)
+    @PrepareForTest({ OpenAIClient.class, ChatCompletions.class, ChatChoice.class, ChatResponseMessage.class })
+    @DisplayName("Test that a mark is awarded based on the marking formula")
+    public static class FormulaBasedMarking {
+        @Parameter(0)
+        public String testDescription;
+        @Parameter(1)
+        public IsaacLLMFreeTextQuestion question;
+        @Parameter(2)
+        public Mark response;
+        @Parameter(3)
+        public boolean expectedResult;
+        @Parameter(4)
+        public int expectedMark;
+
+        @Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][] {
+                    { "A one-mark answer for a default marking formula one-mark question gets recognised as correct",
+                            genericOneMarkQuestion(), mark().setReasonFoo(1), CORRECT, 1 },
+                    { "A three-mark answer for a default marking formula one-mark question gets recognised as correct",
+                            genericOneMarkQuestion(), mark().setReasonFoo(1).setReasonBar(1).setReasonFizz(1), CORRECT,
+                            1 },
+                    { "A zero-mark answer for a one-mark question gets recognised as incorrect",
+                            genericOneMarkQuestion(), mark(), INCORRECT, 0 },
+
+                    { "A two-mark answer for a default marking formula two-mark question gets recognised as correct",
+                            genericTwoMarkQuestion(), mark().setReasonFoo(1).setReasonBar(1), CORRECT, 2 },
+                    { "A one-mark answer for a default marking formula two-mark question receives exactly one mark",
+                            genericTwoMarkQuestion(), mark().setReasonFoo(1), CORRECT, 1 },
+
+                    { "An answer containing an advantage and a disadvantage mark for a two-mark advantage/disadvantage question receives two marks",
+                            advantageQuestion(), advantageMark().setAdvantageOne(1).setDisadvantageOne(1), CORRECT, 2 },
+                    { "An answer containing only a disadvantage mark for a two-mark advantage/disadvantage question receives one mark",
+                            advantageQuestion(), advantageMark().setDisadvantageOne(1), CORRECT, 1 },
+                    { "An answer containing two advantage marks for a two-mark advantage/disadvantage question receives one mark",
+                            advantageQuestion(), advantageMark().setAdvantageOne(1).setAdvantageTwo(1), CORRECT, 1 },
+
+                    { "An answer containing a point and matching explanation for a two-mark point/explanation question receives two marks",
+                            pointExplanationQuestion(), pointMark().setPointOne(1).setExplanationOne(1), CORRECT, 2 },
+                    { "An answer containing an explanation without a matching point for a two-mark point/explanation question receives zero marks",
+                            pointExplanationQuestion(), pointMark().setExplanationOne(1), INCORRECT, 0 },
+                    { "An answer containing a point and a mismatched explanation for a two-mark point/explanation question receives one mark",
+                            pointExplanationQuestion(), pointMark().setPointOne(1).setExplanationTwo(1), CORRECT, 1 }
+            });
+        }
+
+        @Test
+        public void test() throws Exception {
+            var resp = validate(question, response);
+            expectMark(resp, expectedResult, expectedMark, response);
+        }
     }
 
-    @Test
-    @DisplayName("A three-mark answer for a default marking formula one-mark question gets recognised as correct")
-    public void isaacLLMFreeTextValidator_OneMarkQuestionThreeMarkAnswer_MarkSchemeShouldIncludeMarks()
-            throws Exception {
-        var breakdown = mark().setReasonFoo(1).setReasonBar(1).setReasonFizz(1);
-        var response = validate(genericOneMarkQuestion(), breakdown);
-        expectMark(response, CORRECT, 1, breakdown);
-    }
+    @RunWith(PowerMockRunner.class)
+    @PrepareForTest({ OpenAIClient.class, ChatCompletions.class, ChatChoice.class, ChatResponseMessage.class })
+    @DisplayName("Test application behaviour in case of errors.")
+    public static class ErrorHandling {
+        @Test
+        @DisplayName("A response from the client not in the expected json format returns zero marks")
+        public void isaacLLMFreeTextValidator_ResponseInvalidFormat_MarkSchemeShouldIncludeNoMarks() throws Exception {
+            var response = validate(genericOneMarkQuestion(), "Not a valid JSON response");
+            expectMark(response, INCORRECT, 0, mark());
+        }
 
-    @Test
-    @DisplayName("A zero-mark answer for a one-mark question gets recognised as incorrect")
-    public void isaacLLMFreeTextValidator_OneMarkQuestionZeroMarkAnswer_MarkSchemeShouldIncludeNoMarks()
-            throws Exception {
-        var response = validate(genericOneMarkQuestion(), mark());
-        expectMark(response, INCORRECT, 0, mark());
-    }
+        @Test
+        @DisplayName("An answer exceeding the maximum answer length is handled with an exception")
+        public void isaacLLMFreeTextValidator_AnswerOverLengthLimit_ExceptionShouldBeThrown() throws Exception {
+            var maxAnswerLength = getIntTestProperty(LLM_MARKER_MAX_ANSWER_LENGTH, 4096);
+            var answr = answer(String.join("", Collections.nCopies((maxAnswerLength / 10 + 1), "Repeat Me ")));
 
-    @Test
-    @DisplayName("A two-mark answer for a default marking formula two-mark question gets recognised as correct")
-    public void isaacLLMFreeTextValidator_TwoMarkQuestionTwoMarkAnswer_MarkSchemeShouldIncludeMarks() throws Exception {
-        var breakdown = mark().setReasonFoo(1).setReasonBar(1);
-        var response = validate(genericTwoMarkQuestion(), breakdown);
-        expectMark(response, CORRECT, 2, breakdown);
-    }
+            var exception = assertThrows(
+                    IllegalArgumentException.class,
+                    () -> createValidator(client()).validateQuestionResponse(genericOneMarkQuestion(), answr));
 
-    @Test
-    @DisplayName("A one-mark answer for a default marking formula two-mark question receives exactly one mark")
-    public void isaacLLMFreeTextValidator_TwoMarkQuestionOneMarkAnswer_MarkSchemeShouldIncludeMarks() throws Exception {
-        var breakdown = mark().setReasonFoo(1);
-        var response = validate(genericTwoMarkQuestion(), breakdown);
-        expectMark(response, CORRECT, 1, breakdown);
-    }
+            assertEquals("Answer is too long for LLM free-text question marking", exception.getMessage());
+        }
 
-    @Test
-    @DisplayName("An answer containing an advantage and a disadvantage mark for a two-mark advantage/disadvantage question receives two marks")
-    public void isaacLLMFreeTextValidator_AdvantageDisadvantageQuestionADMarks_MarkTotalShouldBeTwo() throws Exception {
-        var breakdown = advantageMark().setAdvantageOne(1).setDisadvantageOne(1);
-        var response = validate(advantageQuestion(), breakdown);
-        expectMark(response, CORRECT, 2, breakdown);
-    }
+        @Test
+        @DisplayName("Error from the client (e.g. timeout, rate limit, out of credits) is handled with an exception")
+        public void isaacLLMFreeTextValidator_ResponseError_ExceptionShouldBeThrown() {
+            var clnt = createMock(OpenAIClient.class);
+            EasyMock.expect(clnt.getChatCompletions(anyString(), isA(ChatCompletionsOptions.class)))
+                    .andThrow(new RuntimeException("Test OpenAI Exception"));
+            replay(clnt);
 
-    @Test
-    @DisplayName("An answer containing only a disadvantage mark for a two-mark advantage/disadvantage question receives one mark")
-    public void isaacLLMFreeTextValidator_AdvantageDisadvantageQuestionDMarks_MarkTotalShouldBeOne() throws Exception {
-        var breakdown = advantageMark().setDisadvantageOne(1);
-        var response = validate(advantageQuestion(), breakdown);
-        expectMark(response, CORRECT, 1, breakdown);
-    }
+            assertEquals("We are having problems marking LLM marked questions. Please try again later!",
+                    assertThrows(
+                            ValidatorUnavailableException.class,
+                            () -> createValidator(clnt).validateQuestionResponse(genericOneMarkQuestion(), answer("")))
+                            .getMessage());
+        }
 
-    @Test
-    @DisplayName("An answer containing two advantage marks for a two-mark advantage/disadvantage question receives one mark")
-    public void isaacLLMFreeTextValidator_AdvantageDisadvantageQuestionAAMarks_MarkTotalShouldBeOne() throws Exception {
-        var breakdown = advantageMark().setAdvantageOne(1).setAdvantageTwo(1);
-        var response = validate(advantageQuestion(), breakdown);
-        expectMark(response, CORRECT, 1, breakdown);
-    }
+        @Test
+        @DisplayName("Invalid question (i.e. missing maxMarks field/not LLMFreeTextQuestion) is handled with an exception")
+        public void isaacLLMFreeTextValidator_InvalidQuestion_ExceptionShouldBeThrown() {
+            IsaacLLMFreeTextQuestion invalidQuestionFields = createLLMFreeTextQuestion(null, null, null, null);
+            Question invalidQuestionType = new Question();
 
-    @Test
-    @DisplayName("An answer containing a point and matching explanation for a two-mark point/explanation question receives two marks")
-    public void isaacLLMFreeTextValidator_PointExplanationQuestionPEMarks_MarkTotalShouldBeTwo() throws Exception {
-        var breakdown = pointMark().setPointOne(1).setExplanationOne(1);
-        var response = validate(pointExplanationQuestion(), breakdown);
-        expectMark(response, CORRECT, 2, breakdown);
-    }
+            assertEquals("This question cannot be answered correctly",
+                    assertThrows(IllegalArgumentException.class, () -> validate(invalidQuestionFields, ""))
+                            .getMessage());
 
-    @Test
-    @DisplayName("An answer containing an explanation without a matching point for a two-mark point/explanation question receives zero marks")
-    public void isaacLLMFreeTextValidator_PointExplanationQuestionEMark_MarkTotalShouldBeZero() throws Exception {
-        var breakdown = pointMark().setExplanationOne(1);
-        var response = validate(pointExplanationQuestion(), breakdown);
-        expectMark(response, INCORRECT, 0, breakdown);
-    }
-
-    @Test
-    @DisplayName("An answer containing a point and a mismatched explanation for a two-mark point/explanation question receives one mark")
-    public void isaacLLMFreeTextValidator_PointExplanationQuestionPEMismatchMarks_MarkTotalShouldBeOne()
-            throws Exception {
-        var breakdown = pointMark().setPointOne(1).setExplanationTwo(1);
-        var response = validate(pointExplanationQuestion(), breakdown);
-        expectMark(response, CORRECT, 1, breakdown);
-    }
-
-    @Test
-    @DisplayName("A response from the client not in the expected json format returns zero marks")
-    public void isaacLLMFreeTextValidator_ResponseInvalidFormat_MarkSchemeShouldIncludeNoMarks() throws Exception {
-        var response = validate(genericOneMarkQuestion(), "Not a valid JSON response");
-        expectMark(response, INCORRECT, 0, mark());
-    }
-
-    @Test
-    @DisplayName("An answer exceeding the maximum answer length is handled with an exception")
-    public void isaacLLMFreeTextValidator_AnswerOverLengthLimit_ExceptionShouldBeThrown() throws Exception {
-        var maxAnswerLength = getIntTestProperty(LLM_MARKER_MAX_ANSWER_LENGTH, 4096);
-        var answr = answer(String.join("", Collections.nCopies((maxAnswerLength / 10 + 1), "Repeat Me ")));
-
-        var exception = assertThrows(
-                IllegalArgumentException.class,
-                () -> createValidator(client()).validateQuestionResponse(genericOneMarkQuestion(), answr));
-
-        assertEquals("Answer is too long for LLM free-text question marking", exception.getMessage());
-    }
-
-    @Test
-    @DisplayName("Error from the client (e.g. timeout, rate limit, out of credits) is handled with an exception")
-    public void isaacLLMFreeTextValidator_ResponseError_ExceptionShouldBeThrown() {
-        var clnt = createMock(OpenAIClient.class);
-        EasyMock.expect(clnt.getChatCompletions(anyString(), isA(ChatCompletionsOptions.class)))
-                .andThrow(new RuntimeException("Test OpenAI Exception"));
-        replay(clnt);
-
-        assertEquals("We are having problems marking LLM marked questions. Please try again later!",
-                assertThrows(
-                        ValidatorUnavailableException.class,
-                        () -> createValidator(clnt).validateQuestionResponse(genericOneMarkQuestion(), answer("")))
-                        .getMessage());
-    }
-
-    @Test
-    @DisplayName("Invalid question (i.e. missing maxMarks field/not LLMFreeTextQuestion) is handled with an exception")
-    public void isaacLLMFreeTextValidator_InvalidQuestion_ExceptionShouldBeThrown() {
-        IsaacLLMFreeTextQuestion invalidQuestionFields = createLLMFreeTextQuestion(null, null, null, null);
-        Question invalidQuestionType = new Question();
-
-        assertEquals("This question cannot be answered correctly",
-                assertThrows(IllegalArgumentException.class, () -> validate(invalidQuestionFields, "")).getMessage());
-
-        assertEquals(invalidQuestionType.getId() + " is not a LLM free-text question",
-                assertThrows(IllegalArgumentException.class, () -> validate(invalidQuestionType, "")).getMessage());
+            assertEquals(invalidQuestionType.getId() + " is not a LLM free-text question",
+                    assertThrows(IllegalArgumentException.class, () -> validate(invalidQuestionType, "")).getMessage());
+        }
     }
 }
 
 class Helpers {
     public static boolean CORRECT = true;
     public static boolean INCORRECT = false;
-    
+
     public static LLMFreeTextQuestionValidationResponse validate(Question question, Mark response) throws Exception {
         return validate(question, response.toJSON());
     }
