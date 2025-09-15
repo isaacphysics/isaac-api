8d5ba6a17cc724c3b6c91995bf8ec6bc206133ff
Alex Lewin <sl2281@cam.ac.uk>
2025-09-15 14:02:54 +0100

    Remove email addresses from UserSummaryDTOs in EventBookingDTOs

--- src/main/java/uk/ac/cam/cl/dtg/util/mappers/UserMapper.java
+++ src/main/java/uk/ac/cam/cl/dtg/util/mappers/UserMapper.java
@@ -1,6 +1,8 @@
 package uk.ac.cam.cl.dtg.util.mappers;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.SubclassMapping;
 import org.mapstruct.factory.Mappers;
@@ -13,6 +15,25 @@ import uk.ac.cam.cl.dtg.isaac.dto.users.UserSummaryWithGroupMembershipDTO;
 @Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    default <T> T map(UserSummaryDTO source, Class<T> targetClass) {
+        if (targetClass.equals(UserSummaryWithGroupMembershipDTO.class)) {
+            return (T) mapUserSummaryDTOtoUserSummaryWithGroupMembershipDTO(source);
+        } else if (targetClass.equals(UserSummaryDTO.class)) {
+            return (T) mapExtendedUserSummaryDTOtoBaseUserSummaryDTO(source);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Mapping(target = "groupMembershipInformation", ignore = true)
+    @SubclassMapping(source = UserSummaryForAdminUsersDTO.class, target = UserSummaryWithGroupMembershipDTO.class)
+    @SubclassMapping(source = UserSummaryWithEmailAddressDTO.class, target = UserSummaryWithGroupMembershipDTO.class)
+    @SubclassMapping(source = UserSummaryWithGroupMembershipDTO.class, target = UserSummaryWithGroupMembershipDTO.class)
+    UserSummaryWithGroupMembershipDTO mapUserSummaryDTOtoUserSummaryWithGroupMembershipDTO(UserSummaryDTO source);
+
+    @BeanMapping(resultType = UserSummaryDTO.class)
+    UserSummaryDTO mapExtendedUserSummaryDTOtoBaseUserSummaryDTO(UserSummaryDTO source);
 
     @Named("copyUserSummaryDTO")
     @SubclassMapping(source = UserSummaryForAdminUsersDTO.class, target = UserSummaryForAdminUsersDTO.class)
