92203a6eb18d9019439cc2387b5376cd27aaa234
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2025-10-22 12:12:23 +0100

    Convert database migration query to split by dates

--- src/main/resources/db_scripts/migrations/2025-10-questions-attempts-add-mark-field.sql
+++ src/main/resources/db_scripts/migrations/2025-10-questions-attempts-add-mark-field.sql
@@ -1,11 +1,34 @@
 ALTER TABLE question_attempts
-ADD marks integer GENERATED ALWAYS AS (
-    CASE
-        WHEN
-            (question_attempt -> 'answer' ->> 'type') = 'llmFreeTextChoice'
-        THEN
-            (question_attempt -> 'marksAwarded')::int
-        ELSE
-            correct::int
-        END
-    ) STORED;
+ADD marks integer;
+
+CREATE FUNCTION update_marks_for_period(start_date date, end_date date)
+    RETURNS void AS $$
+BEGIN
+    WITH case_statement AS (
+        SELECT
+            id,
+            CASE
+                WHEN
+                    (question_attempt -> 'answer' ->> 'type') = 'llmFreeTextChoice'
+                THEN
+                    (question_attempt ->> 'marksAwarded')::int
+                ELSE
+                    correct::int
+            END AS marks
+        FROM question_attempts
+        WHERE timestamp > start_date
+          AND timestamp <= end_date
+    )
+
+    UPDATE question_attempts
+    SET marks = case_statement.marks
+    FROM case_statement
+    WHERE question_attempts.id = case_statement.id;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT update_marks_for_period('2014-01-01', '2015-01-02');
+SELECT update_marks_for_period('2015-01-01', '2016-01-02');
+SELECT update_marks_for_period('2016-01-02', '2025-12-01');
+
+DROP FUNCTION update_marks_for_period;
