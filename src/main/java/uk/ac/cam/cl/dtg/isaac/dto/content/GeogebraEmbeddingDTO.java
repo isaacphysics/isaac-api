442189cf6131a4705758fda509310424b268de99
Jaycie Brown <jeab2@cam.ac.uk>
2026-01-09 14:45:54 +0000

    Include more fields in Geogebra DO/DTO

--- src/main/java/uk/ac/cam/cl/dtg/isaac/dto/content/GeogebraEmbeddingDTO.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/dto/content/GeogebraEmbeddingDTO.java
@@ -1,13 +1,31 @@
 package uk.ac.cam.cl.dtg.isaac.dto.content;
 
 public class GeogebraEmbeddingDTO extends MediaDTO {
-    private String appId;
+    private String materialId;
+    private String appType;
+    private Boolean allowNewInputs;
 
-    public String getAppId() {
-        return appId;
+    public String getMaterialId() {
+        return materialId;
     }
 
-    public void setAppId(String appId) {
-        this.appId = appId;
+    public void setMaterialId(String materialId) {
+        this.materialId = materialId;
+    }
+
+    public String getAppType() {
+        return appType;
+    }
+
+    public void setAppType(String appType) {
+        this.appType = appType;
+    }
+
+    public Boolean getAllowNewInputs() {
+        return allowNewInputs;
+    }
+
+    public void setAllowNewInputs(Boolean allowNewInputs) {
+        this.allowNewInputs = allowNewInputs;
     }
 }
