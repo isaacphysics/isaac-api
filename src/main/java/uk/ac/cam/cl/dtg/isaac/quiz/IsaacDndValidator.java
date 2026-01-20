99e2e9623a59e6f81648fbe464f34d57a410b1de
Barna Magyarkuti <bm710@cam.ac.uk>
2026-01-20 16:37:44 +0000

    for answers, just use if statements for validation

--- src/main/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacDndValidator.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/quiz/IsaacDndValidator.java
@@ -132,7 +132,10 @@ public class IsaacDndValidator implements IValidator {
 
         return new ValidationUtils.BiRuleValidator<IsaacDndQuestion, DndChoice>()
             .add((q, a) -> questionValidator(IsaacDndValidator::logIfTrue).check(q))
-            .add((q, a) -> answerValidator(IsaacDndValidator::noLog).check(q, a).map(m -> "You provided " + m))
+            .add((q, a) -> {
+                var result = validateAnswer(q, a);
+                return result == null ? Optional.empty() : Optional.of(String.format("You provided %s", result.problem));
+            })
             .check((IsaacDndQuestion) question, (DndChoice) answer);
     }
 
@@ -165,8 +168,10 @@ public class IsaacDndValidator implements IValidator {
                 format("Expected DndItem in question (%s), instead found %s!", q.getId(), c.getClass())
             )))
             .add(q -> q.getChoices().stream()
-                .flatMap(c -> answerValidator(IsaacDndValidator::logIfTrue).check(q, (DndChoice) c)
-                    .map(m -> "The question is invalid, because it has " + m).stream())
+                .map(c -> validateAnswer(q, (DndChoice) c))
+                .filter(Objects::nonNull)
+                .peek(DndLogger::log)
+                .map(m -> "The question is invalid, because it has " + m.problem)
                 .findFirst()
             )
             .add(Constants.FEEDBACK_NO_CORRECT_ANSWERS, q -> logged.apply(
@@ -180,42 +185,31 @@ public class IsaacDndValidator implements IValidator {
             )));
     }
 
-    private static ValidationUtils.BiRuleValidator<IsaacDndQuestion, DndChoice> answerValidator(
-        final BiFunction<Boolean, String, Boolean> logged
-    ) {
-        return new ValidationUtils.BiRuleValidator<IsaacDndQuestion, DndChoice>()
-            .add("an empty answer.", (q, a) -> logged.apply(
-                a.getItems() == null || a.getItems().isEmpty(),
-                format("Expected list of DndItems, but none found in choice for question id (%s)!", q.getId())
-            ))
-            .add("an invalid answer.", (q, a) -> a.getItems().stream().anyMatch(i -> logged.apply(
-                i.getClass() != DndItem.class,
-                format("Expected list of DndItems, but something else found in choice for question id (%s)!", q.getId())
-            )))
-            .add("an answer in an unrecognised format.", (q, a) -> a.getItems().stream().anyMatch(i -> logged.apply(
-                i.getId() == null || i.getDropZoneId() == null
-                    || Objects.equals(i.getId(), "") || Objects.equals(i.getDropZoneId(), ""),
-                format("Found item with missing id or drop zone id in answer for question id (%s)!", q.getId()))
-            ))
-            .add("an answer with unrecognised items.", (q, a) -> a.getItems().stream().anyMatch(i -> logged.apply(
-                !q.getItems().contains(i),
-                format("Question contains invalid item ref. %s src %s", q.getId(), q.getCanonicalSourceFile())
-            )))
-            .add("an answer with unrecognised drop zones.", (q, a) -> a.getItems().stream().anyMatch(i -> logged.apply(
-                !DropZones.getFromQuestion(q).contains(i.getDropZoneId()),
-                format("Question contains invalid drop zone ref. %s src %s", q.getId(), q.getCanonicalSourceFile())
-            )))
-            .add("an answer with more items than we have gaps.", (q, a) -> logged.apply(
-                a.getItems().size() > DropZones.getFromQuestion(q).size(),
-                format("Question has answer with more items than we have gaps. %s src %s",
-                       q.getId(), q.getCanonicalSourceFile())
-            ))
-            .add("an answer with duplicate drop zones.", (q, a) -> {
-                var dropZoneIds = a.getItems().stream().map(DndItem::getDropZoneId).collect(Collectors.toList());
-                return logged.apply(dropZoneIds.size() != new HashSet<>(dropZoneIds).size(),
-                    format("Question contains duplicate drop zones. %s src %s", q.getId(), q.getCanonicalSourceFile())
-                );
-            });
+    private static DndValidationResult validateAnswer(final IsaacDndQuestion q, final DndChoice a) {
+        if (a.getItems() == null || a.getItems().isEmpty()) {
+            return new DndValidationResult("an empty answer.", q);
+        }
+        if (a.getItems().stream().anyMatch(i -> i.getClass() != DndItem.class)) {
+            return new DndValidationResult("an invalid answer.", q);
+        }
+        if (a.getItems().stream().anyMatch(i -> i.getId() == null || i.getDropZoneId() == null
+                || Objects.equals(i.getId(), "") || Objects.equals(i.getDropZoneId(), ""))) {
+            return new DndValidationResult("an answer in an unrecognised format.", q);
+        }
+        if (a.getItems().stream().anyMatch(i -> !q.getItems().contains(i))) {
+            return new DndValidationResult("an answer with unrecognised items.", q);
+        }
+        if (a.getItems().stream().anyMatch(i -> !DropZones.getFromQuestion(q).contains(i.getDropZoneId()))) {
+            return new DndValidationResult("an answer with unrecognised drop zones.", q);
+        }
+        if (a.getItems().size() > DropZones.getFromQuestion(q).size()) {
+            return new DndValidationResult("an answer with more items than we have gaps.", q);
+        }
+        var dropZoneIds = a.getItems().stream().map(DndItem::getDropZoneId).collect(Collectors.toList());
+        if (dropZoneIds.size() != new HashSet<>(dropZoneIds).size()) {
+            return new DndValidationResult("an answer with duplicate drop zones.", q);
+        }
+        return null;
     }
 
     private static boolean logIfTrue(final boolean result, final String message) {
@@ -260,6 +254,44 @@ public class IsaacDndValidator implements IValidator {
                 return ((Content) content).getChildren().stream().flatMap(DropZones::getFromContent);
             }
             return Stream.of();
+        }
+    }
+
+    private static class DndValidationResult {
+        String problem;
+        Question question;
+
+        public DndValidationResult(final String problem, final Question question) {
+            this.problem = problem;
+            this.question = question;
+        }
+    }
+
+    private static class DndLogger {
+        private static void log(final DndValidationResult result) {
+            log.error(messageFor(result));
+        }
+
+        private static String messageFor(final DndValidationResult message) {
+            var id = message.question.getId();
+            var sourceFile = message.question.getCanonicalSourceFile();
+            switch (message.problem) {
+                case "an empty answer.": return String.format(
+                    "Expected list of DndItems, but none found in choice for question id (%s)!", id);
+                case "an invalid answer.": return String.format(
+                    "Expected list of DndItems, but something else found in choice for question id (%s)!", id);
+                case "an answer in an unrecognised format.": return String.format(
+                    "Found item with missing id or drop zone id in answer for question id (%s)!", id);
+                case "an answer with unrecognised items.": return String.format(
+                    "Question contains invalid item ref. %s src %s", id, sourceFile);
+                case "an answer with unrecognised drop zones.": return String.format(
+                    "Question contains invalid drop zone ref. %s src %s", id, sourceFile);
+                case "an answer with more items than we have gaps.": return String.format(
+                    "Question has answer with more items than we have gaps. %s src %s", id, sourceFile);
+                case "an answer with duplicate drop zones.": return String.format(
+                    "Question contains duplicate drop zones. %s src %s", id, sourceFile);
+                default: return null;
+            }
         }
     }
 }
