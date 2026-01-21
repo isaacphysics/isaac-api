aeafe77721175ce6a46d4c6f957c3f1f76d48ea4
Alex Lewin <sl2281@cam.ac.uk>
2026-01-21 15:55:50 +0000

    Increase socket timeout to 180s

--- src/main/java/uk/ac/cam/cl/dtg/segue/search/ElasticSearchProvider.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/search/ElasticSearchProvider.java
@@ -337,7 +337,7 @@ public class ElasticSearchProvider implements ISearchProvider {
 
         RestClient restClient = RestClient.builder(new HttpHost(InetAddress.getByName(address), port, "http"))
                 .setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
-                .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setSocketTimeout(60000))
+                .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setSocketTimeout(180000))
                 .build();
 
         ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
