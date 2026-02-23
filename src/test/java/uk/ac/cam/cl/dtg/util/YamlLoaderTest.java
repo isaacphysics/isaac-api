968bf32e9c2b690d487ff762759960d3f1767949
Alex Lewin <sl2281@cam.ac.uk>
2026-02-23 15:41:24 +0000

    Remove PowerMock usage from YamlLoaderTest

--- src/test/java/uk/ac/cam/cl/dtg/util/YamlLoaderTest.java
+++ src/test/java/uk/ac/cam/cl/dtg/util/YamlLoaderTest.java
@@ -2,11 +2,11 @@ package uk.ac.cam.cl.dtg.util;
 
 import org.easymock.EasyMock;
 import org.junit.Test;
-import org.powermock.api.easymock.PowerMock;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -35,62 +35,56 @@ public class YamlLoaderTest {
 
     @Test
     public void yamlLoader_usingSingleConfigFile_loadsSuccessfully() throws Exception {
-        // Arrange
-        setUpMockConfigs();
-
-        // Act
-        YamlLoader loader = new YamlLoader("/some/external/file/path.properties");
-
-        // Assert
+        YamlLoader loader = getTestableYamlLoader("/some/external/file/path.properties", false);
         assertEquals("src/test/resources/content_indices.test.properties", loader.getProperty("CONTENT_INDICES_LOCATION"));
     }
 
     @Test
     public void yamlLoader_usingMultipleConfigFiles_firstFileValuesAreOverriddenBySecond() throws Exception {
-        // Arrange
-        setUpMockConfigs();
-
-        // Act
-        YamlLoader loader = new YamlLoader(
-                "/some/external/file/path.properties,/another/external/file/path.properties"
-        );
-
-        // Assert
+        YamlLoader loader = getTestableYamlLoader("/some/external/file/path.properties,"
+                        + "/another/external/file/path.properties", true);
         assertEquals("somewhere/on/my/machine.properties", loader.getProperty("CONTENT_INDICES_LOCATION"));
     }
 
-    public void setUpMockConfigs() throws Exception {
+    private YamlLoader getTestableYamlLoader(final String paths, final boolean includeOverride) throws Exception {
         // Mock on-disk config files
         // main config file
-        String configPath = "/some/external/file/path.properties";
-
         Map<String, String> mockConfig = new ConcurrentHashMap<>();
         mockConfig.put("CONTENT_INDICES_LOCATION", "src/test/resources/content_indices.test.properties");
 
         FileInputStream mockConfigFileInputStream = EasyMock.createNiceMock(FileInputStream.class);
-        EasyMock.replay(mockConfigFileInputStream);
-
-        PowerMock.expectNew(FileInputStream.class, configPath).andReturn(mockConfigFileInputStream);
 
         // override config file
-        String overridePath = "/another/external/file/path.properties";
-
         Map<String, String> mockOverrideConfig = new ConcurrentHashMap<>();
         mockOverrideConfig.put("CONTENT_INDICES_LOCATION", "somewhere/on/my/machine.properties");
 
         FileInputStream mockOverrideFileInputStream = EasyMock.createNiceMock(FileInputStream.class);
-        EasyMock.replay(mockOverrideFileInputStream);
-
-        PowerMock.expectNew(FileInputStream.class, overridePath).andReturn(mockOverrideFileInputStream);
-
-        // configure mock YAML parser to return mock configs
-        PowerMock.replay(FileInputStream.class);
 
         Yaml mockYaml = EasyMock.createMock(Yaml.class);
-        EasyMock.expect(mockYaml.load(mockConfigFileInputStream)).andStubReturn(mockConfig);
-        EasyMock.expect(mockYaml.load(mockOverrideFileInputStream)).andStubReturn(mockOverrideConfig);
-        EasyMock.replay(mockYaml);
-        PowerMock.expectNew(Yaml.class).andReturn(mockYaml);
-        PowerMock.replay(Yaml.class);
+
+        // configure mock YAML parser to return mock configs
+        if (includeOverride) {
+            EasyMock.expect(mockYaml.load(EasyMock.anyObject(InputStream.class)))
+                    .andReturn(mockConfig)
+                    .andReturn(mockOverrideConfig);
+        } else {
+            EasyMock.expect(mockYaml.load(EasyMock.anyObject(InputStream.class)))
+                    .andReturn(mockConfig);
+        }
+
+        EasyMock.replay(mockConfigFileInputStream, mockOverrideFileInputStream, mockYaml);
+
+        // This cannot be a (partial) mock because we need to set configPath
+        return new YamlLoader(paths) {
+            @Override
+            protected Yaml createYaml() {
+                return mockYaml;
+            }
+
+            @Override
+            protected FileInputStream createFileInputStream(final String path) {
+                return includeOverride ? mockOverrideFileInputStream : mockConfigFileInputStream;
+            }
+        };
     }
 }
