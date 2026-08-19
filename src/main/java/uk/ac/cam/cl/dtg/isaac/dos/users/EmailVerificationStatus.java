7b782efb6863682759d0c2ee9bb0f028297bd249
Jaycie Brown <jeab2@cam.ac.uk>
2026-08-19 15:25:52 +0100

    Support basic u13 SSO flow via RPF for Ada

--- src/main/java/uk/ac/cam/cl/dtg/isaac/dos/users/EmailVerificationStatus.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/dos/users/EmailVerificationStatus.java
@@ -22,5 +22,5 @@ package uk.ac.cam.cl.dtg.isaac.dos.users;
  *
  */
 public enum EmailVerificationStatus {
-    VERIFIED, NOT_VERIFIED, DELIVERY_FAILED;
+    VERIFIED, NOT_VERIFIED, DELIVERY_FAILED, LOCKED;
 }
