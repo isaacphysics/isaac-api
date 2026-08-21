164a0499e14bcde635fbfd5c397bfcfb5ae35d7d
James Sharkey <jsharkey13@users.noreply.github.com>
2026-08-21 15:20:17 +0100

    Make completion_notifications NOT NULL in database

--- src/main/resources/db_scripts/migrations/2026-08-quiz-assignments-notifications.sql
+++ src/main/resources/db_scripts/migrations/2026-08-quiz-assignments-notifications.sql
@@ -1,1 +1,1 @@
-ALTER TABLE quiz_assignments ADD COLUMN completion_notifications BOOLEAN DEFAULT FALSE;
+ALTER TABLE quiz_assignments ADD COLUMN completion_notifications BOOLEAN NOT NULL DEFAULT FALSE;
