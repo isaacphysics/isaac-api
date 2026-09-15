1d7e45c4e3e7c665e3d6fc5d5d254e51d536d928
Alex Lewin <sl2281@cam.ac.uk>
2026-09-15 15:38:12 +0100

    Modify password reset metric name
    Clarify that this counts requests, regardless of whether they result in a successful reset.

--- src/main/java/uk/ac/cam/cl/dtg/segue/api/monitors/SegueMetrics.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/monitors/SegueMetrics.java
@@ -83,7 +83,7 @@ public final class SegueMetrics {
             .name("segue_log_out_everywhere_total").help("Log out everywhere requests since process start.").register();
 
     public static final Counter PASSWORD_RESET_REQUESTS = Counter.build()
-            .name("segue_password_reset_total").help("Password reset requests since process start.").register();
+            .name("segue_password_reset_requests").help("Password reset requests since process start.").register();
 
     // Email Metrics
     public static final Counter QUEUED_EMAIL = Counter.build()
