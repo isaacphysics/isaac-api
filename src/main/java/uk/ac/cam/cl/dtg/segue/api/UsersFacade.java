6881fd0f74de59e17993b48d11329c09c3677e6d
Alex Lewin <sl2281@cam.ac.uk>
2026-09-16 11:01:04 +0100

    Add sitewide misuse monitor for password reset attempts
    As well as limiting password reset attempts by email address and IP address, implement an overall sitewide limit to help protect against credential-stuffing attacks.

--- src/main/java/uk/ac/cam/cl/dtg/segue/api/UsersFacade.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/UsersFacade.java
@@ -43,6 +43,7 @@ import uk.ac.cam.cl.dtg.segue.api.managers.UserAssociationManager;
 import uk.ac.cam.cl.dtg.segue.api.monitors.IMisuseMonitor;
 import uk.ac.cam.cl.dtg.segue.api.monitors.PasswordResetByEmailMisuseHandler;
 import uk.ac.cam.cl.dtg.segue.api.monitors.PasswordResetByIPMisuseHandler;
+import uk.ac.cam.cl.dtg.segue.api.monitors.PasswordResetSitewideMisuseHandler;
 import uk.ac.cam.cl.dtg.segue.api.monitors.RegistrationMisuseHandler;
 import uk.ac.cam.cl.dtg.segue.api.monitors.SegueLoginMisuseHandler;
 import uk.ac.cam.cl.dtg.segue.api.monitors.SegueMetrics;
@@ -448,6 +449,7 @@ public class UsersFacade extends AbstractSegueFacade {
             String requestingIPAddress = RequestIPExtractor.getClientIpAddr(request);
             misuseMonitor.notifyEvent(userObject.getEmail(), PasswordResetByEmailMisuseHandler.class.getSimpleName());
             misuseMonitor.notifyEvent(requestingIPAddress, PasswordResetByIPMisuseHandler.class.getSimpleName());
+            misuseMonitor.notifyEvent(SITEWIDE_MISUSE, PasswordResetSitewideMisuseHandler.class.getSimpleName());
             userManager.resetPasswordRequest(userObject);
 
             this.getLogManager()
---
+++ src/main/java/uk/ac/cam/cl/dtg/segue/api/monitors/PasswordResetSitewideMisuseHandler.java
@@ -0,0 +1,67 @@
+package uk.ac.cam.cl.dtg.segue.api.monitors;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.ac.cam.cl.dtg.segue.api.Constants;
+import uk.ac.cam.cl.dtg.segue.comm.EmailCommunicationMessage;
+import uk.ac.cam.cl.dtg.segue.comm.EmailManager;
+import uk.ac.cam.cl.dtg.segue.comm.EmailType;
+import uk.ac.cam.cl.dtg.util.AbstractConfigLoader;
+
+import static uk.ac.cam.cl.dtg.segue.api.Constants.NUMBER_SECONDS_IN_ONE_DAY;
+
+public class PasswordResetSitewideMisuseHandler implements IMisuseHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(PasswordResetSitewideMisuseHandler.class);
+
+    private static final Integer SOFT_THRESHOLD = 500;
+    private static final Integer HARD_THRESHOLD = 2500;
+    private static final Integer ACCOUNTING_INTERVAL = NUMBER_SECONDS_IN_ONE_DAY;
+
+    private final AbstractConfigLoader properties;
+    private final EmailManager emailManager;
+
+    /**
+     *  Constructor for Guice injection.
+     */
+    @Inject
+    public PasswordResetSitewideMisuseHandler(final EmailManager emailManager, final AbstractConfigLoader properties) {
+        this.properties = properties;
+        this.emailManager = emailManager;
+    }
+
+    @Override
+    public Integer getSoftThreshold() {
+        return SOFT_THRESHOLD;
+    }
+
+    @Override
+    public Integer getHardThreshold() {
+        return HARD_THRESHOLD;
+    }
+
+    @Override
+    public Integer getAccountingIntervalInSeconds() {
+        return ACCOUNTING_INTERVAL;
+    }
+
+    @Override
+    public void executeSoftThresholdAction(final String message) {
+        final String subject = "Soft Threshold limit reached for Sitewide Password Reset endpoint";
+        EmailCommunicationMessage e = new EmailCommunicationMessage(properties.getProperty(Constants.SERVER_ADMIN_ADDRESS),
+                subject, message, message, EmailType.ADMIN);
+        emailManager.addSystemEmailToQueue(e);
+        log.warn("Soft threshold limit: {}", message);
+    }
+
+    @Override
+    public void executeHardThresholdAction(final String message) {
+        final String subject = "HARD Threshold limit reached for Sitewide Password Reset endpoint";
+        EmailCommunicationMessage e = new EmailCommunicationMessage(properties.getProperty(Constants.SERVER_ADMIN_ADDRESS),
+                subject, message, message, EmailType.ADMIN);
+        emailManager.addSystemEmailToQueue(e);
+        log.error("Hard threshold limit: {}", message);
+    }
+
+}
