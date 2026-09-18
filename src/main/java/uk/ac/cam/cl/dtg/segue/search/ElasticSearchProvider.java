1142ffc1ad0ee5a5fa8d04f0e3702e503fb60853
Alex Lewin <sl2281@cam.ac.uk>
2026-09-18 16:11:01 +0100

    Fix totalHits calculation
    This should be the total hits remaining after threshold filtering, since the discarded hits are not accessible from outside of this method.
    
    This isn't _strictly_ how totalHits should be used in an Elasticsearch sense, but in the Isaac-specific ResultsWrapper implementation, we use totalHits to determine how many results could be returned, and filtered-out results are never returned.

--- src/main/java/uk/ac/cam/cl/dtg/segue/search/ElasticSearchProvider.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/search/ElasticSearchProvider.java
@@ -335,10 +335,6 @@ public class ElasticSearchProvider implements ISearchProvider {
             List<Hit<ObjectNode>> hits = response.hits().hits();
             List<String> resultList = new ArrayList<>();
 
-            long totalHits = null != response.hits().total()
-                    ? response.hits().total().value()
-                    : 0;
-
             Double bestScore = null;
             if (!hits.isEmpty()) {
                 bestScore = hits.getFirst().score();
@@ -351,6 +347,8 @@ public class ElasticSearchProvider implements ISearchProvider {
                     resultList.add(null != src ? src.toString() : "{}");
                 }
             }
+
+            long totalHits = resultList.size();
 
             return new ResultsWrapper<>(resultList, totalHits);
         } catch (ElasticsearchException | IOException e) {
